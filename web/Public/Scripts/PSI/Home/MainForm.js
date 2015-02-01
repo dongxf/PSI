52476ee4f895191dc72b7db57f54e7d68c325934
CRM8000 <crm8000@qq.com>
2015-02-01 21:02:41 +0800

    修正Bug

--- web/Public/Scripts/PSI/Home/MainForm.js
+++ web/Public/Scripts/PSI/Home/MainForm.js
@@ -8,7 +8,7 @@
     items: [{
             border: 0,
             html: "<h1>欢迎使用开源进销存PSI</h1>\n\
-<p>使用帮助请点击这里：<a href='http://my.oschina.net/u/134395/blog/373978' target='_blank'>http://my.oschina.net/u/134395/blog/373978</a></p>\n\
+<p>使用帮助请点击这里：<a href='http://my.oschina.net/u/134395/blog/374195' target='_blank'>http://my.oschina.net/u/134395/blog/374195</a></p>\n\
 <p>如需技术支持，请联系：QQ：1569352868 Email：1569352868@qq.com</p>"
     }],
 
