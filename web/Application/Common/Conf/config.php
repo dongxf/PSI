fd455dd29bce9edba6a3a9084bae7cf6d5d19a99
CRM8000 <crm8000@qq.com>
2015-02-01 17:40:48 +0800

    修改数据库配置，部署到MOPASS

--- web/Application/Common/Conf/config.php
+++ web/Application/Common/Conf/config.php
@@ -2,9 +2,9 @@
 
 return array(
 	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => getenv("MOPAAS") == "1" ? getenv("MOPAAS_MYSQL22118_HOST") : 'localhost', // 服务器地址
-	'DB_NAME'   => getenv("MOPAAS") == "1" ? getenv("MOPAAS_MYSQL22118_NAME") :  'psi', // 数据库名
-	'DB_USER'   => getenv("MOPAAS") == "1" ? getenv("MOPAAS_MYSQL22118_USER") : 'root', // 用户名
-	'DB_PWD'    => getenv("MOPAAS") == "1" ? getenv("MOPAAS_MYSQL22118_PASSWORD") : '', // 密码
-	'DB_PORT'   => getenv("MOPAAS") == "1" ? getenv("MOPAAS_MYSQL22118_PORT") : 3306, // 端口
+	'DB_HOST'   => getenv("MOPAAS_MYSQL22118_HOST") ? getenv("MOPAAS_MYSQL22118_HOST") : 'localhost', // 服务器地址
+	'DB_NAME'   => getenv("MOPAAS_MYSQL22118_NAME") ? getenv("MOPAAS_MYSQL22118_NAME") :  'psi', // 数据库名
+	'DB_USER'   => getenv("MOPAAS_MYSQL22118_USER") ? getenv("MOPAAS_MYSQL22118_USER") : 'root', // 用户名
+	'DB_PWD'    => getenv("MOPAAS_MYSQL22118_PASSWORD") ? getenv("MOPAAS_MYSQL22118_PASSWORD") : '', // 密码
+	'DB_PORT'   => getenv("MOPAAS_MYSQL22118_PORT") ? getenv("MOPAAS_MYSQL22118_PORT") : 3306, // 端口
 );
