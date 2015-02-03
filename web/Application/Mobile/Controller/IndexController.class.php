dec524527d5274cf5561da20c9d6069a439730f6
CRM8000 <crm8000@qq.com>
2015-02-03 21:14:20 +0800

    为移动端页面做准备

---
+++ web/Application/Mobile/Controller/IndexController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Mobile\Controller;
+use Think\Controller;
+
+class IndexController extends Controller {
+    public function index(){		
+		$this->assign("title", "首页");
+		$this->assign("uri", __ROOT__ . "/");
+		
+		$dtFlag = getdate();
+		$this->assign("dtFlag", $dtFlag[0]);
+
+		$this->display();
+    }
+}
