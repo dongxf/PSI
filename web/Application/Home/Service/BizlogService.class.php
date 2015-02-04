be5e52cf0d151e55d99b2bbf0188951660997e7f
CRM8000 <crm8000@qq.com>
2015-02-04 13:09:24 +0800

    修正Bug

--- web/Application/Home/Service/BizlogService.class.php
+++ web/Application/Home/Service/BizlogService.class.php
@@ -9,61 +9,57 @@ namespace Home\Service;
  */
 class BizlogService {
 
-    public function logList($params) {
-        $page = $params["page"];
-        $start = $params["start"];
-        $limit = $params["limit"];
+	public function logList($params) {
+		$page = $params["page"];
+		$start = $params["start"];
+		$limit = $params["limit"];
 
-        $sql = "select b.id, u.login_name, u.name, b.ip, b.info, b.date_created, b.log_category "
-                . " from t_biz_log b, t_user u"
-                . " where b.user_id = u.id"
-                . " order by b.date_created desc"
-                . " limit " . $start . ", " . $limit;
-        $data = M()->query($sql);
-        $result = array();
+		$sql = "select b.id, u.login_name, u.name, b.ip, b.info, b.date_created, b.log_category "
+				. " from t_biz_log b, t_user u"
+				. " where b.user_id = u.id"
+				. " order by b.date_created desc"
+				. " limit " . $start . ", " . $limit;
+		$data = M()->query($sql);
+		$result = array();
 
-        foreach ($data as $i => $v) {
-            $result[$i]["id"] = $v["id"];
-            $result[$i]["loginName"] = $v["login_name"];
-            $result[$i]["userName"] = $v["name"];
-            $result[$i]["ip"] = $v["ip"];
-            $result[$i]["content"] = $v["info"];
-            $result[$i]["dt"] = $v["date_created"];
-            $result[$i]["logCategory"] = $v["log_category"];
-        }
+		foreach ($data as $i => $v) {
+			$result[$i]["id"] = $v["id"];
+			$result[$i]["loginName"] = $v["login_name"];
+			$result[$i]["userName"] = $v["name"];
+			$result[$i]["ip"] = $v["ip"];
+			$result[$i]["content"] = $v["info"];
+			$result[$i]["dt"] = $v["date_created"];
+			$result[$i]["logCategory"] = $v["log_category"];
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public function logTotalCount() {
-        $sql = "select count(*) as cnt "
-                . " from t_biz_log b, t_user u"
-                . " where b.user_id = u.id";
-        $data = M()->query($sql);
-        return $data[0]["cnt"];
-    }
+	public function logTotalCount() {
+		$sql = "select count(*) as cnt "
+				. " from t_biz_log b, t_user u"
+				. " where b.user_id = u.id";
+		$data = M()->query($sql);
+		return $data[0]["cnt"];
+	}
 
-    public function insertBizlog($log, $category = "系统") {
-        try {
-            $us = new UserService();
+	public function insertBizlog($log, $category = "系统") {
+		try {
+			$us = new UserService();
 			if ($us->getLoginUserId() == null) {
 				return;
 			}
 
-            $sql = "insert into t_biz_log (user_id, info, ip, date_created, log_category) "
-                    . " values ('%s', '%s', '%s',  now(), '%s')";
-            M()->execute($sql, $us->getLoginUserId(), $log, $this->getClientIP(), $category);
-        } catch (Exception $ex) {
-            // TODO log 
-        }
-    }
-
-	private function getClientIP() {
-		$xRealIP = $_SERVER["HTTP_X_REAL_IP"];
-		if ($xRealIP) {
-			return $xRealIP;
-		} else {
-			return get_client_ip();
+			$sql = "insert into t_biz_log (user_id, info, ip, date_created, log_category) "
+					. " values ('%s', '%s', '%s',  now(), '%s')";
+			M()->execute($sql, $us->getLoginUserId(), $log, $this->getClientIP(), $category);
+		} catch (Exception $ex) {
+			// TODO log 
 		}
 	}
+
+	private function getClientIP() {
+		return get_client_ip();
+	}
+
 }
