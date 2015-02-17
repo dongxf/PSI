f54b6e81799c591aacad4f8ea3ffaf9b0d4109fb
CRM8000 <crm8000@qq.com>
2015-02-17 10:09:54 +0800

    修订版本号

--- web/Public/Scripts/PSI/Home/MainForm.js
+++ web/Public/Scripts/PSI/Home/MainForm.js
@@ -9,7 +9,8 @@
             border: 0,
             html: "<h1>欢迎使用开源进销存PSI</h1>\n\
 <p>使用帮助请点击这里：<a href='http://my.oschina.net/u/134395/blog/374195' target='_blank'>http://my.oschina.net/u/134395/blog/374195</a></p>\n\
-<p>如需技术支持，请联系：QQ：1569352868 Email：1569352868@qq.com  QQ群：414474186</p>"
+<p>如需技术支持，请联系：QQ：1569352868 Email：1569352868@qq.com  QQ群：414474186</p>\n\
+<p>当前版本：" + PSI.Const.VERSION + "</p>"
     }],
 
     initComponent: function () {
