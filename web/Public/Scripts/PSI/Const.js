c20586b41017bfbb93eb0e170ab8ae3d7077862f
CRM8000 <crm8000@qq.com>
2015-02-23 21:40:59 +0800

    修改首页

--- web/Public/Scripts/PSI/Const.js
+++ web/Public/Scripts/PSI/Const.js
@@ -4,6 +4,6 @@ Ext.define("PSI.Const", {
         LOADING: "数据加载中...",
         SAVING: "数据保存中...",
         BASE_URL: "",
-        VERSION: "PSI2015 beta1 - build201502232021"
+        VERSION: "PSI2015 beta1 - build2015-02-23/21:39"
     }
 });
--- web/Public/Scripts/PSI/Home/MainForm.js
+++ web/Public/Scripts/PSI/Home/MainForm.js
@@ -9,7 +9,10 @@
             border: 0,
             html: "<h1>欢迎使用开源进销存PSI</h1><br />\n\
 <p>使用帮助请点击这里：<a href='http://my.oschina.net/u/134395/blog/374195' target='_blank'>http://my.oschina.net/u/134395/blog/374195</a></p> <br />\n\
-<p>免费技术支持，请联系：QQ：1569352868 Email：1569352868@qq.com  QQ群：414474186</p> <br/>" +
+<p>免费技术支持请联系：</p>" 
+            	+ "<p>QQ：1569352868 " + '<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=1569352868&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:1569352868:51" alt="点击这里给我发消息" title="点击这里给我发消息"/></a>'
+            	+ "<p>Email：1569352868@qq.com  <p>QQ群：414474186 " + '<a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=64808ce24f2a3186ccb1f37aad9ed591bcc4fb257d09749753aca98c6c73e400"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="开源进销存PSI" title="开源进销存PSI"></a>'
+            	+ "</p> <br/>" +
 "<p>如需购买商业服务，请访问：<a href='http://weidian.com/?userid=315007574' target='_blank'>http://weidian.com/?userid=315007574</a></p> <br />" +
 		"<p>当前版本：" + PSI.Const.VERSION + "</p>"
     }],
