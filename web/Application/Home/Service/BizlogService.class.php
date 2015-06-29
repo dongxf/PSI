3d0e88c9281d861befc8950f7db1ea5ad3b2596c
CRM8000 <crm8000@qq.com>
2015-06-29 11:51:46 +0800

    BizlogService.class.php, SQL格式修正

--- web/Application/Home/Service/BizlogService.class.php
+++ web/Application/Home/Service/BizlogService.class.php
@@ -13,16 +13,16 @@ class BizlogService extends PSIBaseService {
 		$page = $params["page"];
 		$start = $params["start"];
 		$limit = $params["limit"];
-
-		$sql = "select b.id, u.login_name, u.name, b.ip, b.info, b.date_created, b.log_category "
-				. " from t_biz_log b, t_user u"
-				. " where b.user_id = u.id"
-				. " order by b.date_created desc"
-				. " limit " . $start . ", " . $limit;
+		
+		$sql = "select b.id, u.login_name, u.name, b.ip, b.info, b.date_created, b.log_category 
+				from t_biz_log b, t_user u
+				where b.user_id = u.id
+				order by b.date_created desc
+				limit  $start , $limit ";
 		$data = M()->query($sql);
 		$result = array();
-
-		foreach ($data as $i => $v) {
+		
+		foreach ( $data as $i => $v ) {
 			$result[$i]["id"] = $v["id"];
 			$result[$i]["loginName"] = $v["login_name"];
 			$result[$i]["userName"] = $v["name"];
@@ -31,14 +31,14 @@ class BizlogService extends PSIBaseService {
 			$result[$i]["dt"] = $v["date_created"];
 			$result[$i]["logCategory"] = $v["log_category"];
 		}
-
+		
 		return $result;
 	}
 
 	public function logTotalCount() {
-		$sql = "select count(*) as cnt "
-				. " from t_biz_log b, t_user u"
-				. " where b.user_id = u.id";
+		$sql = "select count(*) as cnt 
+				from t_biz_log b, t_user u
+				where b.user_id = u.id";
 		$data = M()->query($sql);
 		return $data[0]["cnt"];
 	}
@@ -49,12 +49,11 @@ class BizlogService extends PSIBaseService {
 			if ($us->getLoginUserId() == null) {
 				return;
 			}
-
-			$sql = "insert into t_biz_log (user_id, info, ip, date_created, log_category) "
-					. " values ('%s', '%s', '%s',  now(), '%s')";
+			
+			$sql = "insert into t_biz_log (user_id, info, ip, date_created, log_category) 
+					values ('%s', '%s', '%s',  now(), '%s')";
 			M()->execute($sql, $us->getLoginUserId(), $log, $this->getClientIP(), $category);
-		} catch (Exception $ex) {
-			// TODO log 
+		} catch ( Exception $ex ) {
 		}
 	}
 
@@ -70,7 +69,7 @@ class BizlogService extends PSIBaseService {
 					return $result[0];
 				}
 			}
-
+			
 			if ($_SERVER["HTTP_CLIENT_IP"]) {
 				$ip = $_SERVER["HTTP_CLIENT_IP"];
 			} else {
@@ -81,9 +80,7 @@ class BizlogService extends PSIBaseService {
 				return $ip;
 			}
 		}
-
-
+		
 		return get_client_ip();
 	}
-
 }
