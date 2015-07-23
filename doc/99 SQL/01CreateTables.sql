57b9410331d63aee427a71c37ec502b100bdaaa8
CRM8000 <crm8000@qq.com>
2015-07-23 12:15:39 +0800

    库间调拨

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -467,6 +467,7 @@ CREATE TABLE IF NOT EXISTS `t_it_bill_detail` (
   `goods_id` varchar(255) NOT NULL,
   `goods_count` int(11) NOT NULL,
   `show_order` int(11) NOT NULL,
+  `itbill_id` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
