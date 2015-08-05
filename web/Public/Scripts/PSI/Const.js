731eecef762d8a3594fa0758e43cc8d54f95040d
CRM8000 <crm8000@qq.com>
2015-08-05 10:48:44 +0800

    http://git.oschina.net/crm8000/PSI/issues/16

--- web/Public/Scripts/PSI/Const.js
+++ web/Public/Scripts/PSI/Const.js
@@ -4,6 +4,6 @@ Ext.define("PSI.Const", {
         LOADING: "数据加载中...",
         SAVING: "数据保存中...",
         BASE_URL: "",
-        VERSION: "PSI2015 beta2 - build2015-08-04/22:42"
+        VERSION: "PSI2015 beta2 - build2015-08-05/10:48"
     }
 });
