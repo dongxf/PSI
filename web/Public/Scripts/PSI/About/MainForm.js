1b34ce7214b4068fdcc1a923dd9eacf012f5240e
CRM8000 <crm8000@qq.com>
2015-08-07 17:06:53 +0800

    首页

--- web/Public/Scripts/PSI/About/MainForm.js
+++ web/Public/Scripts/PSI/About/MainForm.js
@@ -18,7 +18,7 @@ Ext.define("PSI.About.MainForm", {
                     border: 0,
                     xtype: "container",
                     html: "<h1>欢迎使用开源进销存PSI</h1><p>当前版本：" + PSI.Const.VERSION + "</p>"
-                    + "<p>产品主页请点击这里：<a href='http://git.oschina.net/crm8000/PSI' target='_blank'>http://git.oschina.net/crm8000/PSI</a></p>"
+                    + "<p>产品源码下载请访问<a href='http://git.oschina.net/crm8000/PSI' target='_blank'>http://git.oschina.net/crm8000/PSI</a></p>"
                     + "<p>如需技术支持，请联系：</p><p>QQ：1569352868 Email：1569352868@qq.com QQ群：414474186</p>"
                 }
             ],
--- web/Public/Scripts/PSI/Home/MainForm.js
+++ web/Public/Scripts/PSI/Home/MainForm.js
@@ -2,19 +2,217 @@
     extend: "Ext.panel.Panel",
 
     border: 0,
-    layout: "fit",
     bodyPadding: 5,
-    autoScroll: true,
-    items: [{
-            border: 0,
-            html: "<h1>欢迎使用开源进销存PSI</h1>"
-            	+ "<br /><p>产品源码及下载请访问<a href='http://git.oschina.net/crm8000/PSI' target='_blank'>http://git.oschina.net/crm8000/PSI</a></p>"
-            	+ "<br /><p>当前版本：" + PSI.Const.VERSION + "</p>"
-    }],
-
+    
     initComponent: function () {
         var me = this;
+        
+        Ext.apply(me, {
+        	layout: "hbox",
+            items: [{
+            	region: "west", flex: 1, layout: "vbox", border: 0,
+            	items: [
+            	        {
+            	        	flex: 1,
+            	        	width: "100%",
+            	        	height: 240,
+            	        	margin: "5",
+            	        	header: {
+            	                title: "<span style='font-size:120%'>销售看板</span>",
+            	                iconCls: "PSI-portal-sale",
+            	                height: 40
+            	            },
+        	                layout: "fit",
+        	                items: [
+    	                        me.getSaleGrid()
+        	                ]
+            	        },
+            	        {
+            	        	header: {
+            	                title: "<span style='font-size:120%'>采购看板</span>",
+            	                iconCls: "PSI-portal-purchase",
+            	                height: 40
+            	            },
+            	        	flex: 1,
+            	        	width: "100%",
+            	        	height: 240,
+            	        	margin: "5",
+            	        	layout: "fit",
+            	        	items: [me.getPurchaseGrid()]
+            	        }
+            	]
+            },{
+            	flex: 1, layout: "vbox", border: 0,
+            	items: [
+            	        {
+            	        	header: {
+            	                title: "<span style='font-size:120%'>库存看板</span>",
+            	                iconCls: "PSI-portal-inventory",
+            	                height: 40
+            	            },
+            	        	flex: 1,
+            	        	width: "100%",
+            	        	height: 240,
+            	        	margin: "5",
+            	        	layout: "fit",
+            	        	items: [me.getInventoryGrid()]
+            	        },
+            	        {
+            	        	header: {
+            	                title: "<span style='font-size:120%'>资金看板</span>",
+            	                iconCls: "PSI-portal-money",
+            	                height: 40
+            	            },
+            	        	flex: 1,
+            	        	width: "100%",
+            	        	height: 240,
+            	        	margin: "5",
+            	        	layout: "fit",
+            	        	items: [
+            	        	        me.getMoneyGrid()
+            	        	]
+            	        }]
+            }]
+        });
 
         me.callParent(arguments);
+    },
+    
+    getSaleHtml: function() {
+    	return "<p>本月销售金额&nbsp;&nbsp;20000.00元</p><p>本月毛利&nbsp;&nbsp;1800元, 毛利率 18%</p>";
+    },
+    
+    getSaleGrid: function() {
+    	var me = this;
+    	if (me.__saleGrid) {
+    		return me.__saleGrid;
+    	}
+    	
+    	var modelName = "PSIPortalSale";
+        Ext.define(modelName, {
+            extend: "Ext.data.Model",
+            fields: ["month", "saleMoney", "profit", "rate"]
+        });
+
+        me.__saleGrid = Ext.create("Ext.grid.Panel", {
+            viewConfig: {
+                enableTextSelection: true
+            },
+            columnLines: true,
+            border: 0,
+            columns: [
+                {header: "月份", dataIndex: "month", width: 80, menuDisabled: true, sortable: false},
+                {header: "销售额", dataIndex: "saleMoney", width: 120, menuDisabled: true, sortable: false},
+                {header: "毛利", dataIndex: "profit", width: 120, menuDisabled: true, sortable: false},
+                {header: "毛利率", dataIndex: "rate", menuDisabled: true, sortable: false}
+            ],
+            store: Ext.create("Ext.data.Store", {
+                model: modelName,
+                autoLoad: false,
+                data: []
+            })
+        });
+
+        return me.__saleGrid;
+    },
+    
+    getPurchaseGrid: function() {
+    	var me = this;
+    	if (me.__purchaseGrid) {
+    		return me.__purchaseGrid;
+    	}
+    	
+    	var modelName = "PSIPortalPurchase";
+        Ext.define(modelName, {
+            extend: "Ext.data.Model",
+            fields: ["month", "purchaseMoney"]
+        });
+
+        me.__purchaseGrid = Ext.create("Ext.grid.Panel", {
+            viewConfig: {
+                enableTextSelection: true
+            },
+            columnLines: true,
+            border: 0,
+            columns: [
+                {header: "月份", dataIndex: "month", width: 80, menuDisabled: true, sortable: false},
+                {header: "采购额", dataIndex: "purchaseMoney", width: 120, menuDisabled: true, sortable: false}
+            ],
+            store: Ext.create("Ext.data.Store", {
+                model: modelName,
+                autoLoad: false,
+                data: []
+            })
+        });
+
+        return me.__purchaseGrid;
+    },
+
+    getInventoryGrid: function() {
+    	var me = this;
+    	if (me.__inventoryGrid) {
+    		return me.__inventoryGrid;
+    	}
+    	
+    	var modelName = "PSIPortalInventory";
+        Ext.define(modelName, {
+            extend: "Ext.data.Model",
+            fields: ["warehouseName", "inventoryMoney"]
+        });
+
+        me.__inventoryGrid = Ext.create("Ext.grid.Panel", {
+            viewConfig: {
+                enableTextSelection: true
+            },
+            columnLines: true,
+            border: 0,
+            columns: [
+                {header: "仓库", dataIndex: "warehouseName", width: 80, menuDisabled: true, sortable: false},
+                {header: "存货金额", dataIndex: "inventoryMoney", width: 120, menuDisabled: true, sortable: false}
+            ],
+            store: Ext.create("Ext.data.Store", {
+                model: modelName,
+                autoLoad: false,
+                data: []
+            })
+        });
+
+        return me.__inventoryGrid;
+    },
+
+    getMoneyGrid: function() {
+    	var me = this;
+    	if (me.__moneyGrid) {
+    		return me.__moneyGrid;
+    	}
+    	
+    	var modelName = "PSIPortalMoney";
+        Ext.define(modelName, {
+            extend: "Ext.data.Model",
+            fields: ["item", "balanceMoney", "money30", "money30to60", "money60to90", "money90"]
+        });
+
+        me.__moneyGrid = Ext.create("Ext.grid.Panel", {
+            viewConfig: {
+                enableTextSelection: true
+            },
+            columnLines: true,
+            border: 0,
+            columns: [
+                {header: "款项", dataIndex: "item", width: 80, menuDisabled: true, sortable: false},
+                {header: "当期余额", dataIndex: "balanceMoney", width: 120, menuDisabled: true, sortable: false},
+                {header: "账龄30天内", dataIndex: "money30", width: 120, menuDisabled: true, sortable: false},
+                {header: "账龄30-60天", dataIndex: "money30to60", menuDisabled: true, sortable: false},
+                {header: "账龄60-90天", dataIndex: "money60to90", menuDisabled: true, sortable: false},
+                {header: "账龄大于90天", dataIndex: "money90", menuDisabled: true, sortable: false}
+            ],
+            store: Ext.create("Ext.data.Store", {
+                model: modelName,
+                autoLoad: false,
+                data: []
+            })
+        });
+
+        return me.__moneyGrid;
     }
 });
