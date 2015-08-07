c62ba9aa8078cab42edf0502bffe21a8518c0fa3
CRM8000 <crm8000@qq.com>
2015-08-07 11:55:45 +0800

    修改About

--- web/Public/Scripts/PSI/Home/MainForm.js
+++ web/Public/Scripts/PSI/Home/MainForm.js
@@ -7,7 +7,9 @@
     autoScroll: true,
     items: [{
             border: 0,
-            html: "<h1>欢迎使用开源进销存PSI</h1><br /><p>当前版本：" + PSI.Const.VERSION + "</p>"
+            html: "<h1>欢迎使用开源进销存PSI</h1>"
+            	+ "<br /><p>产品源码及下载请访问<a href='http://git.oschina.net/crm8000/PSI' target='_blank'>http://git.oschina.net/crm8000/PSI</a></p>"
+            	+ "<br /><p>当前版本：" + PSI.Const.VERSION + "</p>"
     }],
 
     initComponent: function () {
