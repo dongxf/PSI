fc157509aff9338e9084da249e872107fd21a465
CRM8000 <crm8000@qq.com>
2015-08-15 19:12:12 +0800

    代码微调

--- web/Public/Scripts/PSI/Const.js
+++ web/Public/Scripts/PSI/Const.js
@@ -4,6 +4,6 @@ Ext.define("PSI.Const", {
         LOADING: "数据加载中...",
         SAVING: "数据保存中...",
         BASE_URL: "",
-        VERSION: "PSI2015 beta2 - build2015-08-15/16:01"
+        VERSION: "PSI2015 beta2 - build2015-08-15/19:11"
     }
 });
