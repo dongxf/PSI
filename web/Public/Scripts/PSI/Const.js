07cddbe8db358d33f4386632de29c82dc5492904
CRM8000 <crm8000@qq.com>
2015-08-22 20:06:01 +0800

    Service层增加用户不在线时的处理逻辑

--- web/Public/Scripts/PSI/Const.js
+++ web/Public/Scripts/PSI/Const.js
@@ -4,6 +4,6 @@ Ext.define("PSI.Const", {
         LOADING: "数据加载中...",
         SAVING: "数据保存中...",
         BASE_URL: "",
-        VERSION: "PSI2015 beta2 - build2015-08-22/18:07"
+        VERSION: "PSI2015 beta2 - build2015-08-22/20:05"
     }
 });
