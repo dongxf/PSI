94f65f76333c912c1eeefc49bac81a74438b0647
CRM8000 <crm8000@qq.com>
2015-08-30 07:04:27 +0800

    采购入库单，增加付款方式

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -272,6 +272,7 @@ CREATE TABLE IF NOT EXISTS `t_pw_bill` (
   `ref` varchar(255) NOT NULL,
   `supplier_id` varchar(255) NOT NULL,
   `warehouse_id` varchar(255) NOT NULL,
+  `payment_type` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
