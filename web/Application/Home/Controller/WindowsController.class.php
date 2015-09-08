9a103d3e5c4ddb85a8f241dd1f1bc82ff88507b5
CRM8000 <crm8000@qq.com>
2015-09-08 21:47:13 +0800

    增加Windows客户端Controller

---
+++ web/Application/Home/Controller/WindowsController.class.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Home\Controller;
+
+use Think\Controller;
+use Home\Service\UserService;
+use Home\Service\BizConfigService;
+use Home\Common\FIdConst;
+use Home\Service\BillViewService;
+
+/**
+ * Windows客户端 Controller
+ *
+ * @author 李静波
+ *        
+ */
+class WindowsController extends Controller {
+
+	/**
+	 * 客户端设置服务器地址，测试服务器地址是否设置正确
+	 */
+	public function test() {
+		if (IS_POST) {
+			$this->ajaxReturn(array(
+					"success" => true
+			));
+		}
+	}
+}
