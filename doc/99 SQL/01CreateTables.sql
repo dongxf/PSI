2e6dd8f55872681e04b08967395809116c88e48c
CRM8000 <crm8000@qq.com>
2015-09-12 16:21:56 +0800

    采购订单

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -657,6 +657,8 @@ CREATE TABLE IF NOT EXISTS `t_po_bill` (
   `deal_address` varchar(255) DEFAULT NULL,
   `bill_memo` varchar(255) DEFAULT NULL,
   `payment_type` int(11) NOT NULL DEFAULT 0,
+  `confirm_user_id` varchar(255) DEFAULT NULL,
+  `confirm_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -675,8 +677,6 @@ CREATE TABLE IF NOT EXISTS `t_po_bill_detail` (
   `pw_count` int(11) NOT NULL,
   `left_count` int(11) NOT NULL,
   `show_order` int(11) NOT NULL,
-  `confirm_user_id` varchar(255) DEFAULT NULL,
-  `confirm_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
