5098c034da3554829ddea5b939fdeda4c129c1a9
CRM8000 <crm8000@qq.com>
2015-11-04 08:14:29 +0800

    商品 - 数据导入 bug修正

--- web/Application/Home/Service/GoodsImportService.class.php
+++ web/Application/Home/Service/GoodsImportService.class.php
@@ -14,7 +14,7 @@ require __DIR__ . '/../Common/Excel/PHPExcel/Reader/Excel2007.php';
 class GoodsImportService {
 
 	public function importGoodsFromExcelFile($excelFilename, $ext) {
-		// $PHPExcel=new \PHPExcel();
+		$PHPExcel = new \PHPExcel();
 		
 		// 默认xlsx
 		$PHPReader = new \PHPExcel_Reader_Excel2007();
@@ -22,16 +22,33 @@ class GoodsImportService {
 		if ($ext == 'xls') {
 			$PHPReader = new \PHPExcel_Reader_Excel5();
 		}
-		$ps = new PinyinService();
-		$idGen = new IdGenService();
-		$bs = new BizlogService();
-		$gs = new GoodsService();
-		
-		// $py = $ps->toPY($name);
 		// 载入文件
 		$PHPExcel = $PHPReader->load($excelFilename);
 		// 获取表中的第一个工作表
 		$currentSheet = $PHPExcel->getSheet(0);
+		// 获取总行数
+		$allRow = $currentSheet->getHighestRow();
+		if ($allRow < 2) {
+			return array(
+					"msg" => '',
+					"success" => true
+			);
+		}
+		
+		$ps = new PinyinService();
+		$idGen = new IdGenService();
+		$bs = new BizlogService();
+		$gs = new GoodsService();
+		$db = M();
+		$units = array(); // 将计量单位缓存，以免频繁访问数据库
+		$categories = array(); // 同上
+		$message = "";
+		
+		$us = new UserService();
+		$dataOrg = $us->getLoginUserDataOrg();
+		
+		$insertSql = "insert into t_goods (id, code, name, spec, category_id, unit_id, sale_price,	py, purchase_price, bar_code, data_org) values";
+		$insertDataSql = "('%s', '%s', '%s', '%s', '%s', '%s', %f, '%s', %f, '%s', '%s'),";
 		/**
 		 * 单元格定义
 		 * A 商品分类编码
@@ -43,9 +60,7 @@ class GoodsImportService {
 		 * G 建议采购单价
 		 * H 条形码
 		 */
-		// 获取总行数
-		$allRow = $currentSheet->getHighestRow();
-		$message = " ";
+		
 		// 从第一行获取数据
 		for($currentRow = 2; $currentRow <= $allRow; $currentRow ++) {
 			// 数据坐标
@@ -58,7 +73,7 @@ class GoodsImportService {
 			$index_purchase_price = 'G' . $currentRow;
 			$index_barcode = 'H' . $currentRow;
 			// 读取到的数据，保存到数组$arr中
-			$catagory = $currentSheet->getCell($index_category)->getValue();
+			$category = $currentSheet->getCell($index_category)->getValue();
 			$code = $currentSheet->getCell($index_code)->getValue();
 			$name = $currentSheet->getCell($index_name)->getValue();
 			$spec = $currentSheet->getCell($index_spec)->getValue();
@@ -68,33 +83,46 @@ class GoodsImportService {
 			$barcode = $currentSheet->getCell($index_barcode)->getValue();
 			
 			// 如果为空则直接读取下一条记录
-			if (! $catagory || ! $code || ! $name || ! $unit)
+			if (! $category || ! $code || ! $name || ! $unit)
 				continue;
 			
 			$unitId = null;
 			$categoryId = null;
 			
-			$db = M();
-			$sql = "select id, name from t_goods_unit where name = '%s' ";
-			$data = $db->query($sql, $unit);
-			if (! $data) {
-				// 新增计量单位
-				$newUnitParams = array(
-						name => $unit
-				);
-				$newUnit = $gs->editUnit($newUnitParams);
-				$unitId = $newUnit["id"];
+			if ($units["{$unit}"]) {
+				$unitId = $units["{$unit}"];
 			} else {
-				$unitId = $data[0]["id"];
+				$sql = "select id, name from t_goods_unit where name = '%s' ";
+				$data = $db->query($sql, $unit);
+				if (! $data) {
+					// 新增计量单位
+					$newUnitParams = array(
+							name => $unit
+					);
+					$newUnit = $gs->editUnit($newUnitParams);
+					$unitId = $newUnit["id"];
+				} else {
+					$unitId = $data[0]["id"];
+				}
+				$units += array(
+						"{$unit}" => "{$unitId}"
+				);
 			}
 			
-			$sql = "select id, name from t_goods_category where code = '%s' ";
-			$data = $db->query($sql, $catagory);
-			if (! $data) {
-				// 新增分类
-				continue;
+			if ($categories["{$category}"]) {
+				$categoryId = $categories["{$category}"];
 			} else {
-				$categoryId = $data[0]["id"];
+				$sql = "select id, name from t_goods_category where code = '%s' ";
+				$data = $db->query($sql, $category);
+				if (! $data) {
+					// 新增分类
+					continue;
+				} else {
+					$categoryId = $data[0]["id"];
+				}
+				$categories += array(
+						"{$category}" => "{$categoryId}"
+				);
 			}
 			
 			// 新增
@@ -119,20 +147,30 @@ class GoodsImportService {
 			$id = $idGen->newId();
 			$py = $ps->toPY($name);
 			
-			$sql = "insert into t_goods (id, code, name, spec, category_id, unit_id, sale_price,
-						py, purchase_price, bar_code)
-					values ('%s', '%s', '%s', '%s', '%s', '%s', %f, '%s', %f, '%s')";
-			$db->execute($sql, $id, $code, $name, $spec, $categoryId, $unitId, $sale_price, $py, 
-					$purchase_price, $barcode);
-			
-			$log = "导入方式新增商品: 商品编码 = {$code}, 品名 = {$name}, 规格型号 = {$spec}";
-			// $message = $message."导入方式新增商品: 商品编码 = {$code}, 品名 = {$name}, 规格型号 = {$spec}";
-			$bs->insertBizlog($log, "基础数据-商品");
+			$datas = array(
+					$id,
+					$code,
+					$name,
+					$spec,
+					$categoryId,
+					$unitId,
+					$sale_price,
+					$py,
+					$purchase_price,
+					$barcode,
+					$dataOrg
+			);
+			// 组装数据部分Sql，整体导入
+			$insertSql .= vsprintf($insertDataSql, $datas);
 		}
+		$db->execute(rtrim($insertSql, ','));
+		
+		$log = "导入方式新增商品;{$excelFilename}";
+		$bs->insertBizlog($log, "基础数据-商品");
 		
 		$result = array(
-				msg => $message,
-				success => true
+				"msg" => $message,
+				"success" => true
 		);
 		
 		return $result;
