e93d884faf2caa25280264206cb4775e1e04201a
CRM8000 <crm8000@qq.com>
2015-11-06 09:43:58 +0800

    客户资料录入时，联系人，手机，传真，收货地址为必录项。

--- web/Public/Scripts/PSI/Customer/CustomerEditForm.js
+++ web/Public/Scripts/PSI/Customer/CustomerEditForm.js
@@ -136,6 +136,9 @@ Ext.define("PSI.Customer.CustomerEditForm", {
                         },{
                             id: "editContact01",
                             fieldLabel: "联系人",
+                            allowBlank: false,
+                            blankText: "没有输入联系人",
+                            beforeLabelTextTpl: PSI.Const.REQUIRED,
                             name: "contact01",
                             value: entity == null ? null : entity.get("contact01"),
                             listeners: {
@@ -148,6 +151,9 @@ Ext.define("PSI.Customer.CustomerEditForm", {
                         {
                             id: "editMobile01",
                             fieldLabel: "手机",
+                            allowBlank: false,
+                            blankText: "没有输入手机",
+                            beforeLabelTextTpl: PSI.Const.REQUIRED,
                             name: "mobile01",
                             value: entity == null ? null : entity.get("mobile01"),
                             listeners: {
@@ -231,6 +237,9 @@ Ext.define("PSI.Customer.CustomerEditForm", {
                         },{
                             id: "editAddressReceipt",
                             fieldLabel: "收货地址",
+                            allowBlank: false,
+                            blankText: "没有输入收货地址",
+                            beforeLabelTextTpl: PSI.Const.REQUIRED,
                             name: "addressReceipt",
                             value: entity == null ? null : entity.get("addressReceipt"),
                             listeners: {
@@ -281,6 +290,9 @@ Ext.define("PSI.Customer.CustomerEditForm", {
                         {
                             id: "editFax",
                             fieldLabel: "传真",
+                            allowBlank: false,
+                            blankText: "没有输入传真",
+                            beforeLabelTextTpl: PSI.Const.REQUIRED,
                             name: "fax",
                             value: entity == null ? null : entity.get("fax"),
                             listeners: {
