6d10b49064e05074d2993cae5968d30bac2ebbb9
CRM8000 <crm8000@qq.com>
2015-11-12 16:58:41 +0800

    演示demo sql文件更新

--- doc/99 SQL/99psi_demo_data.sql
+++ doc/99 SQL/99psi_demo_data.sql
@@ -6,20 +6,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-TRUNCATE TABLE `t_config`;
-INSERT INTO `t_config` (`id`, `name`, `value`, `note`, `show_order`) VALUES
-('9000-01', '公司名称', '', '', 100),
-('9000-02', '公司地址', '', '', 101),
-('9000-03', '公司电话', '', '', 102),
-('9000-04', '公司传真', '', '', 103),
-('9000-05', '公司邮编', '', '', 104),
-('2001-01', '采购入库默认仓库', '', '', 200),
-('2002-02', '销售出库默认仓库', '', '', 300),
-('2002-01', '销售出库单允许编辑销售单价', '0', '当允许编辑的时候，还需要给用户赋予权限[销售出库单允许编辑销售单价]', 301),
-('1003-02', '存货计价方法', '0', '', 401),
-('9001-01', '增值税税率', '17', '', 501),
-('9002-01', '产品名称', '开源进销存PSI', '', 0);
-
 TRUNCATE TABLE `t_customer`;
 INSERT INTO `t_customer` (`id`, `category_id`, `code`, `name`, `contact01`, `qq01`, `tel01`, `mobile01`, `contact02`, `qq02`, `tel02`, `mobile02`, `py`, `init_receivables`, `init_receivables_dt`, `init_payables`, `init_payables_dt`) VALUES
 ('04B53C5E-B812-11E4-8FC9-782BCBD7746B', 'CDD1DE38-B811-11E4-8FC9-782BCBD7746B', '102', '大连宏光天宝大酒店有限公司', '', '', '', '', '', '', '', '', 'DLHGTBDJDYXGS', '7000.00', '2015-01-01 00:00:00', NULL, NULL),
@@ -28,61 +14,14 @@ INSERT INTO `t_customer` (`id`, `category_id`, `code`, `name`, `contact01`, `qq0
 ('3DE2F4E1-B812-11E4-8FC9-782BCBD7746B', 'D33D2C0B-B811-11E4-8FC9-782BCBD7746B', '201', '大连海恩橡胶辅机有限公司', '', '', '', '', '', '', '', '', 'DLHEXJFJYXGS', NULL, NULL, NULL, NULL),
 ('E76FA3FC-B811-11E4-8FC9-782BCBD7746B', 'CDD1DE38-B811-11E4-8FC9-782BCBD7746B', '101', '匿名', '', '', '', '', '', '', '', '', 'NM', NULL, NULL, NULL, NULL);
 
+update t_customer set data_org = '01010001' ;
+
 TRUNCATE TABLE `t_customer_category`;
 INSERT INTO `t_customer_category` (`id`, `code`, `name`, `parent_id`) VALUES
 ('CDD1DE38-B811-11E4-8FC9-782BCBD7746B', '1', '市内', NULL),
 ('D33D2C0B-B811-11E4-8FC9-782BCBD7746B', '2', '北三市', NULL);
 
-TRUNCATE TABLE `t_fid`;
-INSERT INTO `t_fid` (`fid`, `name`) VALUES
-('-9999', '重新登录'),
-('-9997', '首页'),
-('-9996', '修改我的密码'),
-('-9995', '帮助'),
-('-9994', '关于'),
-('-9993', '购买商业服务'),
-('-8999', '用户管理'),
-('-8997', '业务日志'),
-('-8996', '权限管理'),
-('1001', '商品'),
-('1002', '商品计量单位'),
-('1003', '仓库'),
-('1004', '供应商档案'),
-('1007', '客户资料'),
-('2000', '库存建账'),
-('2001', '采购入库'),
-('2002', '销售出库'),
-('2002-01', '销售出库'),
-('2003', '库存账查询'),
-('2004', '应收账款管理'),
-('2005', '应付账款管理'),
-('2006', '销售退货入库'),
-('2007', '采购退货出库'),
-('2008', '业务设置'),
-('2009', '库间调拨'),
-('2010', '库存盘点'),
-('2011-01', '首页-销售看板'),
-('2011-02', '首页-库存看板'),
-('2011-03', '首页-采购看板'),
-('2011-04', '首页-资金看板'),
-('2012', '报表-销售日报表(按商品汇总)'),
-('2013', '报表-销售日报表(按客户汇总)'),
-('2014', '报表-销售日报表(按仓库汇总)'),
-('2015', '报表-销售日报表(按业务员汇总)'),
-('2016', '报表-销售月报表(按商品汇总)'),
-('2017', '报表-销售月报表(按客户汇总)'),
-('2018', '报表-销售月报表(按仓库汇总)'),
-('2019', '报表-销售月报表(按业务员汇总)'),
-('2020', '报表-安全库存明细表'),
-('2021', '报表-应收账款账龄分析表'),
-('2022', '报表-应付账款账龄分析表'),
-('2023', '报表-库存超上限明细表'),
-('2024', '现金收支查询'),
-('2025', '预收款管理'),
-('2026', '预付款管理'),
-('2027', '采购订单'),
-('2027-01', '采购订单 - 审核/取消审核'),
-('2027-02', '采购订单 - 生成采购入库单');
+update t_customer_category set data_org = '01010001' ;
 
 TRUNCATE TABLE `t_goods`;
 INSERT INTO `t_goods` (`id`, `category_id`, `code`, `name`, `sale_price`, `spec`, `unit_id`, `purchase_price`, `py`) VALUES
@@ -125,6 +64,8 @@ INSERT INTO `t_goods` (`id`, `category_id`, `code`, `name`, `sale_price`, `spec`
 ('F4BA9C47-B748-11E4-98B5-782BCBD7746B', '66D899C2-B3F3-11E4-9DEA-782BCBD7746B', '1403', '海尔消毒柜', '1098.00', 'ZTD100-A', '5D6D901E-B3F3-11E4-9DEA-782BCBD7746B', NULL, 'HEXDG'),
 ('FDE1B0F0-B3F3-11E4-9DEA-782BCBD7746B', '66D899C2-B3F3-11E4-9DEA-782BCBD7746B', '1102', '康佳窄边全高清液晶电视', '1999.00', '42英寸黑色LED42E330CE', '5D6D901E-B3F3-11E4-9DEA-782BCBD7746B', NULL, 'KJZBQGQYJDS');
 
+update t_goods set data_org = '01010001' ;
+
 TRUNCATE TABLE `t_goods_category`;
 INSERT INTO `t_goods_category` (`id`, `code`, `name`) VALUES
 ('66D899C2-B3F3-11E4-9DEA-782BCBD7746B', '1', '大家电'),
@@ -133,148 +74,37 @@ INSERT INTO `t_goods_category` (`id`, `code`, `name`) VALUES
 ('89BCA8E9-B3F3-11E4-9DEA-782BCBD7746B', '4', '个护健康'),
 ('953219F9-B3F3-11E4-9DEA-782BCBD7746B', '5', '五金家装');
 
+update t_goods_category set data_org = '01010001' ;
+
 TRUNCATE TABLE `t_goods_unit`;
 INSERT INTO `t_goods_unit` (`id`, `name`) VALUES
 ('50C40382-B745-11E4-9C3C-782BCBD7746B', '套'),
 ('5D6D901E-B3F3-11E4-9DEA-782BCBD7746B', '台');
 
-TRUNCATE TABLE `t_inventory`;
-TRUNCATE TABLE `t_inventory_detail`;
-
-TRUNCATE TABLE `t_menu_item`;
-INSERT INTO `t_menu_item` (`id`, `caption`, `fid`, `parent_id`, `show_order`) VALUES
-('01', '文件', NULL, NULL, 1),
-('0101', '首页', '-9997', '01', 1),
-('0102', '重新登录', '-9999', '01', 2),
-('0103', '修改我的密码', '-9996', '01', 3),
-('02', '采购', NULL, NULL, 2),
-('0200', '采购订单', '2027', '02', 0),
-('0201', '采购入库', '2001', '02', 1),
-('0202', '采购退货出库', '2007', '02', 2),
-('03', '库存', NULL, NULL, 3),
-('0301', '库存账查询', '2003', '03', 1),
-('0302', '库存建账', '2000', '03', 2),
-('0303', '库间调拨', '2009', '03', 3),
-('0304', '库存盘点', '2010', '03', 4),
-('04', '销售', NULL, NULL, 4),
-('0401', '销售出库', '2002', '04', 1),
-('0402', '销售退货入库', '2006', '04', 2),
-('05', '客户关系', NULL, NULL, 5),
-('0501', '客户资料', '1007', '05', 1),
-('06', '资金', NULL, NULL, 6),
-('0601', '应收账款管理', '2004', '06', 1),
-('0602', '应付账款管理', '2005', '06', 2),
-('0603', '现金收支查询', '2024', '06', 3),
-('0604', '预收款管理', '2025', '06', 4),
-('0605', '预付款管理', '2026', '06', 5),
-('07', '报表', NULL, NULL, 7),
-('0701', '销售日报表', NULL, '07', 1),
-('070101', '销售日报表(按商品汇总)', '2012', '0701', 1),
-('070102', '销售日报表(按客户汇总)', '2013', '0701', 2),
-('070103', '销售日报表(按仓库汇总)', '2014', '0701', 3),
-('070104', '销售日报表(按业务员汇总)', '2015', '0701', 4),
-('0702', '销售月报表', NULL, '07', 2),
-('070201', '销售月报表(按商品汇总)', '2016', '0702', 1),
-('070202', '销售月报表(按客户汇总)', '2017', '0702', 2),
-('070203', '销售月报表(按仓库汇总)', '2018', '0702', 3),
-('070204', '销售月报表(按业务员汇总)', '2019', '0702', 4),
-('0703', '库存报表', NULL, '07', 3),
-('070301', '安全库存明细表', '2020', '0703', 1),
-('070302', '库存超上限明细表', '2023', '0703', 2),
-('0706', '资金报表', NULL, '07', 6),
-('070601', '应收账款账龄分析表', '2021', '0706', 1),
-('070602', '应付账款账龄分析表', '2022', '0706', 2),
-('08', '基础数据', NULL, NULL, 8),
-('0801', '商品', '1001', '08', 1),
-('0802', '商品计量单位', '1002', '08', 2),
-('0803', '仓库', '1003', '08', 3),
-('0804', '供应商档案', '1004', '08', 4),
-('09', '系统管理', NULL, NULL, 9),
-('0901', '用户管理', '-8999', '09', 1),
-('0902', '权限管理', '-8996', '09', 2),
-('0903', '业务日志', '-8997', '09', 3),
-('0904', '业务设置', '2008', '09', 4),
-('10', '帮助', NULL, NULL, 10),
-('1001', '使用帮助', '-9995', '10', 1),
-('1002', '购买商业服务', '-9993', '10', 2),
-('1003', '关于PSI', '-9994', '10', 3);
-
-TRUNCATE TABLE `t_org`;
-INSERT INTO `t_org` (`id`, `full_name`, `name`, `org_code`, `parent_id`) VALUES
-('281F55E0-B3E6-11E4-AC63-782BCBD7746B', '大连安世商贸有限公司\\仓储', '仓储', '0104', '4D74E1E4-A129-11E4-9B6A-782BCBD7746B'),
-('2D496F44-B3E5-11E4-AC63-782BCBD7746B', '大连安世商贸有限公司\\采购部', '采购部', '0102', '4D74E1E4-A129-11E4-9B6A-782BCBD7746B'),
-('37500493-B3E5-11E4-AC63-782BCBD7746B', '大连安世商贸有限公司\\销售部', '销售部', '0103', '4D74E1E4-A129-11E4-9B6A-782BCBD7746B'),
-('4D74E1E4-A129-11E4-9B6A-782BCBD7746B', '大连安世商贸有限公司', '大连安世商贸有限公司', '01', NULL),
-('527369D1-B3E5-11E4-AC63-782BCBD7746B', '大连安世商贸有限公司\\总经理办公室', '总经理办公室', '0101', '4D74E1E4-A129-11E4-9B6A-782BCBD7746B'),
-('5EBDBE11-A129-11E4-9B6A-782BCBD7746B', '大连安世商贸有限公司\\信息部', '信息部', '0199', '4D74E1E4-A129-11E4-9B6A-782BCBD7746B'),
-('92FAB90F-B3E8-11E4-A1A5-782BCBD7746B', '大连安世商贸有限公司\\财务部', '财务部', '0105', '4D74E1E4-A129-11E4-9B6A-782BCBD7746B');
-
 TRUNCATE TABLE `t_payables`;
 INSERT INTO `t_payables` (`id`, `act_money`, `balance_money`, `ca_id`, `ca_type`, `pay_money`) VALUES
 ('88072F8B-B80F-11E4-8FC9-782BCBD7746B', '0.00', '5000.00', '87D62652-B80F-11E4-8FC9-782BCBD7746B', 'supplier', '5000.00');
+
+update t_payables set data_org = '01010001' ;
 
 TRUNCATE TABLE `t_payables_detail`;
 INSERT INTO `t_payables_detail` (`id`, `act_money`, `balance_money`, `ca_id`, `ca_type`, `biz_date`, `date_created`, `pay_money`, `ref_number`, `ref_type`) VALUES
 ('8800479E-B80F-11E4-8FC9-782BCBD7746B', '0.00', '5000.00', '87D62652-B80F-11E4-8FC9-782BCBD7746B', 'supplier', '2015-01-01 00:00:00', '2015-02-19 16:15:57', '5000.00', '87D62652-B80F-11E4-8FC9-782BCBD7746B', '应付账款期初建账');
 
-TRUNCATE TABLE `t_payment`;
+update t_payables_detail set data_org = '01010001' ;
 
-TRUNCATE TABLE `t_permission`;
-INSERT INTO `t_permission` (`id`, `fid`, `name`, `note`) VALUES
-('-8996', '-8996', '权限管理', '权限管理'),
-('-8997', '-8997', '业务日志', '业务日志'),
-('-8999', '-8999', '用户管理', '用户管理'),
-('1001', '1001', '商品', '商品'),
-('1002', '1002', '商品计量单位', '商品计量单位'),
-('1003', '1003', '仓库', '仓库'),
-('1004', '1004', '供应商档案', '供应商档案'),
-('1007', '1007', '客户资料', '客户资料'),
-('2000', '2000', '库存建账', '库存建账'),
-('2001', '2001', '采购入库', '采购入库'),
-('2002', '2002', '销售出库', '销售出库'),
-('2002-01', '2002-01', '销售出库单允许编辑销售单价', '销售出库单允许编辑销售单价'),
-('2003', '2003', '库存账查询', '库存账查询'),
-('2004', '2004', '应收账款管理', '应收账款管理'),
-('2005', '2005', '应付账款管理', '应付账款管理'),
-('2006', '2006', '销售退货入库', '销售退货入库'),
-('2007', '2007', '采购退货出库', '采购退货出库'),
-('2008', '2008', '业务设置', '业务设置'),
-('2009', '2009', '库间调拨', '库间调拨'),
-('2010', '2010', '库存盘点', '库存盘点'),
-('2011-01', '2011-01', '首页-销售看板', '首页-销售看板'),
-('2011-02', '2011-02', '首页-库存看板', '首页-库存看板'),
-('2011-03', '2011-03', '首页-采购看板', '首页-采购看板'),
-('2011-04', '2011-04', '首页-资金看板', '首页-资金看板'),
-('2012', '2012', '报表-销售日报表(按商品汇总)', '报表-销售日报表(按商品汇总)'),
-('2013', '2013', '报表-销售日报表(按客户汇总)', '报表-销售日报表(按客户汇总)'),
-('2014', '2014', '报表-销售日报表(按仓库汇总)', '报表-销售日报表(按仓库汇总)'),
-('2015', '2015', '报表-销售日报表(按业务员汇总)', '报表-销售日报表(按业务员汇总)'),
-('2016', '2016', '报表-销售月报表(按商品汇总)', '报表-销售月报表(按商品汇总)'),
-('2017', '2017', '报表-销售月报表(按客户汇总)', '报表-销售月报表(按客户汇总)'),
-('2018', '2018', '报表-销售月报表(按仓库汇总)', '报表-销售月报表(按仓库汇总)'),
-('2019', '2019', '报表-销售月报表(按业务员汇总)', '报表-销售月报表(按业务员汇总)'),
-('2020', '2020', '报表-安全库存明细表', '报表-安全库存明细表'),
-('2021', '2021', '报表-应收账款账龄分析表', '报表-应收账款账龄分析表'),
-('2022', '2022', '报表-应付账款账龄分析表', '报表-应付账款账龄分析表'),
-('2023', '2023', '报表-库存超上限明细表', '报表-库存超上限明细表'),
-('2024', '2024', '现金收支查询', '现金收支查询'),
-('2025', '2025', '预收款管理', '预收款管理'),
-('2026', '2026', '预付款管理', '预付款管理'),
-('2027', '2027', '采购订单', '采购订单'),
-('2027-01', '2027-01', '采购订单 - 审核/取消审核', '采购订单 - 审核/取消审核'),
-('2027-02', '2027-02', '采购订单 - 生成采购入库单', '采购订单 - 生成采购入库单');
-
-TRUNCATE TABLE `t_pw_bill`;
-TRUNCATE TABLE `t_pw_bill_detail`;
 TRUNCATE TABLE `t_receivables`;
 INSERT INTO `t_receivables` (`id`, `act_money`, `balance_money`, `ca_id`, `ca_type`, `rv_money`) VALUES
 ('04DFC20D-B812-11E4-8FC9-782BCBD7746B', '0.00', '7000.00', '04B53C5E-B812-11E4-8FC9-782BCBD7746B', 'customer', '7000.00');
+
+update t_receivables set data_org = '01010001' ;
 
 TRUNCATE TABLE `t_receivables_detail`;
 INSERT INTO `t_receivables_detail` (`id`, `act_money`, `balance_money`, `ca_id`, `ca_type`, `biz_date`, `date_created`, `ref_number`, `ref_type`, `rv_money`) VALUES
 ('04D71E39-B812-11E4-8FC9-782BCBD7746B', '0.00', '7000.00', '04B53C5E-B812-11E4-8FC9-782BCBD7746B', 'customer', '2015-01-01 00:00:00', '2015-02-19 16:33:45', '04B53C5E-B812-11E4-8FC9-782BCBD7746B', '应收账款期初建账', '7000.00');
 
-TRUNCATE TABLE `t_receiving`;
+update t_receivables_detail set data_org = '01010001' ;
+
 TRUNCATE TABLE `t_recent_fid`;
 INSERT INTO `t_recent_fid` (`fid`, `user_id`, `click_count`) VALUES
 ('-8999', '6C2A09CD-A129-11E4-9B6A-782BCBD7746B', 4),
@@ -291,78 +121,6 @@ INSERT INTO `t_recent_fid` (`fid`, `user_id`, `click_count`) VALUES
 ('2000', '6C2A09CD-A129-11E4-9B6A-782BCBD7746B', 1),
 ('2009', '6C2A09CD-A129-11E4-9B6A-782BCBD7746B', 1);
 
-TRUNCATE TABLE `t_role`;
-INSERT INTO `t_role` (`id`, `name`) VALUES
-('13A411EE-B3E8-11E4-A1A5-782BCBD7746B', '采购经理'),
-('38C5705B-B3E8-11E4-A1A5-782BCBD7746B', '销售经理'),
-('85E8B5C2-B3E8-11E4-A1A5-782BCBD7746B', '仓储经理'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '系统管理'),
-('E796C447-B3E8-11E4-A1A5-782BCBD7746B', '财务经理');
-
-TRUNCATE TABLE `t_role_permission`;
-INSERT INTO `t_role_permission` (`role_id`, `permission_id`) VALUES
-('38C5705B-B3E8-11E4-A1A5-782BCBD7746B', '2002'),
-('38C5705B-B3E8-11E4-A1A5-782BCBD7746B', '2002-01'),
-('13A411EE-B3E8-11E4-A1A5-782BCBD7746B', '1004'),
-('13A411EE-B3E8-11E4-A1A5-782BCBD7746B', '2001'),
-('85E8B5C2-B3E8-11E4-A1A5-782BCBD7746B', '1003'),
-('85E8B5C2-B3E8-11E4-A1A5-782BCBD7746B', '2000'),
-('85E8B5C2-B3E8-11E4-A1A5-782BCBD7746B', '2003'),
-('E796C447-B3E8-11E4-A1A5-782BCBD7746B', '2005'),
-('E796C447-B3E8-11E4-A1A5-782BCBD7746B', '2004'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '-8997'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2008'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '1003'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '1004'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '1001'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '1002'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '1007'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2000'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2003'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2005'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2004'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '-8996'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '-8999'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2001'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2002'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2002-01'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2006'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2007'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2009'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2010'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2011-01'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2011-02'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2011-03'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2011-04'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2012'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2013'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2014'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2015'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2016'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2017'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2018'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2019'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2020'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2021'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2022'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2023'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2024'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2025'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2026'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2027'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2027-01'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '2027-02');
-
-TRUNCATE TABLE `t_role_user`;
-INSERT INTO `t_role_user` (`role_id`, `user_id`) VALUES
-('38C5705B-B3E8-11E4-A1A5-782BCBD7746B', '073089A4-B3E6-11E4-AC63-782BCBD7746B'),
-('13A411EE-B3E8-11E4-A1A5-782BCBD7746B', 'F90E470B-B3E5-11E4-AC63-782BCBD7746B'),
-('85E8B5C2-B3E8-11E4-A1A5-782BCBD7746B', '70E2D355-B3E6-11E4-AC63-782BCBD7746B'),
-('E796C447-B3E8-11E4-A1A5-782BCBD7746B', 'CCF6A105-B3E8-11E4-A1A5-782BCBD7746B'),
-('A83F617E-A153-11E4-A9B8-782BCBD7746B', '6C2A09CD-A129-11E4-9B6A-782BCBD7746B');
-
-TRUNCATE TABLE `t_sr_bill`;
-TRUNCATE TABLE `t_sr_bill_detail`;
 TRUNCATE TABLE `t_supplier`;
 INSERT INTO `t_supplier` (`id`, `category_id`, `code`, `name`, `contact01`, `qq01`, `tel01`, `mobile01`, `contact02`, `qq02`, `tel02`, `mobile02`, `py`, `init_receivables`, `init_receivables_dt`, `init_payables`, `init_payables_dt`) VALUES
 ('01F98E5A-B811-11E4-8FC9-782BCBD7746B', '602BA712-B80F-11E4-8FC9-782BCBD7746B', '220', '大连华鑫船舶物资有限公司', '', '', '', '', '', '', '', '', 'DLHXCBWZYXGS', NULL, NULL, NULL, NULL),
@@ -392,31 +150,21 @@ INSERT INTO `t_supplier` (`id`, `category_id`, `code`, `name`, `contact01`, `qq0
 ('F5AA127C-B810-11E4-8FC9-782BCBD7746B', '602BA712-B80F-11E4-8FC9-782BCBD7746B', '219', '大连海珍尚品海洋产品商贸有限公司', '', '', '', '', '', '', '', '', 'DLHZSPHYCPSMYXGS', NULL, NULL, NULL, NULL),
 ('FE7BCC92-B80F-11E4-8FC9-782BCBD7746B', '602BA712-B80F-11E4-8FC9-782BCBD7746B', '207', '大连彤加贸易有限公司', '', '', '', '', '', '', '', '', 'DLTJMYYXGS', NULL, NULL, NULL, NULL);
 
+update t_supplier set data_org = '01010001' ;
+
 TRUNCATE TABLE `t_supplier_category`;
 INSERT INTO `t_supplier_category` (`id`, `code`, `name`) VALUES
 ('599B7CF5-B80F-11E4-8FC9-782BCBD7746B', '1', '电商'),
 ('602BA712-B80F-11E4-8FC9-782BCBD7746B', '2', '本地供应商');
 
-TRUNCATE TABLE `t_user`;
-INSERT INTO `t_user` (`id`, `enabled`, `login_name`, `name`, `org_id`, `org_code`, `password`, `py`, `gender`, `birthday`, `id_card_number`, `tel`, `tel02`, `address`) VALUES
-('073089A4-B3E6-11E4-AC63-782BCBD7746B', 1, 'lijingbo', '李静波', '37500493-B3E5-11E4-AC63-782BCBD7746B', '010301', 'e10adc3949ba59abbe56e057f20f883e', 'LJB', NULL, NULL, NULL, NULL, NULL, NULL),
-('54E6592A-B3E6-11E4-AC63-782BCBD7746B', 1, 'xueweixian', '薛伟先', '527369D1-B3E5-11E4-AC63-782BCBD7746B', '010101', 'e10adc3949ba59abbe56e057f20f883e', 'XWX', NULL, NULL, NULL, NULL, NULL, NULL),
-('6C2A09CD-A129-11E4-9B6A-782BCBD7746B', 1, 'admin', '系统管理员', '5EBDBE11-A129-11E4-9B6A-782BCBD7746B', '019901', '21232f297a57a5a743894a0e4a801fc3', 'XTGLY', NULL, NULL, NULL, NULL, NULL, NULL),
-('70E2D355-B3E6-11E4-AC63-782BCBD7746B', 1, 'zhouwenshuang', '周文双', '281F55E0-B3E6-11E4-AC63-782BCBD7746B', '010401', 'e10adc3949ba59abbe56e057f20f883e', 'ZWS', NULL, NULL, NULL, NULL, NULL, NULL),
-('CCF6A105-B3E8-11E4-A1A5-782BCBD7746B', 1, 'zhaoxue', '赵雪', '92FAB90F-B3E8-11E4-A1A5-782BCBD7746B', '010501', 'e10adc3949ba59abbe56e057f20f883e', 'ZX', NULL, NULL, NULL, NULL, NULL, NULL),
-('F90E470B-B3E5-11E4-AC63-782BCBD7746B', 1, 'zhangxiaodong', '张晓东', '2D496F44-B3E5-11E4-AC63-782BCBD7746B', '010201', 'e10adc3949ba59abbe56e057f20f883e', 'ZXD', NULL, NULL, NULL, NULL, NULL, NULL);
+update t_supplier_category set data_org = '01010001' ;
 
 TRUNCATE TABLE `t_warehouse`;
 INSERT INTO `t_warehouse` (`id`, `code`, `inited`, `name`, `py`) VALUES
-('17A72FFA-B3F3-11E4-9DEA-782BCBD7746B', '1', 0, '城区中心库房', 'CQZXKF');
+('17A72FFA-B3F3-11E4-9DEA-782BCBD7746B', '1', 1, '城区中心库房', 'CQZXKF');
 
-TRUNCATE TABLE `t_warehouse_org`;
-TRUNCATE TABLE `t_ws_bill`;
-TRUNCATE TABLE `t_ws_bill_detail`;
+update t_warehouse set data_org = '01010001' ;
 
-TRUNCATE TABLE `t_psi_db_version`;
-INSERT INTO `t_psi_db_version` (`db_version`, `update_dt`) VALUES
-('20151106-002', now());
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
