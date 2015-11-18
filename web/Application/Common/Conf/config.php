83cbe6e00e936caf346312e7d7a73eb4d9daa14e
CRM8000 <crm8000@qq.com>
2015-11-18 11:24:29 +0800

    代码微调

--- web/Application/Common/Conf/config.php
+++ web/Application/Common/Conf/config.php
@@ -1,11 +1,59 @@
 <?php
+
+function PSI_getHost() {
+	// MoPaaS V1
+	$host = getenv("MOPAAS_MYSQL22118_HOST");
+	if ($host) {
+		return $host;
+	}
+	
+	return "localhost";
+}
+
+function PSI_getDBName() {
+	$name = getenv("MOPAAS_MYSQL22118_NAME");
+	if ($name) {
+		return $name;
+	}
+	
+	return "psi";
+}
+
+function PSI_getUser() {
+	$user = getenv("MOPAAS_MYSQL22118_USER");
+	if ($user) {
+		return $user;
+	}
+	
+	return "root";
+}
+
+function PSI_getPassword() {
+	$password = getenv("MOPAAS_MYSQL22118_PASSWORD");
+	if ($password) {
+		return $password;
+	}
+	
+	return "";
+}
+
+function PSI_getPort() {
+	$port = getenv("MOPAAS_MYSQL22118_PORT");
+	if ($port) {
+		return $port;
+	}
+	
+	return 3306;
+}
+
 return array(
 		'URL_CASE_INSENSITIVE' => false,
 		'SHOW_ERROR_MSG' => true,
 		'DB_TYPE' => 'mysql', // 数据库类型
-		'DB_HOST' => getenv("MOPAAS_MYSQL22118_HOST") ? getenv("MOPAAS_MYSQL22118_HOST") : 'localhost', // 服务器地址
-		'DB_NAME' => getenv("MOPAAS_MYSQL22118_NAME") ? getenv("MOPAAS_MYSQL22118_NAME") : 'psi', // 数据库名
-		'DB_USER' => getenv("MOPAAS_MYSQL22118_USER") ? getenv("MOPAAS_MYSQL22118_USER") : 'root', // 用户名
-		'DB_PWD' => getenv("MOPAAS_MYSQL22118_PASSWORD") ? getenv("MOPAAS_MYSQL22118_PASSWORD") : '', // 密码
-		'DB_PORT' => getenv("MOPAAS_MYSQL22118_PORT") ? getenv("MOPAAS_MYSQL22118_PORT") : 3306 // 端口
-);
+		'DB_HOST' => PSI_getHost(), // 服务器地址
+		'DB_NAME' => PSI_getDBName(), // 数据库名
+		'DB_USER' => PSI_getUser(), // 用户名
+		'DB_PWD' => PSI_getPassword(), // 密码
+		'DB_PORT' => PSI_getPort()
+); // 端口
+
