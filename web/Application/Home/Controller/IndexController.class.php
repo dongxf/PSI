dae04961533360c62e95bd5a01c740f1332aa69b
CRM8000 <crm8000@qq.com>
2015-11-18 20:28:27 +0800

    代码微调

--- web/Application/Home/Controller/IndexController.class.php
+++ web/Application/Home/Controller/IndexController.class.php
@@ -9,11 +9,11 @@ use Home\Service\BizConfigService;
 
 /**
  * 首页Controller
- * 
+ *
  * @author 李静波
  *        
  */
-class IndexController extends Controller {
+class IndexController extends PSIBaseController {
 
 	/**
 	 * 首页
@@ -22,20 +22,15 @@ class IndexController extends Controller {
 		$us = new UserService();
 		
 		if ($us->hasPermission()) {
-			$bcs = new BizConfigService();
-			$this->assign("productionName", $bcs->getProductionName());
-
-			$this->assign("title", "首页");
-			$this->assign("uri", __ROOT__ . "/");
+			$this->initVar();
 			
-			$this->assign("loginUserName", $us->getLoignUserNameWithOrgFullName());
-			$dtFlag = getdate();
-			$this->assign("dtFlag", $dtFlag[0]);
-			$this->assign("pSale", $us->hasPermission(FIdConst::PORTAL_SALE) ? 1: 0);
-			$this->assign("pInventory", $us->hasPermission(FIdConst::PORTAL_INVENTORY) ? 1: 0);
-			$this->assign("pPurchase", $us->hasPermission(FIdConst::PORTAL_PURCHASE) ? 1: 0);
-			$this->assign("pMoney", $us->hasPermission(FIdConst::PORTAL_MONEY) ? 1: 0);
-				
+			$this->assign("title", "首页");
+			
+			$this->assign("pSale", $us->hasPermission(FIdConst::PORTAL_SALE) ? 1 : 0);
+			$this->assign("pInventory", $us->hasPermission(FIdConst::PORTAL_INVENTORY) ? 1 : 0);
+			$this->assign("pPurchase", $us->hasPermission(FIdConst::PORTAL_PURCHASE) ? 1 : 0);
+			$this->assign("pMoney", $us->hasPermission(FIdConst::PORTAL_MONEY) ? 1 : 0);
+			
 			$this->display();
 		} else {
 			redirect(__ROOT__ . "/Home/User/login");
