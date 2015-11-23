a5ea923bfb43937cb10001ce472b31b7ab8247e1
CRM8000 <crm8000@qq.com>
2015-11-23 15:33:56 +0800

    数据库文档更新：t_ws_bill 新增字段 company_id

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -411,6 +411,7 @@ CREATE TABLE IF NOT EXISTS `t_ws_bill` (
   `warehouse_id` varchar(255) NOT NULL,
   `receiving_type` int(11) NOT NULL DEFAULT 0,
   `data_org` varchar(255) DEFAULT NULL,
+  `company_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
