f4be470442141f34da48a7215d95735f445df235
CRM8000 <crm8000@qq.com>
2015-11-23 15:38:10 +0800

    一键更新数据库：t_ws_bill新增字段company_id

--- web/Application/Home/Service/UpdateDBService.class.php
+++ web/Application/Home/Service/UpdateDBService.class.php
@@ -8,7 +8,7 @@ namespace Home\Service;
  * @author 李静波
  */
 class UpdateDBService extends PSIBaseService {
-	private $CURRENT_DB_VERSION = "20151123-001";
+	private $CURRENT_DB_VERSION = "20151123-002";
 
 	private function tableExists($db, $tableName) {
 		$dbName = C('DB_NAME');
@@ -96,6 +96,7 @@ class UpdateDBService extends PSIBaseService {
 		$this->update_20151119_03($db);
 		$this->update_20151121_01($db);
 		$this->update_20151123_01($db);
+		$this->update_20151123_02($db);
 		
 		$sql = "delete from t_psi_db_version";
 		$db->execute($sql);
@@ -107,6 +108,17 @@ class UpdateDBService extends PSIBaseService {
 		$bl->insertBizlog("升级数据库，数据库版本 = " . $this->CURRENT_DB_VERSION);
 		
 		return $this->ok();
+	}
+
+	private function update_20151123_02($db) {
+		// 本次更新：销售出库单新增company_id字段
+		$tableName = "t_ws_bill";
+		$columnName = "company_id";
+		
+		if (! $this->columnExists($db, $tableName, $columnName)) {
+			$sql = "alter table {$tableName} add {$columnName} varchar(255) default null;";
+			$db->execute($sql);
+		}
 	}
 
 	private function update_20151123_01($db) {
