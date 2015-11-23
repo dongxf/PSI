d89c675f4f9168a3963cabc658a05c3604540456
CRM8000 <crm8000@qq.com>
2015-11-23 13:06:28 +0800

    销售出库单，bug修正

--- web/Application/Home/Service/WSBillService.class.php
+++ web/Application/Home/Service/WSBillService.class.php
@@ -28,27 +28,19 @@ class WSBillService extends PSIBaseService {
 			$result["bizUserName"] = $us->getLoginUserName();
 			
 			$ts = new BizConfigService();
-			if ($ts->warehouseUsesOrg()) {
-				$ws = new WarehouseService();
-				$data = $ws->getWarehouseListForLoginUser("2002");
-				if (count($data) > 0) {
+			$db = M();
+			$sql = "select value from t_config where id = '2002-02' ";
+			$data = $db->query($sql);
+			if ($data) {
+				$warehouseId = $data[0]["value"];
+				$sql = "select id, name from t_warehouse where id = '%s' ";
+				$data = $db->query($sql, $warehouseId);
+				if ($data) {
 					$result["warehouseId"] = $data[0]["id"];
 					$result["warehouseName"] = $data[0]["name"];
 				}
-			} else {
-				$db = M();
-				$sql = "select value from t_config where id = '2002-02' ";
-				$data = $db->query($sql);
-				if ($data) {
-					$warehouseId = $data[0]["value"];
-					$sql = "select id, name from t_warehouse where id = '%s' ";
-					$data = $db->query($sql, $warehouseId);
-					if ($data) {
-						$result["warehouseId"] = $data[0]["id"];
-						$result["warehouseName"] = $data[0]["name"];
-					}
-				}
 			}
+			
 			return $result;
 		} else {
 			// 编辑
