d7732d7f090310ebb1cf59ae60d82084fcf02a98
CRM8000 <crm8000@qq.com>
2015-11-25 15:04:08 +0800

    商品分类 - 代码微调

--- web/Application/Home/Service/GoodsService.class.php
+++ web/Application/Home/Service/GoodsService.class.php
@@ -181,36 +181,68 @@ class GoodsService extends PSIBaseService {
 			$result[$i]["leaf"] = count($children) == 0;
 			$result[$i]["expanded"] = true;
 			
-			$sql = "select count(*) as cnt
-					from t_goods c
-					where c.category_id = '%s' ";
-			$queryParam = array();
-			$queryParam[] = $id;
-			if ($rs) {
-				$sql .= " and " . $rs[0];
-				$queryParam = array_merge($queryParam, $rs[1]);
-			}
-			if ($code) {
-				$sql .= " and (c.code like '%s') ";
-				$queryParam[] = "%{$code}%";
-			}
-			if ($name) {
-				$sql .= " and (c.name like '%s' or c.py like '%s') ";
-				$queryParam[] = "%{$name}%";
-				$queryParam[] = "%{$name}%";
-			}
-			if ($spec) {
-				$sql .= " and (c.spec like '%s')";
-				$queryParam[] = "%{$spec}%";
-			}
-			if ($barCode) {
-				$sql .= " and (c.bar_code = '%s') ";
-				$queryParam[] = $barCode;
-			}
-			$data = $db->query($sql, $queryParam);
-			$result[$i]["cnt"] = $data[0]["cnt"];
+			$result[$i]["cnt"] = $this->getGoodsCountWithAllSub($db, $id, $params, $rs);
 		}
 		
+		return $result;
+	}
+
+	/**
+	 * 获得某个商品分类及其所属子分类下的所有商品的种类数
+	 */
+	private function getGoodsCountWithAllSub($db, $categoryId, $params, $rs) {
+		$code = $params["code"];
+		$name = $params["name"];
+		$spec = $params["spec"];
+		$barCode = $params["barCode"];
+		
+		$sql = "select count(*) as cnt 
+					from t_goods c
+					where c.category_id = '%s' ";
+		$queryParam = array();
+		$queryParam[] = $categoryId;
+		if ($rs) {
+			$sql .= " and " . $rs[0];
+			$queryParam = array_merge($queryParam, $rs[1]);
+		}
+		if ($code) {
+			$sql .= " and (c.code like '%s') ";
+			$queryParam[] = "%{$code}%";
+		}
+		if ($name) {
+			$sql .= " and (c.name like '%s' or c.py like '%s') ";
+			$queryParam[] = "%{$name}%";
+			$queryParam[] = "%{$name}%";
+		}
+		if ($spec) {
+			$sql .= " and (c.spec like '%s')";
+			$queryParam[] = "%{$spec}%";
+		}
+		if ($barCode) {
+			$sql .= " and (c.bar_code = '%s') ";
+			$queryParam[] = $barCode;
+		}
+		
+		$data = $db->query($sql, $queryParam);
+		$result = $data[0]["cnt"];
+		
+		// 子分类
+		$sql = "select id
+				from t_goods_category c
+				where (parent_id = '%s')
+				";
+		$queryParam = array();
+		$queryParam[] = $categoryId;
+		if ($rs) {
+			$sql .= " and " . $rs[0];
+			$queryParam = array_merge($queryParam, $rs[1]);
+		}
+		
+		$data = $db->query($sql, $queryParam);
+		foreach ( $data as $v ) {
+			// 递归调用自身
+			$result += $this->getGoodsCountWithAllSub($db, $v["id"], $params, $rs);
+		}
 		return $result;
 	}
 
@@ -261,34 +293,7 @@ class GoodsService extends PSIBaseService {
 			$result[$i]["leaf"] = count($children) == 0;
 			$result[$i]["expanded"] = true;
 			
-			$sql = "select count(*) as cnt 
-					from t_goods c
-					where c.category_id = '%s' ";
-			$queryParam = array();
-			$queryParam[] = $id;
-			if ($rs) {
-				$sql .= " and " . $rs[0];
-				$queryParam = array_merge($queryParam, $rs[1]);
-			}
-			if ($code) {
-				$sql .= " and (c.code like '%s') ";
-				$queryParam[] = "%{$code}%";
-			}
-			if ($name) {
-				$sql .= " and (c.name like '%s' or c.py like '%s') ";
-				$queryParam[] = "%{$name}%";
-				$queryParam[] = "%{$name}%";
-			}
-			if ($spec) {
-				$sql .= " and (c.spec like '%s')";
-				$queryParam[] = "%{$spec}%";
-			}
-			if ($barCode) {
-				$sql .= " and (c.bar_code = '%s') ";
-				$queryParam[] = $barCode;
-			}
-			$data = $db->query($sql, $queryParam);
-			$result[$i]["cnt"] = $data[0]["cnt"];
+			$result[$i]["cnt"] = $this->getGoodsCountWithAllSub($db, $id, $params, $rs);
 		}
 		
 		return $result;
