3c0d4727e53ee2d0d14ebf6a91ca83e9b2c4ed17
CRM8000 <crm8000@qq.com>
2015-11-26 10:05:17 +0800

    一键升级数据库：销售出库单新增备注

--- web/Application/Home/Service/UpdateDBService.class.php
+++ web/Application/Home/Service/UpdateDBService.class.php
@@ -8,7 +8,7 @@ namespace Home\Service;
  * @author 李静波
  */
 class UpdateDBService extends PSIBaseService {
-	private $CURRENT_DB_VERSION = "20151124-001";
+	private $CURRENT_DB_VERSION = "20151126-001";
 
 	private function tableExists($db, $tableName) {
 		$dbName = C('DB_NAME');
@@ -99,6 +99,7 @@ class UpdateDBService extends PSIBaseService {
 		$this->update_20151123_02($db);
 		$this->update_20151123_03($db);
 		$this->update_20151124_01($db);
+		$this->update_20151126_01($db);
 		
 		$sql = "delete from t_psi_db_version";
 		$db->execute($sql);
@@ -110,6 +111,25 @@ class UpdateDBService extends PSIBaseService {
 		$bl->insertBizlog("升级数据库，数据库版本 = " . $this->CURRENT_DB_VERSION);
 		
 		return $this->ok();
+	}
+
+	private function update_20151126_01($db) {
+		// 本次更新：销售出库单新增备注字段
+		$tableName = "t_ws_bill";
+		$columnName = "memo";
+		
+		if (! $this->columnExists($db, $tableName, $columnName)) {
+			$sql = "alter table {$tableName} add {$columnName} varchar(1000) default null;";
+			$db->execute($sql);
+		}
+		
+		$tableName = "t_ws_bill_detail";
+		$columnName = "memo";
+		
+		if (! $this->columnExists($db, $tableName, $columnName)) {
+			$sql = "alter table {$tableName} add {$columnName} varchar(1000) default null;";
+			$db->execute($sql);
+		}
 	}
 
 	private function update_20151124_01($db) {
