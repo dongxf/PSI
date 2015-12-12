9b359402cd80ceb772cd98fbc62b9b101407cb09
CRM8000 <crm8000@qq.com>
2015-12-12 21:08:17 +0800

    代码微调

--- web/Public/Scripts/PSI/App.js
+++ web/Public/Scripts/PSI/App.js
@@ -1,235 +1,258 @@
-//PSI的应用容器：承载主菜单、其他模块的UI
+// PSI的应用容器：承载主菜单、其他模块的UI
 Ext.define("PSI.App", {
-    config: {
-        userName: "",
-        productionName: "开源进销存PSI"
-    },
-    
-    constructor: function (config) {
-        this.initConfig(config);
+	config : {
+		userName : "",
+		productionName : "开源进销存PSI"
+	},
 
-        this.createMainUI();
-    },
-    
-    createMainUI: function () {
-        var me = this;
+	constructor : function(config) {
+		this.initConfig(config);
 
-        me.mainPanel = Ext.create("Ext.panel.Panel", {
-            border: 0,
-            layout: "fit"
-        });
+		this.createMainUI();
+	},
 
-        Ext.define("PSIFId", {
-            extend: "Ext.data.Model",
-            fields: ["fid", "name"]
-        });
+	createMainUI : function() {
+		var me = this;
 
-        var storeRecentFid = Ext.create("Ext.data.Store", {
-            autoLoad: false,
-            model: "PSIFId",
-            data: []
-        });
+		me.mainPanel = Ext.create("Ext.panel.Panel", {
+					border : 0,
+					layout : "fit"
+				});
 
-        me.gridRecentFid = Ext.create("Ext.grid.Panel", {
-            title: "常用功能",
-            forceFit: true,
-            hideHeaders: true,
-            columns: [
-                {
-                    dataIndex: "name", menuDisabled: true, menuDisabled: true, sortable: false,
-                    renderer: function (value, metaData, record) {
-                        var fid = record.get("fid");
-                        var fileName = PSI.Const.BASE_URL + "Public/Images/fid/fid" + fid + ".png";
-                        return "<img src='" + fileName + "'><a href='#' style='text-decoration:none'>" + value + "</a></img>";
-                    }
-                }
-            ],
-            store: storeRecentFid
-        });
+		Ext.define("PSIFId", {
+					extend : "Ext.data.Model",
+					fields : ["fid", "name"]
+				});
 
-        me.gridRecentFid.on("itemclick", function (v, r) {
-            var fid = r.get("fid");
+		var storeRecentFid = Ext.create("Ext.data.Store", {
+					autoLoad : false,
+					model : "PSIFId",
+					data : []
+				});
 
-            if (fid === "-9999") {
-                PSI.MsgBox.confirm("请确认是否重新登录", function () {
-                    location.replace(PSI.Const.BASE_URL + "Home/MainMenu/navigateTo/fid/-9999");
-                });
-            } else {
-                location.replace(PSI.Const.BASE_URL + "Home/MainMenu/navigateTo/fid/" + fid);
-            }
-        }, me);
+		me.gridRecentFid = Ext.create("Ext.grid.Panel", {
+					title : "常用功能",
+					forceFit : true,
+					hideHeaders : true,
+					columns : [{
+						dataIndex : "name",
+						menuDisabled : true,
+						menuDisabled : true,
+						sortable : false,
+						renderer : function(value, metaData, record) {
+							var fid = record.get("fid");
+							var fileName = PSI.Const.BASE_URL
+									+ "Public/Images/fid/fid" + fid + ".png";
+							return "<img src='"
+									+ fileName
+									+ "'><a href='#' style='text-decoration:none'>"
+									+ value + "</a></img>";
+						}
+					}],
+					store : storeRecentFid
+				});
 
-        me.vp = Ext.create("Ext.container.Viewport", {
-            layout: "fit",
-            items: [{
-                    id: "__PSITopPanel",
-                    xtype: "panel",
-                    border: 0,
-                    layout: "border",
-                    bbar: ["当前用户：<span style='color:red'>" + me.getUserName() + "</span>"],
-                    items: [{
-                            region: "center", layout: "fit", xtype: "panel",
-                            items: [me.mainPanel]
-                        }, {
-                            xtype: "panel",
-                            region: "east",
-                            width: 250,
-                            maxWidth: 250,
-                            split: true,
-                            collapsible: true,
-                            collapsed: me.getRecentFidPanelCollapsed(),
-                            header: false,
-                            layout: "fit",
-                            items: [ me.gridRecentFid ],
-                            listeners: {
-                                collapse: {
-                                    fn: me.onRecentFidPanelCollapse,
-                                    scope: me
-                                },
-                                expand: {
-                                    fn: me.onRecentFidPanelExpand,
-                                    scope: me
-                                }
-                            }
-                        }
-                    ]
-                }]
-        });
+		me.gridRecentFid.on("itemclick", function(v, r) {
+					var fid = r.get("fid");
 
-        var el = Ext.getBody();
-        el.mask("系统正在加载中...");
+					if (fid === "-9999") {
+						PSI.MsgBox.confirm("请确认是否重新登录", function() {
+									location
+											.replace(PSI.Const.BASE_URL
+													+ "Home/MainMenu/navigateTo/fid/-9999");
+								});
+					} else {
+						location.replace(PSI.Const.BASE_URL
+								+ "Home/MainMenu/navigateTo/fid/" + fid);
+					}
+				}, me);
 
-        Ext.Ajax.request({
-            url: PSI.Const.BASE_URL + "Home/MainMenu/mainMenuItems",
-            method: "POST",
-            callback: function (opt, success, response) {
-                if (success) {
-                    var data = Ext.JSON.decode(response.responseText);
-                    me.createMainMenu(data);
-                    me.refreshRectFidGrid();
-                }
+		me.vp = Ext.create("Ext.container.Viewport", {
+					layout : "fit",
+					items : [{
+						id : "__PSITopPanel",
+						xtype : "panel",
+						border : 0,
+						layout : "border",
+						bbar : ["当前用户：<span style='color:red'>"
+								+ me.getUserName() + "</span>"],
+						items : [{
+									region : "center",
+									layout : "fit",
+									xtype : "panel",
+									items : [me.mainPanel]
+								}, {
+									xtype : "panel",
+									region : "east",
+									width : 250,
+									maxWidth : 250,
+									split : true,
+									collapsible : true,
+									collapsed : me.getRecentFidPanelCollapsed(),
+									header : false,
+									layout : "fit",
+									items : [me.gridRecentFid],
+									listeners : {
+										collapse : {
+											fn : me.onRecentFidPanelCollapse,
+											scope : me
+										},
+										expand : {
+											fn : me.onRecentFidPanelExpand,
+											scope : me
+										}
+									}
+								}]
+					}]
+				});
 
-                el.unmask();
-            },
-            scope: me
-        });
-    },
-    
-    refreshRectFidGrid: function () {
-        var me = this;
+		var el = Ext.getBody();
+		el.mask("系统正在加载中...");
 
-        var el = me.gridRecentFid.getEl() || Ext.getBody();
-        el.mask("系统正在加载中...");
-        var store = me.gridRecentFid.getStore();
-        store.removeAll();
+		Ext.Ajax.request({
+					url : PSI.Const.BASE_URL + "Home/MainMenu/mainMenuItems",
+					method : "POST",
+					callback : function(opt, success, response) {
+						if (success) {
+							var data = Ext.JSON.decode(response.responseText);
+							me.createMainMenu(data);
+							me.refreshRectFidGrid();
+						}
 
-        Ext.Ajax.request({
-            url: PSI.Const.BASE_URL + "Home/MainMenu/recentFid",
-            method: "POST",
-            callback: function (opt, success, response) {
-                if (success) {
-                    var data = Ext.JSON.decode(response.responseText);
-                    store.add(data);
-                }
-                el.unmask();
-            },
-            scope: me
-        });
-    },
-    
-    createMainMenu: function (root) {
-        var me = this;
+						el.unmask();
+					},
+					scope : me
+				});
+	},
 
-        var menuItemClick = function () {
-            var fid = this.fid;
-            
-            if (fid == "-9995") {
-                window.open("http://my.oschina.net/u/134395/blog/374195");
-            } else if (fid == "-9994"){
-                var form = Ext.create("PSI.About.MainForm", {
-                	productionName: me.getProductionName()
-                });
-                form.show();
-            } else if (fid == "-9993") {
-            	window.open("https://zb.oschina.net/market/opus/134395_122");
-            } else if (fid === "-9999") {
-                PSI.MsgBox.confirm("请确认是否重新登录", function () {
-                    location.replace(PSI.Const.BASE_URL + "Home/MainMenu/navigateTo/fid/-9999");
-                });
-            } else {
-                location.replace(PSI.Const.BASE_URL + "Home/MainMenu/navigateTo/fid/" + fid);
-            }
-        };
+	refreshRectFidGrid : function() {
+		var me = this;
 
-        var mainMenu = [];
-        for (var i = 0; i < root.length; i++) {
-            var m1 = root[i];
+		var el = me.gridRecentFid.getEl() || Ext.getBody();
+		el.mask("系统正在加载中...");
+		var store = me.gridRecentFid.getStore();
+		store.removeAll();
 
-            var menuItem = Ext.create("Ext.menu.Menu");
-            for (var j = 0; j < m1.children.length; j++) {
-                var m2 = m1.children[j];
+		Ext.Ajax.request({
+					url : PSI.Const.BASE_URL + "Home/MainMenu/recentFid",
+					method : "POST",
+					callback : function(opt, success, response) {
+						if (success) {
+							var data = Ext.JSON.decode(response.responseText);
+							store.add(data);
+						}
+						el.unmask();
+					},
+					scope : me
+				});
+	},
 
-                if (m2.children.length === 0) {
-                    // 只有二级菜单
-                	if (m2.fid) {
-                        menuItem.add({
-                            text: m2.caption, fid: m2.fid, handler: menuItemClick,
-                            iconCls: "PSI-fid" + m2.fid
-                        });
-                	}
-                } else {
-                    var menuItem2 = Ext.create("Ext.menu.Menu");
+	createMainMenu : function(root) {
+		var me = this;
 
-                    menuItem.add({text: m2.caption, menu: menuItem2});
+		var menuItemClick = function() {
+			var fid = this.fid;
 
-                    // 三级菜单
-                    for (var k = 0; k < m2.children.length; k++) {
-                        var m3 = m2.children[k];
-                        menuItem2.add({
-                            text: m3.caption, fid: m3.fid, handler: menuItemClick,
-                            iconCls: "PSI-fid" + m3.fid
-                        });
-                    }
-                }
-            }
+			if (fid == "-9995") {
+				window.open("http://my.oschina.net/u/134395/blog/374195");
+			} else if (fid == "-9994") {
+				var form = Ext.create("PSI.About.MainForm", {
+							productionName : me.getProductionName()
+						});
+				form.show();
+			} else if (fid == "-9993") {
+				window.open("https://zb.oschina.net/market/opus/134395_122");
+			} else if (fid === "-9999") {
+				PSI.MsgBox.confirm("请确认是否重新登录", function() {
+							location.replace(PSI.Const.BASE_URL
+									+ "Home/MainMenu/navigateTo/fid/-9999");
+						});
+			} else {
+				location.replace(PSI.Const.BASE_URL
+						+ "Home/MainMenu/navigateTo/fid/" + fid);
+			}
+		};
 
-            if (m1.children.length > 0) {
-                mainMenu.push({text: m1.caption, menu: menuItem});
-            }
-        }
+		var mainMenu = [];
+		for (var i = 0; i < root.length; i++) {
+			var m1 = root[i];
 
-        var mainToolbar = Ext.create("Ext.toolbar.Toolbar", {
-            dock: "top"
-        });
-        mainToolbar.add(mainMenu);
+			var menuItem = Ext.create("Ext.menu.Menu");
+			for (var j = 0; j < m1.children.length; j++) {
+				var m2 = m1.children[j];
 
-        me.vp.getComponent(0).addDocked(mainToolbar);
-    },
-    
-    // 设置模块的标题
-    setAppHeader: function (header) {
-        if (!header) {
-            return;
-        }
-        var panel = Ext.getCmp("__PSITopPanel");
-        panel.setTitle(header.title + " - " + this.getProductionName());
-        panel.setIconCls(header.iconCls);
-    },
-    
-    add: function (comp) {
-        this.mainPanel.add(comp);
-    },
-    
-    onRecentFidPanelCollapse: function () {
-        Ext.util.Cookies.set("PSI_RECENT_FID", "1", Ext.Date.add(new Date(), Ext.Date.YEAR, 1));
-    },
-    
-    onRecentFidPanelExpand: function () {
-        Ext.util.Cookies.clear("PSI_RECENT_FID");
-    },
-    
-    getRecentFidPanelCollapsed: function () {
-        var v = Ext.util.Cookies.get("PSI_RECENT_FID");
-        return v === "1";
-    }
+				if (m2.children.length === 0) {
+					// 只有二级菜单
+					if (m2.fid) {
+						menuItem.add({
+									text : m2.caption,
+									fid : m2.fid,
+									handler : menuItemClick,
+									iconCls : "PSI-fid" + m2.fid
+								});
+					}
+				} else {
+					var menuItem2 = Ext.create("Ext.menu.Menu");
+
+					menuItem.add({
+								text : m2.caption,
+								menu : menuItem2
+							});
+
+					// 三级菜单
+					for (var k = 0; k < m2.children.length; k++) {
+						var m3 = m2.children[k];
+						menuItem2.add({
+									text : m3.caption,
+									fid : m3.fid,
+									handler : menuItemClick,
+									iconCls : "PSI-fid" + m3.fid
+								});
+					}
+				}
+			}
+
+			if (m1.children.length > 0) {
+				mainMenu.push({
+							text : m1.caption,
+							menu : menuItem
+						});
+			}
+		}
+
+		var mainToolbar = Ext.create("Ext.toolbar.Toolbar", {
+					dock : "top"
+				});
+		mainToolbar.add(mainMenu);
+
+		me.vp.getComponent(0).addDocked(mainToolbar);
+	},
+
+	// 设置模块的标题
+	setAppHeader : function(header) {
+		if (!header) {
+			return;
+		}
+		var panel = Ext.getCmp("__PSITopPanel");
+		panel.setTitle(header.title + " - " + this.getProductionName());
+		panel.setIconCls(header.iconCls);
+	},
+
+	add : function(comp) {
+		this.mainPanel.add(comp);
+	},
+
+	onRecentFidPanelCollapse : function() {
+		Ext.util.Cookies.set("PSI_RECENT_FID", "1", Ext.Date.add(new Date(),
+						Ext.Date.YEAR, 1));
+	},
+
+	onRecentFidPanelExpand : function() {
+		Ext.util.Cookies.clear("PSI_RECENT_FID");
+	},
+
+	getRecentFidPanelCollapsed : function() {
+		var v = Ext.util.Cookies.get("PSI_RECENT_FID");
+		return v === "1";
+	}
 });
--- web/Public/Scripts/PSI/Const.js
+++ web/Public/Scripts/PSI/Const.js
@@ -1,9 +1,9 @@
 Ext.define("PSI.Const", {
-    statics: {
-        REQUIRED: '<span style="color:red;font-weight:bold" data-qtip="必录项">*</span>',
-        LOADING: "数据加载中...",
-        SAVING: "数据保存中...",
-        BASE_URL: "",
-        VERSION: "PSI 2016 - build201512121613"
-    }
+	statics : {
+		REQUIRED : '<span style="color:red;font-weight:bold" data-qtip="必录项">*</span>',
+		LOADING : "数据加载中...",
+		SAVING : "数据保存中...",
+		BASE_URL : "",
+		VERSION : "PSI 2016 - build201512121613"
+	}
 });
