247cd25554e09c2dc7d1ffffe7538e5258878e91
CRM8000 <crm8000@qq.com>
2015-12-13 18:21:16 +0800

    代码微调

--- web/Public/Scripts/PSI/Bill/ITViewForm.js
+++ web/Public/Scripts/PSI/Bill/ITViewForm.js
@@ -1,162 +1,191 @@
-// 调拨单 - 查看界面
+/**
+ * 调拨单 - 查看界面
+ */
 Ext.define("PSI.Bill.ITViewForm", {
-    extend: "Ext.window.Window",
-    
-    config: {
-        ref: null
-    },
-    
-    initComponent: function () {
-        var me = this;
+			extend : "Ext.window.Window",
 
-        Ext.apply(me, {
-        	title: "查看调拨单",
-            modal: true,
-            onEsc: Ext.emptyFn,
-            maximized: true,
-			closable: false,
-            width: 1000,
-            height: 600,
-            layout: "border",
-            items: [{
-                    region: "center",
-                    border: 0,
-                    bodyPadding: 10,
-                    layout: "fit",
-                    items: [me.getGoodsGrid()]
-                },
-                {
-                    region: "north",
-                    border: 0,
-                    layout: {
-                        type: "table",
-                        columns: 2
-                    },
-                    height: 100,
-                    bodyPadding: 10,
-                    items: [
-                        {
-                            id: "editRef",
-                            fieldLabel: "单号",
-                            labelWidth: 60,
-                            labelAlign: "right",
-                            labelSeparator: ":",
-                            xtype: "displayfield",
-                            value: me.getRef()
-                        },
-                        {
-                            id: "editBizDT",
-                            fieldLabel: "业务日期",
-                            labelWidth: 60,
-                            labelAlign: "right",
-                            labelSeparator: ":",
-                            xtype: "displayfield"
-                        },
-                        {
-                            id: "editFromWarehouse",
-                            fieldLabel: "调出仓库",
-                            labelWidth: 60,
-                            labelAlign: "right",
-                            labelSeparator: ":",
-                            xtype: "displayfield"
-                        },
-                        {
-                            id: "editToWarehouse",
-                            fieldLabel: "调入仓库",
-                            labelWidth: 60,
-                            labelAlign: "right",
-                            labelSeparator: ":",
-                            xtype: "displayfield"
-                        },
-                        {
-                            id: "editBizUser",
-                            fieldLabel: "业务员",
-                            xtype: "displayfield",
-                            labelWidth: 60,
-                            labelAlign: "right",
-                            labelSeparator: ":"
-                        }
-                    ]
-                }],
-            listeners: {
-                show: {
-                    fn: me.onWndShow,
-                    scope: me
-                }
-            }
-        });
+			config : {
+				ref : null
+			},
 
-        me.callParent(arguments);
-    },
-    
-    onWndShow: function () {
-        var me = this;
-        var el = me.getEl() || Ext.getBody();
-        el.mask(PSI.Const.LOADING);
-        Ext.Ajax.request({
-            url: PSI.Const.BASE_URL + "Home/Bill/itBillInfo",
-            params: {
-                ref: me.getRef()
-            },
-            method: "POST",
-            callback: function (options, success, response) {
-                el.unmask();
+			initComponent : function() {
+				var me = this;
 
-                if (success) {
-                    var data = Ext.JSON.decode(response.responseText);
+				Ext.apply(me, {
+							title : "查看调拨单",
+							modal : true,
+							onEsc : Ext.emptyFn,
+							maximized : true,
+							closable : false,
+							width : 1000,
+							height : 600,
+							layout : "border",
+							items : [{
+										region : "center",
+										border : 0,
+										bodyPadding : 10,
+										layout : "fit",
+										items : [me.getGoodsGrid()]
+									}, {
+										region : "north",
+										border : 0,
+										layout : {
+											type : "table",
+											columns : 2
+										},
+										height : 100,
+										bodyPadding : 10,
+										items : [{
+													id : "editRef",
+													fieldLabel : "单号",
+													labelWidth : 60,
+													labelAlign : "right",
+													labelSeparator : ":",
+													xtype : "displayfield",
+													value : me.getRef()
+												}, {
+													id : "editBizDT",
+													fieldLabel : "业务日期",
+													labelWidth : 60,
+													labelAlign : "right",
+													labelSeparator : ":",
+													xtype : "displayfield"
+												}, {
+													id : "editFromWarehouse",
+													fieldLabel : "调出仓库",
+													labelWidth : 60,
+													labelAlign : "right",
+													labelSeparator : ":",
+													xtype : "displayfield"
+												}, {
+													id : "editToWarehouse",
+													fieldLabel : "调入仓库",
+													labelWidth : 60,
+													labelAlign : "right",
+													labelSeparator : ":",
+													xtype : "displayfield"
+												}, {
+													id : "editBizUser",
+													fieldLabel : "业务员",
+													xtype : "displayfield",
+													labelWidth : 60,
+													labelAlign : "right",
+													labelSeparator : ":"
+												}]
+									}],
+							listeners : {
+								show : {
+									fn : me.onWndShow,
+									scope : me
+								}
+							}
+						});
 
-                    Ext.getCmp("editBizUser").setValue(data.bizUserName);
-                    Ext.getCmp("editBizDT").setValue(data.bizDT);
-                	Ext.getCmp("editFromWarehouse").setValue(data.fromWarehouseName);
-                	Ext.getCmp("editToWarehouse").setValue(data.toWarehouseName);
+				me.callParent(arguments);
+			},
 
-                    var store = me.getGoodsGrid().getStore();
-                    store.removeAll();
-                    if (data.items) {
-                        store.add(data.items);
-                    }
-                } else {
-                    PSI.MsgBox.showInfo("网络错误")
-                }
-            }
-        });
-    },
-    
-    getGoodsGrid: function () {
-        var me = this;
-        if (me.__goodsGrid) {
-            return me.__goodsGrid;
-        }
-        var modelName = "PSIITBillDetail_ViewForm";
-        Ext.define(modelName, {
-            extend: "Ext.data.Model",
-            fields: ["id", "goodsId", "goodsCode", "goodsName", "goodsSpec", "unitName", "goodsCount"]
-        });
-        var store = Ext.create("Ext.data.Store", {
-            autoLoad: false,
-            model: modelName,
-            data: []
-        });
+			onWndShow : function() {
+				var me = this;
+				var el = me.getEl() || Ext.getBody();
+				el.mask(PSI.Const.LOADING);
+				Ext.Ajax.request({
+							url : PSI.Const.BASE_URL + "Home/Bill/itBillInfo",
+							params : {
+								ref : me.getRef()
+							},
+							method : "POST",
+							callback : function(options, success, response) {
+								el.unmask();
 
-        me.__goodsGrid = Ext.create("Ext.grid.Panel", {
-        	viewConfig: {
-                enableTextSelection: true
-            },
-            columnLines: true,
-            columns: [
-                Ext.create("Ext.grid.RowNumberer", {text: "序号", width: 30}),
-                {header: "商品编码", dataIndex: "goodsCode", menuDisabled: true,
-                    sortable: false, editor: {xtype: "psi_goodsfield", parentCmp: me}},
-                {header: "商品名称", dataIndex: "goodsName", menuDisabled: true, sortable: false, width: 200},
-                {header: "规格型号", dataIndex: "goodsSpec", menuDisabled: true, sortable: false, width: 200},
-                {header: "调拨数量", dataIndex: "goodsCount", menuDisabled: true,
-                    sortable: false, align: "right", width: 100
-                },
-                {header: "单位", dataIndex: "unitName", menuDisabled: true, sortable: false, width: 60}
-            ],
-            store: store
-        });
+								if (success) {
+									var data = Ext.JSON
+											.decode(response.responseText);
 
-        return me.__goodsGrid;
-    }
-});
+									Ext.getCmp("editBizUser")
+											.setValue(data.bizUserName);
+									Ext.getCmp("editBizDT")
+											.setValue(data.bizDT);
+									Ext.getCmp("editFromWarehouse")
+											.setValue(data.fromWarehouseName);
+									Ext.getCmp("editToWarehouse")
+											.setValue(data.toWarehouseName);
+
+									var store = me.getGoodsGrid().getStore();
+									store.removeAll();
+									if (data.items) {
+										store.add(data.items);
+									}
+								} else {
+									PSI.MsgBox.showInfo("网络错误")
+								}
+							}
+						});
+			},
+
+			getGoodsGrid : function() {
+				var me = this;
+				if (me.__goodsGrid) {
+					return me.__goodsGrid;
+				}
+				var modelName = "PSIITBillDetail_ViewForm";
+				Ext.define(modelName, {
+							extend : "Ext.data.Model",
+							fields : ["id", "goodsId", "goodsCode",
+									"goodsName", "goodsSpec", "unitName",
+									"goodsCount"]
+						});
+				var store = Ext.create("Ext.data.Store", {
+							autoLoad : false,
+							model : modelName,
+							data : []
+						});
+
+				me.__goodsGrid = Ext.create("Ext.grid.Panel", {
+							viewConfig : {
+								enableTextSelection : true
+							},
+							columnLines : true,
+							columns : [Ext.create("Ext.grid.RowNumberer", {
+												text : "序号",
+												width : 30
+											}), {
+										header : "商品编码",
+										dataIndex : "goodsCode",
+										menuDisabled : true,
+										sortable : false,
+										editor : {
+											xtype : "psi_goodsfield",
+											parentCmp : me
+										}
+									}, {
+										header : "商品名称",
+										dataIndex : "goodsName",
+										menuDisabled : true,
+										sortable : false,
+										width : 200
+									}, {
+										header : "规格型号",
+										dataIndex : "goodsSpec",
+										menuDisabled : true,
+										sortable : false,
+										width : 200
+									}, {
+										header : "调拨数量",
+										dataIndex : "goodsCount",
+										menuDisabled : true,
+										sortable : false,
+										align : "right",
+										width : 100
+									}, {
+										header : "单位",
+										dataIndex : "unitName",
+										menuDisabled : true,
+										sortable : false,
+										width : 60
+									}],
+							store : store
+						});
+
+				return me.__goodsGrid;
+			}
+		});
