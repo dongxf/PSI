450d5a4aa723d46eac1a8f56906d02d826bb7ec0
CRM8000 <crm8000@qq.com>
2015-12-16 13:53:09 +0800

    代码微调,增加注释

--- web/Public/Scripts/PSI/User/LoginForm.js
+++ web/Public/Scripts/PSI/User/LoginForm.js
@@ -29,99 +29,129 @@ Ext.define("PSI.User.LoginForm", {
 		var me = this;
 
 		Ext.apply(me, {
-			height : me.getDemoInfo() == "" ? 140 : 200,
-			header : {
-				title : "<span style='font-size:120%'>登录 - "
-						+ me.getProductionName() + "</span>",
-				iconCls : "PSI-login",
-				height : 40
-			},
-			items : [{
-				id : "loginForm",
-				xtype : "form",
-				layout : {
-					type : "table",
-					columns : 1
-				},
-				height : "100%",
-				border : 0,
-				bodyPadding : 5,
-				defaultType : 'textfield',
-				fieldDefaults : {
-					labelWidth : 60,
-					labelAlign : "right",
-					labelSeparator : "",
-					msgTarget : 'side'
-				},
-				items : [{
-							xtype : "hidden",
-							name : "fromDevice",
-							value : "web"
-						}, {
-							id : "editLoginName",
-							width : 370,
-							fieldLabel : "登录名",
-							allowBlank : false,
-							blankText : "没有输入登录名",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							name : "loginName",
-							value : Ext.util.Cookies.get("PSI_user_login_name"),
-							listeners : {
-								specialkey : function(field, e) {
-									if (e.getKey() === e.ENTER) {
-										Ext.getCmp("editPassword").focus();
+					height : me.getDemoInfo() == "" ? 140 : 200,
+					header : {
+						title : "<span style='font-size:120%'>登录 - "
+								+ me.getProductionName() + "</span>",
+						iconCls : "PSI-login",
+						height : 40
+					},
+					items : [{
+						id : "loginForm",
+						xtype : "form",
+						layout : {
+							type : "table",
+							columns : 1
+						},
+						height : "100%",
+						border : 0,
+						bodyPadding : 5,
+						defaultType : 'textfield',
+						fieldDefaults : {
+							labelWidth : 60,
+							labelAlign : "right",
+							labelSeparator : "",
+							msgTarget : 'side'
+						},
+						items : [{
+									xtype : "hidden",
+									name : "fromDevice",
+									value : "web"
+								}, {
+									id : "editLoginName",
+									width : 370,
+									fieldLabel : "登录名",
+									allowBlank : false,
+									blankText : "没有输入登录名",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									name : "loginName",
+									value : me.getLoginNameFromCookie(),
+									listeners : {
+										specialkey : me.onEditLoginNameSpecialKey,
+										scope : me
 									}
-								}
-							}
-						}, {
-							id : "editPassword",
-							fieldLabel : "密码",
-							allowBlank : false,
-							blankText : "没有输入密码",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							inputType : "password",
-							name : "password",
-							width : 370,
-							listeners : {
-								specialkey : function(field, e) {
-									if (e.getKey() === e.ENTER) {
-										if (Ext.getCmp("loginForm").getForm()
-												.isValid()) {
-											me.onOK();
-										}
+								}, {
+									id : "editPassword",
+									fieldLabel : "密码",
+									allowBlank : false,
+									blankText : "没有输入密码",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									inputType : "password",
+									name : "password",
+									width : 370,
+									listeners : {
+										specialkey : me.onEditPasswordSpecialKey,
+										scope : me
 									}
-								}
-							}
-						}, {
-							xtype : "displayfield",
-							value : me.getDemoInfo()
-						}, {
-							xtype : "hidden",
-							name : "ip",
-							value : me.getIp()
-						}, {
-							xtype : "hidden",
-							name : "ipFrom",
-							value : me.getCname()
-						}],
-				buttons : [{
-							text : "登录",
-							formBind : true,
-							handler : me.onOK,
-							scope : me,
-							iconCls : "PSI-button-ok"
-						}, {
-							text : "帮助",
-							iconCls : "PSI-help",
-							handler : function() {
-								window.open(PSI.Const.BASE_URL
-										+ "/Home/Help/index?t=login");
-							}
-						}]
-			}]
-		});
+								}, {
+									xtype : "displayfield",
+									value : me.getDemoInfo()
+								}, {
+									xtype : "hidden",
+									name : "ip",
+									value : me.getIp()
+								}, {
+									xtype : "hidden",
+									name : "ipFrom",
+									value : me.getCname()
+								}],
+						buttons : [{
+									text : "登录",
+									formBind : true,
+									handler : me.onOK,
+									scope : me,
+									iconCls : "PSI-button-ok"
+								}, {
+									text : "帮助",
+									iconCls : "PSI-help",
+									handler : function() {
+										window.open(PSI.Const.BASE_URL
+												+ "/Home/Help/index?t=login");
+									}
+								}]
+					}]
+				});
 
 		me.callParent(arguments);
+	},
+
+	onEditLoginNameSpecialKey : function(field, e) {
+		if (e.getKey() === e.ENTER) {
+			Ext.getCmp("editPassword").focus();
+		}
+	},
+
+	onEditPasswordSpecialKey : function(field, e) {
+		var me = this;
+
+		if (e.getKey() === e.ENTER) {
+			if (Ext.getCmp("loginForm").getForm().isValid()) {
+				me.onOK();
+			}
+		}
+	},
+
+	/**
+	 * 从Cookie中获得上次登录的用户登录名
+	 * 
+	 * @return {}
+	 */
+	getLoginNameFromCookie : function() {
+		var loginName = Ext.util.Cookies.get("PSI_user_login_name");
+		if (loginName) {
+			return decodeURIComponent(loginName);
+		} else {
+			return "";
+		}
+	},
+
+	/**
+	 * 把登录名保存到Cookie中
+	 */
+	setLoginNameToCookie : function(loginName) {
+		loginName = encodeURIComponent(loginName);
+		var dt = Ext.Date.add(new Date(), Ext.Date.YEAR, 1)
+		Ext.util.Cookies.set("PSI_user_login_name", loginName, dt);
 	},
 
 	onOK : function() {
@@ -131,13 +161,12 @@ Ext.define("PSI.User.LoginForm", {
 		var f = Ext.getCmp("loginForm");
 		var el = f.getEl() || Ext.getBody();
 		el.mask("系统登录中...");
-		f.getForm().submit({
+
+		var r = {
 			url : PSI.Const.BASE_URL + "Home/User/loginPOST",
 			method : "POST",
 			success : function(form, action) {
-				Ext.util.Cookies.set("PSI_user_login_name",
-						encodeURIComponent(loginName), Ext.Date.add(new Date(),
-								Ext.Date.YEAR, 1));
+				me.setLoginNameToCookie(loginName);
 
 				var returnPage = me.getReturnPage();
 				if (returnPage) {
@@ -155,6 +184,8 @@ Ext.define("PSI.User.LoginForm", {
 							editPassword.focus();
 						});
 			}
-		});
+		};
+
+		f.getForm().submit(r);
 	}
 });
