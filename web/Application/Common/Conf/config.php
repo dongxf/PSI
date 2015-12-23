cb0ce0a6f9301ce9265ae1b9d7bd9c6e8870fdf0
CRM8000 <crm8000@qq.com>
2015-12-23 16:32:13 +0800

    适配MoPaaS MySQL的docker版

--- web/Application/Common/Conf/config.php
+++ web/Application/Common/Conf/config.php
@@ -3,7 +3,7 @@
 function PSI_getMoPaasV2MySQLConfig() {
 	$services = getenv("VCAP_SERVICES");
 	$services_json = json_decode($services, true);
-	$mysql_config = $services_json["MySQL-5.5"][0]["credentials"];
+	$mysql_config = $services_json["MySQL-docker-5.5"][0]["credentials"];
 	
 	return $mysql_config;
 }
