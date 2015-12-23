3dc43d7259b1652a2e139ba4fe1f2b21323d9344
CRM8000 <crm8000@qq.com>
2015-12-23 11:15:03 +0800

    代码微调,增加注释

--- web/Application/Common/Conf/config.php
+++ web/Application/Common/Conf/config.php
@@ -9,12 +9,6 @@ function PSI_getMoPaasV2MySQLConfig() {
 }
 
 function PSI_getHost() {
-	// MoPaaS V1
-	$host = getenv("MOPAAS_MYSQL22118_HOST");
-	if ($host) {
-		return $host;
-	}
-	
 	// MoPaaS V2
 	$cfg = PSI_getMoPaasV2MySQLConfig();
 	if ($cfg) {
@@ -26,12 +20,6 @@ function PSI_getHost() {
 }
 
 function PSI_getDBName() {
-	// MoPaaS V1
-	$name = getenv("MOPAAS_MYSQL22118_NAME");
-	if ($name) {
-		return $name;
-	}
-	
 	// MoPaaS V2
 	$cfg = PSI_getMoPaasV2MySQLConfig();
 	if ($cfg) {
@@ -42,12 +30,6 @@ function PSI_getDBName() {
 }
 
 function PSI_getUser() {
-	// MoPaaS V1
-	$user = getenv("MOPAAS_MYSQL22118_USER");
-	if ($user) {
-		return $user;
-	}
-	
 	// MoPaaS V2
 	$cfg = PSI_getMoPaasV2MySQLConfig();
 	if ($cfg) {
@@ -58,12 +40,6 @@ function PSI_getUser() {
 }
 
 function PSI_getPassword() {
-	// MoPaaS V1
-	$password = getenv("MOPAAS_MYSQL22118_PASSWORD");
-	if ($password) {
-		return $password;
-	}
-	
 	// MoPaaS V2
 	$cfg = PSI_getMoPaasV2MySQLConfig();
 	if ($cfg) {
@@ -74,12 +50,6 @@ function PSI_getPassword() {
 }
 
 function PSI_getPort() {
-	// MoPaaS V1
-	$port = getenv("MOPAAS_MYSQL22118_PORT");
-	if ($port) {
-		return $port;
-	}
-	
 	// MoPaaS V2
 	$cfg = PSI_getMoPaasV2MySQLConfig();
 	if ($cfg) {
@@ -97,6 +67,22 @@ return array(
 		'DB_NAME' => PSI_getDBName(), // 数据库名
 		'DB_USER' => PSI_getUser(), // 用户名
 		'DB_PWD' => PSI_getPassword(), // 密码
-		'DB_PORT' => PSI_getPort()
-); // 端口
+		'DB_PORT' => PSI_getPort() // 端口
+);
 
+/*
+
+// 也可以把上面的代码都删除掉，然后按下面的写法来配置
+
+return array(
+		'URL_CASE_INSENSITIVE' => false,
+		'SHOW_ERROR_MSG' => true,
+		'DB_TYPE' => 'mysql', // 数据库类型
+		'DB_HOST' => '127.0.0.1', // 服务器地址
+		'DB_NAME' => 'psi', // 数据库名
+		'DB_USER' => 'root', // 用户名
+		'DB_PWD' => '', // 密码
+		'DB_PORT' => 3306 // 端口
+);
+ 
+ */
