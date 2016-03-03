acd728aff71c587878f6eaabb2b70e7c0fa2cb52
CRM8000 <crm8000@qq.com>
2016-03-03 13:15:59 +0800

    商品-品牌

---
+++ web/Public/Scripts/PSI/Goods/BrandMainForm.js
@@ -0,0 +1,81 @@
+/**
+ * 商品品牌 - 主界面
+ * 
+ * @author 李静波
+ */
+Ext.define("PSI.Goods.BrandMainForm", {
+			extend : "Ext.panel.Panel",
+
+			/**
+			 * 初始化组件
+			 */
+			initComponent : function() {
+				var me = this;
+
+				Ext.apply(me, {
+							border : 0,
+							layout : "border",
+							tbar : [{
+										text : "新增品牌",
+										iconCls : "PSI-button-add",
+										handler : me.onAddBrand,
+										scope : me
+									}, {
+										text : "编辑品牌",
+										iconCls : "PSI-button-edit",
+										handler : me.onEditBrand,
+										scope : me
+									}, {
+										text : "删除品牌",
+										iconCls : "PSI-button-delete",
+										handler : me.onDeleteBrand,
+										scope : me
+									}, "-", {
+										text : "关闭",
+										iconCls : "PSI-button-exit",
+										handler : function() {
+											location
+													.replace(PSI.Const.BASE_URL);
+										}
+									}],
+							items : [{
+										region : "center",
+										xtype : "panel",
+										layout : "fit",
+										border : 0,
+										items : []
+									}]
+						});
+
+				me.callParent(arguments);
+
+				me.freshGrid();
+			},
+
+			/**
+			 * 新增商品品牌
+			 */
+			onAddBrand : function() {
+				PSI.MsgBox.showInfo("TODO");
+			},
+
+			/**
+			 * 编辑商品品牌
+			 */
+			onEditBrand : function() {
+				PSI.MsgBox.showInfo("TODO");
+			},
+
+			/**
+			 * 删除商品品牌
+			 */
+			onDeleteBrand : function() {
+				PSI.MsgBox.showInfo("TODO");
+			},
+
+			/**
+			 * 刷新Grid
+			 */
+			freshGrid : function(id) {
+			}
+		});
