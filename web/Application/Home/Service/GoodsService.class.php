82d57a0e259ced34dcd90e777112bea4e04b67cc
CRM8000 <crm8000@qq.com>
2016-03-03 13:56:08 +0800

    商品-品牌

--- web/Application/Home/Service/GoodsService.class.php
+++ web/Application/Home/Service/GoodsService.class.php
@@ -1402,6 +1402,74 @@ class GoodsService extends PSIBaseService {
 		}
 		
 		$result = array();
+		$sql = "select id, name, full_name
+				from t_goods_brand b
+				where (parent_id is null)
+				";
+		$queryParam = array();
+		$ds = new DataOrgService();
+		$rs = $ds->buildSQL(FIdConst::GOODS_BRAND, "b");
+		if ($rs) {
+			$sql .= " and " . $rs[0];
+			$queryParam = array_merge($queryParam, $rs[1]);
+		}
+		
+		$sql .= " order by name";
+		
+		$db = M();
+		$data = $db->query($sql, $queryParam);
+		$result = array();
+		foreach ( $data as $i => $v ) {
+			$id = $v["id"];
+			$result[$i]["id"] = $id;
+			$result[$i]["text"] = $v["name"];
+			$fullName = $v["full_name"];
+			if (! $fullName) {
+				$fullName = $v["name"];
+			}
+			$result[$i]["fullName"] = $fullName;
+			
+			$children = $this->allBrandsInternal($db, $id, $rs);
+			
+			$result[$i]["children"] = $children;
+			$result[$i]["leaf"] = count($children) == 0;
+			$result[$i]["expanded"] = true;
+		}
+		
+		return $result;
+	}
+
+	private function allBrandsInternal($db, $parentId, $rs) {
+		$result = array();
+		$sql = "select id, name, full_name
+				from t_goods_brand b
+				where (parent_id = '%s')
+				";
+		$queryParam = array();
+		$queryParam[] = $parentId;
+		if ($rs) {
+			$sql .= " and " . $rs[0];
+			$queryParam = array_merge($queryParam, $rs[1]);
+		}
+		
+		$sql .= " order by name";
+		$data = $db->query($sql, $queryParam);
+		foreach ( $data as $i => $v ) {
+			$id = $v["id"];
+			$result[$i]["id"] = $v["id"];
+			$result[$i]["text"] = $v["name"];
+			$fullName = $v["full_name"];
+			if (! $fullName) {
+				$fullName = $v["name"];
+			}
+			$result[$i]["fullName"] = $fullName;
+			
+			$children = $this->allBrandsInternal($db, $id, $rs); // 自身递归调用
+			
+			$result[$i]["children"] = $children;
+			$result[$i]["leaf"] = count($children) == 0;
+			$result[$i]["expanded"] = true;
+		}
 		
 		return $result;
 	}
