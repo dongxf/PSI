30cc8e0daee2378243821838ebcd7a85269886f2
CRM8000 <crm8000@qq.com>
2016-10-11 20:19:40 +0800

    代码微调

--- web/Application/Home/Controller/AboutController.class.php
+++ web/Application/Home/Controller/AboutController.class.php
@@ -3,6 +3,7 @@
 namespace Home\Controller;
 
 use Think\Controller;
+use Home\Service\UserService;
 
 /**
  * 关于Controller
@@ -16,10 +17,15 @@ class AboutController extends PSIBaseController {
 	 * 关于 - 主页面
 	 */
 	public function index() {
-		$this->initVar();
-		
-		$this->assign("title", "关于");
-		
-		$this->display();
+		$us = new UserService();
+		if ($us->getLoginUserId()) {
+			$this->initVar();
+			
+			$this->assign("title", "关于");
+			
+			$this->display();
+		} else {
+			$this->gotoLoginPage("/Home/About/index");
+		}
 	}
 }
