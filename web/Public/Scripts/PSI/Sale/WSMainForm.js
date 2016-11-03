b35b6ecba2f17c5751de87ff2f3c77d7df3432c2
CRM8000 <crm8000@qq.com>
2016-11-03 16:54:26 +0800

    代码微调，打开新模块的时候使用浏览器的tab页面

--- web/Public/Scripts/PSI/Sale/WSMainForm.js
+++ web/Public/Scripts/PSI/Sale/WSMainForm.js
@@ -85,7 +85,7 @@ Ext.define("PSI.Sale.WSMainForm", {
 					text : "关闭",
 					iconCls : "PSI-button-exit",
 					handler : function() {
-						location.replace(PSI.Const.BASE_URL);
+						window.close();
 					}
 				}];
 	},
