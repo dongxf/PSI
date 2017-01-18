3d9b3fbb678c25d938893b6602e978551abb3c4e
CRM8000 <crm8000@qq.com>
2017-01-18 16:25:25 +0800

    代码重构

---
+++ web/Application/Home/DAO/GoodsDAO.class.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Home\DAO;
+
+use Home\Common\FIdConst;
+
+/**
+ * 商品DAO
+ *
+ * @author 李静波
+ */
+class GoodsDAO extends PSIBaseDAO {
+	var $db;
+
+	function __construct($db = null) {
+		if ($db == null) {
+			$db = M();
+		}
+		
+		$this->db = $db;
+	}
+
+	/**
+	 * 商品列表
+	 */
+	public function goodsList($params) {
+		$db = $this->db;
+		
+		$categoryId = $params["categoryId"];
+		$code = $params["code"];
+		$name = $params["name"];
+		$spec = $params["spec"];
+		$barCode = $params["barCode"];
+		
+		$start = $params["start"];
+		$limit = $params["limit"];
+		
+		$loginUserId = $params["loginUserId"];
+		
+		$result = array();
+		$sql = "select g.id, g.code, g.name, g.sale_price, g.spec,  g.unit_id, u.name as unit_name,
+					g.purchase_price, g.bar_code, g.memo, g.data_org, g.brand_id
+				from t_goods g, t_goods_unit u
+				where (g.unit_id = u.id) and (g.category_id = '%s') ";
+		$queryParam = array();
+		$queryParam[] = $categoryId;
+		$ds = new DataOrgDAO($db);
+		$rs = $ds->buildSQL(FIdConst::GOODS, "g", $loginUserId);
+		if ($rs) {
+			$sql .= " and " . $rs[0];
+			$queryParam = array_merge($queryParam, $rs[1]);
+		}
+		
+		if ($code) {
+			$sql .= " and (g.code like '%s') ";
+			$queryParam[] = "%{$code}%";
+		}
+		if ($name) {
+			$sql .= " and (g.name like '%s' or g.py like '%s') ";
+			$queryParam[] = "%{$name}%";
+			$queryParam[] = "%{$name}%";
+		}
+		if ($spec) {
+			$sql .= " and (g.spec like '%s')";
+			$queryParam[] = "%{$spec}%";
+		}
+		if ($barCode) {
+			$sql .= " and (g.bar_code = '%s') ";
+			$queryParam[] = $barCode;
+		}
+		
+		$sql .= " order by g.code limit %d, %d";
+		$queryParam[] = $start;
+		$queryParam[] = $limit;
+		$data = $db->query($sql, $queryParam);
+		
+		foreach ( $data as $i => $v ) {
+			$result[$i]["id"] = $v["id"];
+			$result[$i]["code"] = $v["code"];
+			$result[$i]["name"] = $v["name"];
+			$result[$i]["salePrice"] = $v["sale_price"];
+			$result[$i]["spec"] = $v["spec"];
+			$result[$i]["unitId"] = $v["unit_id"];
+			$result[$i]["unitName"] = $v["unit_name"];
+			$result[$i]["purchasePrice"] = $v["purchase_price"] == 0 ? null : $v["purchase_price"];
+			$result[$i]["barCode"] = $v["bar_code"];
+			$result[$i]["memo"] = $v["memo"];
+			$result[$i]["dataOrg"] = $v["data_org"];
+			
+			$brandId = $v["brand_id"];
+			if ($brandId) {
+				$result[$i]["brandFullName"] = $this->getBrandFullNameById($db, $brandId);
+			}
+		}
+		
+		$sql = "select count(*) as cnt from t_goods g where (g.category_id = '%s') ";
+		$queryParam = array();
+		$queryParam[] = $categoryId;
+		$ds = new DataOrgDAO($db);
+		$rs = $ds->buildSQL(FIdConst::GOODS, "g", $loginUserId);
+		if ($rs) {
+			$sql .= " and " . $rs[0];
+			$queryParam = array_merge($queryParam, $rs[1]);
+		}
+		if ($code) {
+			$sql .= " and (g.code like '%s') ";
+			$queryParam[] = "%{$code}%";
+		}
+		if ($name) {
+			$sql .= " and (g.name like '%s' or g.py like '%s') ";
+			$queryParam[] = "%{$name}%";
+			$queryParam[] = "%{$name}%";
+		}
+		if ($spec) {
+			$sql .= " and (g.spec like '%s')";
+			$queryParam[] = "%{$spec}%";
+		}
+		if ($barCode) {
+			$sql .= " and (g.bar_code = '%s') ";
+			$queryParam[] = $barCode;
+		}
+		
+		$data = $db->query($sql, $queryParam);
+		$totalCount = $data[0]["cnt"];
+		
+		return array(
+				"goodsList" => $result,
+				"totalCount" => $totalCount
+		);
+	}
+}
--- web/Application/Home/Service/GoodsService.class.php
+++ web/Application/Home/Service/GoodsService.class.php
@@ -7,6 +7,7 @@ use Home\DAO\GoodsBomDAO;
 use Home\DAO\GoodsBrandDAO;
 use Home\DAO\GoodsUnitDAO;
 use Home\DAO\GoodsCategoryDAO;
+use Home\DAO\GoodsDAO;
 
 /**
  * 商品Service
@@ -269,107 +270,11 @@ class GoodsService extends PSIBaseService {
 			return $this->emptyResult();
 		}
 		
-		$categoryId = $params["categoryId"];
-		$code = $params["code"];
-		$name = $params["name"];
-		$spec = $params["spec"];
-		$barCode = $params["barCode"];
+		$us = new UserService();
+		$params["loginUserId"] = $us->getLoginUserId();
 		
-		$page = $params["page"];
-		$start = $params["start"];
-		$limit = $params["limit"];
-		
-		$db = M();
-		$result = array();
-		$sql = "select g.id, g.code, g.name, g.sale_price, g.spec,  g.unit_id, u.name as unit_name,
-					g.purchase_price, g.bar_code, g.memo, g.data_org, g.brand_id
-				from t_goods g, t_goods_unit u 
-				where (g.unit_id = u.id) and (g.category_id = '%s') ";
-		$queryParam = array();
-		$queryParam[] = $categoryId;
-		$ds = new DataOrgService();
-		$rs = $ds->buildSQL(FIdConst::GOODS, "g");
-		if ($rs) {
-			$sql .= " and " . $rs[0];
-			$queryParam = array_merge($queryParam, $rs[1]);
-		}
-		
-		if ($code) {
-			$sql .= " and (g.code like '%s') ";
-			$queryParam[] = "%{$code}%";
-		}
-		if ($name) {
-			$sql .= " and (g.name like '%s' or g.py like '%s') ";
-			$queryParam[] = "%{$name}%";
-			$queryParam[] = "%{$name}%";
-		}
-		if ($spec) {
-			$sql .= " and (g.spec like '%s')";
-			$queryParam[] = "%{$spec}%";
-		}
-		if ($barCode) {
-			$sql .= " and (g.bar_code = '%s') ";
-			$queryParam[] = $barCode;
-		}
-		
-		$sql .= " order by g.code limit %d, %d";
-		$queryParam[] = $start;
-		$queryParam[] = $limit;
-		$data = $db->query($sql, $queryParam);
-		
-		foreach ( $data as $i => $v ) {
-			$result[$i]["id"] = $v["id"];
-			$result[$i]["code"] = $v["code"];
-			$result[$i]["name"] = $v["name"];
-			$result[$i]["salePrice"] = $v["sale_price"];
-			$result[$i]["spec"] = $v["spec"];
-			$result[$i]["unitId"] = $v["unit_id"];
-			$result[$i]["unitName"] = $v["unit_name"];
-			$result[$i]["purchasePrice"] = $v["purchase_price"] == 0 ? null : $v["purchase_price"];
-			$result[$i]["barCode"] = $v["bar_code"];
-			$result[$i]["memo"] = $v["memo"];
-			$result[$i]["dataOrg"] = $v["data_org"];
-			
-			$brandId = $v["brand_id"];
-			if ($brandId) {
-				$result[$i]["brandFullName"] = $this->getBrandFullNameById($db, $brandId);
-			}
-		}
-		
-		$sql = "select count(*) as cnt from t_goods g where (g.category_id = '%s') ";
-		$queryParam = array();
-		$queryParam[] = $categoryId;
-		$ds = new DataOrgService();
-		$rs = $ds->buildSQL(FIdConst::GOODS, "g");
-		if ($rs) {
-			$sql .= " and " . $rs[0];
-			$queryParam = array_merge($queryParam, $rs[1]);
-		}
-		if ($code) {
-			$sql .= " and (g.code like '%s') ";
-			$queryParam[] = "%{$code}%";
-		}
-		if ($name) {
-			$sql .= " and (g.name like '%s' or g.py like '%s') ";
-			$queryParam[] = "%{$name}%";
-			$queryParam[] = "%{$name}%";
-		}
-		if ($spec) {
-			$sql .= " and (g.spec like '%s')";
-			$queryParam[] = "%{$spec}%";
-		}
-		if ($barCode) {
-			$sql .= " and (g.bar_code = '%s') ";
-			$queryParam[] = $barCode;
-		}
-		
-		$data = $db->query($sql, $queryParam);
-		$totalCount = $data[0]["cnt"];
-		
-		return array(
-				"goodsList" => $result,
-				"totalCount" => $totalCount
-		);
+		$dao = new GoodsDAO();
+		return $dao->goodsList($params);
 	}
 
 	public function getBrandFullNameById($db, $brandId) {
