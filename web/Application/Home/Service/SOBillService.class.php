44dd69b6c58669f68fa9ae1c975be2e1251eabf7
CRM8000 <crm8000@qq.com>
2017-02-08 09:55:18 +0800

    代码重构 - 销售订单

--- web/Application/Home/Service/SOBillService.class.php
+++ web/Application/Home/Service/SOBillService.class.php
@@ -3,13 +3,14 @@
 namespace Home\Service;
 
 use Home\Common\FIdConst;
+use Home\DAO\SOBillDAO;
 
 /**
  * 销售订单Service
  *
  * @author 李静波
  */
-class SOBillService extends PSIBaseService {
+class SOBillService extends PSIBaseExService {
 	private $LOG_CATEGORY = "销售订单";
 
 	/**
@@ -42,140 +43,9 @@ class SOBillService extends PSIBaseService {
 			return $this->emptyResult();
 		}
 		
-		$start = $params["start"];
-		$limit = $params["limit"];
-		
-		$billStatus = $params["billStatus"];
-		$ref = $params["ref"];
-		$fromDT = $params["fromDT"];
-		$toDT = $params["toDT"];
-		$customerId = $params["customerId"];
-		$receivingType = $params["receivingType"];
-		
-		$db = M();
-		
-		$queryParams = array();
-		
-		$result = array();
-		$sql = "select s.id, s.ref, s.bill_status, s.goods_money, s.tax, s.money_with_tax,
-					c.name as customer_name, s.contact, s.tel, s.fax, s.deal_address,
-					s.deal_date, s.receiving_type, s.bill_memo, s.date_created,
-					o.full_name as org_name, u1.name as biz_user_name, u2.name as input_user_name,
-					s.confirm_user_id, s.confirm_date
-				from t_so_bill s, t_customer c, t_org o, t_user u1, t_user u2
-				where (s.customer_id = c.id) and (s.org_id = o.id)
-					and (s.biz_user_id = u1.id) and (s.input_user_id = u2.id) ";
-		
-		$ds = new DataOrgService();
-		$rs = $ds->buildSQL(FIdConst::SALE_ORDER, "s");
-		if ($rs) {
-			$sql .= " and " . $rs[0];
-			$queryParams = $rs[1];
-		}
-		
-		if ($billStatus != - 1) {
-			$sql .= " and (s.bill_status = %d) ";
-			$queryParams[] = $billStatus;
-		}
-		if ($ref) {
-			$sql .= " and (s.ref like '%s') ";
-			$queryParams[] = "%$ref%";
-		}
-		if ($fromDT) {
-			$sql .= " and (s.deal_date >= '%s')";
-			$queryParams[] = $fromDT;
-		}
-		if ($toDT) {
-			$sql .= " and (s.deal_date <= '%s')";
-			$queryParams[] = $toDT;
-		}
-		if ($customerId) {
-			$sql .= " and (s.customer_id = '%s')";
-			$queryParams[] = $customerId;
-		}
-		if ($receivingType != - 1) {
-			$sql .= " and (s.receiving_type = %d) ";
-			$queryParams[] = $receivingType;
-		}
-		$sql .= " order by s.deal_date desc, s.ref desc 
-				  limit %d , %d";
-		$queryParams[] = $start;
-		$queryParams[] = $limit;
-		$data = $db->query($sql, $queryParams);
-		foreach ( $data as $i => $v ) {
-			$result[$i]["id"] = $v["id"];
-			$result[$i]["ref"] = $v["ref"];
-			$result[$i]["billStatus"] = $v["bill_status"];
-			$result[$i]["dealDate"] = $this->toYMD($v["deal_date"]);
-			$result[$i]["dealAddress"] = $v["deal_address"];
-			$result[$i]["customerName"] = $v["customer_name"];
-			$result[$i]["contact"] = $v["contact"];
-			$result[$i]["tel"] = $v["tel"];
-			$result[$i]["fax"] = $v["fax"];
-			$result[$i]["goodsMoney"] = $v["goods_money"];
-			$result[$i]["tax"] = $v["tax"];
-			$result[$i]["moneyWithTax"] = $v["money_with_tax"];
-			$result[$i]["receivingType"] = $v["receiving_type"];
-			$result[$i]["billMemo"] = $v["bill_memo"];
-			$result[$i]["bizUserName"] = $v["biz_user_name"];
-			$result[$i]["orgName"] = $v["org_name"];
-			$result[$i]["inputUserName"] = $v["input_user_name"];
-			$result[$i]["dateCreated"] = $v["date_created"];
-			
-			$confirmUserId = $v["confirm_user_id"];
-			if ($confirmUserId) {
-				$sql = "select name from t_user where id = '%s' ";
-				$d = $db->query($sql, $confirmUserId);
-				if ($d) {
-					$result[$i]["confirmUserName"] = $d[0]["name"];
-					$result[$i]["confirmDate"] = $v["confirm_date"];
-				}
-			}
-		}
-		
-		$sql = "select count(*) as cnt
-				from t_so_bill s, t_customer c, t_org o, t_user u1, t_user u2
-				where (s.customer_id = c.id) and (s.org_id = o.id)
-					and (s.biz_user_id = u1.id) and (s.input_user_id = u2.id)
-				";
-		$queryParams = array();
-		$ds = new DataOrgService();
-		$rs = $ds->buildSQL(FIdConst::SALE_ORDER, "s");
-		if ($rs) {
-			$sql .= " and " . $rs[0];
-			$queryParams = $rs[1];
-		}
-		if ($billStatus != - 1) {
-			$sql .= " and (s.bill_status = %d) ";
-			$queryParams[] = $billStatus;
-		}
-		if ($ref) {
-			$sql .= " and (s.ref like '%s') ";
-			$queryParams[] = "%$ref%";
-		}
-		if ($fromDT) {
-			$sql .= " and (s.deal_date >= '%s')";
-			$queryParams[] = $fromDT;
-		}
-		if ($toDT) {
-			$sql .= " and (s.deal_date <= '%s')";
-			$queryParams[] = $toDT;
-		}
-		if ($customerId) {
-			$sql .= " and (s.customer_id = '%s')";
-			$queryParams[] = $customerId;
-		}
-		if ($receivingType != - 1) {
-			$sql .= " and (s.receiving_type = %d) ";
-			$queryParams[] = $receivingType;
-		}
-		$data = $db->query($sql, $queryParams);
-		$cnt = $data[0]["cnt"];
-		
-		return array(
-				"dataList" => $result,
-				"totalCount" => $cnt
-		);
+		$params["loginUserId"] = $this->getLoginUserId();
+		$dao = new SOBillDAO();
+		return $dao->sobillList($params);
 	}
 
 	/**
