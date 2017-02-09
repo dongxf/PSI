28c807ec5ffc55ebd32762ed096bbf63283399d8
CRM8000 <crm8000@qq.com>
2017-02-09 10:56:21 +0800

    代码重构 - 销售出库单

--- web/Application/Home/DAO/WSBillDAO.class.php
+++ web/Application/Home/DAO/WSBillDAO.class.php
@@ -696,4 +696,55 @@ class WSBillDAO extends PSIBaseExDAO {
 		// 操作成功
 		return null;
 	}
+
+	/**
+	 * 销售出库单生成pdf文件
+	 */
+	public function pdf($params) {
+		$ref = $params["ref"];
+		
+		$db = $this->db;
+		$sql = "select w.id, w.bizdt, c.name as customer_name,
+					  u.name as biz_user_name,
+					  h.name as warehouse_name,
+					  w.sale_money
+					from t_ws_bill w, t_customer c, t_user u, t_warehouse h
+					where w.customer_id = c.id and w.biz_user_id = u.id
+					  and w.warehouse_id = h.id
+					  and w.ref = '%s' ";
+		$data = $db->query($sql, $ref);
+		if (! $data) {
+			return null;
+		}
+		
+		$id = $data[0]["id"];
+		
+		$bill["bizDT"] = date("Y-m-d", strtotime($data[0]["bizdt"]));
+		$bill["customerName"] = $data[0]["customer_name"];
+		$bill["warehouseName"] = $data[0]["warehouse_name"];
+		$bill["bizUserName"] = $data[0]["biz_user_name"];
+		$bill["saleMoney"] = $data[0]["sale_money"];
+		
+		// 明细表
+		$sql = "select g.code, g.name, g.spec, u.name as unit_name, d.goods_count,
+				d.goods_price, d.goods_money, d.sn_note
+				from t_ws_bill_detail d, t_goods g, t_goods_unit u
+				where d.wsbill_id = '%s' and d.goods_id = g.id and g.unit_id = u.id
+				order by d.show_order";
+		$data = $db->query($sql, $id);
+		$items = array();
+		foreach ( $data as $i => $v ) {
+			$items[$i]["goodsCode"] = $v["code"];
+			$items[$i]["goodsName"] = $v["name"];
+			$items[$i]["goodsSpec"] = $v["spec"];
+			$items[$i]["unitName"] = $v["unit_name"];
+			$items[$i]["goodsCount"] = $v["goods_count"];
+			$items[$i]["goodsPrice"] = $v["goods_price"];
+			$items[$i]["goodsMoney"] = $v["goods_money"];
+			$items[$i]["sn"] = $v["sn_note"];
+		}
+		$bill["items"] = $items;
+		
+		return $bill;
+	}
 }
--- web/Application/Home/Service/WSBillService.class.php
+++ web/Application/Home/Service/WSBillService.class.php
@@ -720,48 +720,17 @@ class WSBillService extends PSIBaseExService {
 			return;
 		}
 		
+		$bs = new BizConfigService();
+		$productionName = $bs->getProductionName();
+		
 		$ref = $params["ref"];
-		$db = M();
-		$sql = "select w.id, w.bizdt, c.name as customer_name,
-					  u.name as biz_user_name,
-					  h.name as warehouse_name,
-					  w.sale_money
-					from t_ws_bill w, t_customer c, t_user u, t_warehouse h
-					where w.customer_id = c.id and w.biz_user_id = u.id
-					  and w.warehouse_id = h.id
-					  and w.ref = '%s' ";
-		$data = $db->query($sql, $ref);
-		if (! $data) {
+		
+		$dao = new WSBillDAO($this->db());
+		
+		$bill = $dao->pdf($params);
+		if (! $bill) {
 			return;
 		}
-		
-		$id = $data[0]["id"];
-		
-		$bill["bizDT"] = date("Y-m-d", strtotime($data[0]["bizdt"]));
-		$bill["customerName"] = $data[0]["customer_name"];
-		$bill["warehouseName"] = $data[0]["warehouse_name"];
-		$bill["bizUserName"] = $data[0]["biz_user_name"];
-		$bill["saleMoney"] = $data[0]["sale_money"];
-		
-		// 明细表
-		$sql = "select g.code, g.name, g.spec, u.name as unit_name, d.goods_count,
-					d.goods_price, d.goods_money, d.sn_note
-					from t_ws_bill_detail d, t_goods g, t_goods_unit u
-					where d.wsbill_id = '%s' and d.goods_id = g.id and g.unit_id = u.id
-					order by d.show_order";
-		$data = $db->query($sql, $id);
-		$items = array();
-		foreach ( $data as $i => $v ) {
-			$items[$i]["goodsCode"] = $v["code"];
-			$items[$i]["goodsName"] = $v["name"];
-			$items[$i]["goodsSpec"] = $v["spec"];
-			$items[$i]["unitName"] = $v["unit_name"];
-			$items[$i]["goodsCount"] = $v["goods_count"];
-			$items[$i]["goodsPrice"] = $v["goods_price"];
-			$items[$i]["goodsMoney"] = $v["goods_money"];
-			$items[$i]["sn"] = $v["sn_note"];
-		}
-		$bill["items"] = $items;
 		
 		$ps = new PDFService();
 		$pdf = $ps->getInstance();
@@ -779,7 +748,7 @@ class WSBillService extends PSIBaseExService {
 				14
 		));
 		
-		$pdf->SetHeaderData("", 0, "开源进销存PSI", "销售出库单");
+		$pdf->SetHeaderData("", 0, $productionName, "销售出库单");
 		
 		$pdf->SetFont("stsongstdlight", "", 10);
 		$pdf->AddPage();
