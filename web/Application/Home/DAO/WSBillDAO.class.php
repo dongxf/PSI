3bfe293ce179f56eeeac5ea160db6652f999ad0c
CRM8000 <crm8000@qq.com>
2017-02-09 11:40:53 +0800

    代码重构 - 销售出库单

--- web/Application/Home/DAO/WSBillDAO.class.php
+++ web/Application/Home/DAO/WSBillDAO.class.php
@@ -747,4 +747,525 @@ class WSBillDAO extends PSIBaseExDAO {
 		
 		return $bill;
 	}
+
+	/**
+	 * 提交销售出库单
+	 */
+	public function commitWSBill(& $params) {
+		$db = $this->db;
+		
+		$companyId = $params["companyId"];
+		if ($this->companyIdNotExists($companyId)) {
+			return $this->badParam("companyId");
+		}
+		$loginUserId = $params["loginUserId"];
+		if ($this->loginUserIdNotExists($loginUserId)) {
+			return $this->badParam("loginUserId");
+		}
+		
+		$bs = new BizConfigDAO($db);
+		// true: 先进先出
+		$fifo = $bs->getInventoryMethod($companyId) == 1;
+		
+		$id = $params["id"];
+		
+		$sql = "select ref, bill_status, customer_id, warehouse_id, biz_user_id, bizdt, sale_money,
+					receiving_type, company_id
+				from t_ws_bill where id = '%s' ";
+		$data = $db->query($sql, $id);
+		if (! $data) {
+			return $this->bad("要提交的销售出库单不存在");
+		}
+		$ref = $data[0]["ref"];
+		$bizDT = $data[0]["bizdt"];
+		$bizUserId = $data[0]["biz_user_id"];
+		$billStatus = $data[0]["bill_status"];
+		$receivingType = $data[0]["receiving_type"];
+		$saleMoney = $data[0]["sale_money"];
+		$companyId = $data[0]["company_id"];
+		if ($billStatus != 0) {
+			return $this->bad("销售出库单已经提交出库，不能再次提交");
+		}
+		$customerId = $data[0]["customer_id"];
+		$warehouseId = $data[0]["warehouse_id"];
+		
+		$customerDAO = new CustomerDAO($db);
+		$customer = $customerDAO->getCustomerById($customerId);
+		if (! $customer) {
+			return $this->bad("客户不存在");
+		}
+		
+		$warehouseDAO = new WarehouseDAO($db);
+		$warehouse = $warehouseDAO->getWarehouseById($warehouseId);
+		if (! $warehouse) {
+			return $this->bad("仓库不存在");
+		}
+		$warehouseName = $warehouse["name"];
+		$inited = $warehouse["inited"];
+		if ($inited != 1) {
+			return $this->bad("仓库 [{$warehouseName}]还没有建账，不能进行出库操作");
+		}
+		
+		$userDAO = new UserDAO($db);
+		$user = $userDAO->getUserById($bizUserId);
+		if (! $user) {
+			return $this->bad("业务员不存在");
+		}
+		
+		$allReceivingType = array(
+				0,
+				1,
+				2
+		);
+		
+		if (! in_array($receivingType, $allReceivingType)) {
+			return $this->bad("收款方式不正确，无法完成提交操作");
+		}
+		
+		$sql = "select id, goods_id, goods_count,  goods_price
+				from t_ws_bill_detail
+				where wsbill_id = '%s'
+				order by show_order ";
+		$items = $db->query($sql, $id);
+		if (! $items) {
+			return $this->bad("销售出库单没有出库商品明细记录，无法出库");
+		}
+		
+		foreach ( $items as $v ) {
+			$itemId = $v["id"];
+			$goodsId = $v["goods_id"];
+			$goodsCount = intval($v["goods_count"]);
+			$goodsPrice = floatval($v["goods_price"]);
+			
+			$sql = "select code, name from t_goods where id = '%s' ";
+			$data = $db->query($sql, $goodsId);
+			if (! $data) {
+				return $this->bad("要出库的商品不存在(商品后台id = {$goodsId})");
+			}
+			$goodsCode = $data[0]["code"];
+			$goodsName = $data[0]["name"];
+			if ($goodsCount <= 0) {
+				return $this->bad("商品[{$goodsCode} {$goodsName}]的出库数量需要是正数");
+			}
+			
+			if ($fifo) {
+				// 先进先出法
+				
+				// 库存总账
+				$sql = "select out_count, out_money, balance_count, balance_price,
+						balance_money from t_inventory
+						where warehouse_id = '%s' and goods_id = '%s' ";
+				$data = $db->query($sql, $warehouseId, $goodsId);
+				if (! $data) {
+					return $this->bad(
+							"商品 [{$goodsCode} {$goodsName}] 在仓库 [{$warehouseName}] 中没有存货，无法出库");
+				}
+				$balanceCount = $data[0]["balance_count"];
+				if ($balanceCount < $goodsCount) {
+					return $this->bad(
+							"商品 [{$goodsCode} {$goodsName}] 在仓库 [{$warehouseName}] 中存货数量不足，无法出库");
+				}
+				$balancePrice = $data[0]["balance_price"];
+				$balanceMoney = $data[0]["balance_money"];
+				$outCount = $data[0]["out_count"];
+				$outMoney = $data[0]["out_money"];
+				
+				$sql = "select id, balance_count, balance_price, balance_money,
+								out_count, out_price, out_money, date_created
+							from t_inventory_fifo
+							where warehouse_id = '%s' and goods_id = '%s'
+								and balance_count > 0
+							order by date_created ";
+				$data = $db->query($sql, $warehouseId, $goodsId);
+				if (! $data) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+				
+				$gc = $goodsCount;
+				$fifoMoneyTotal = 0;
+				for($i = 0; $i < count($data); $i ++) {
+					if ($gc == 0) {
+						break;
+					}
+					
+					$fv = $data[$i];
+					$fvBalanceCount = $fv["balance_count"];
+					$fvId = $fv["id"];
+					$fvBalancePrice = $fv["balance_price"];
+					$fvBalanceMoney = $fv["balance_money"];
+					$fvOutCount = $fv["out_count"];
+					$fvOutMoney = $fv["out_money"];
+					$fvDateCreated = $fv["date_created"];
+					
+					if ($fvBalanceCount >= $gc) {
+						if ($fvBalanceCount > $gc) {
+							$fifoMoney = $fvBalancePrice * $gc;
+						} else {
+							$fifoMoney = $fvBalanceMoney;
+						}
+						$fifoMoneyTotal += $fifoMoney;
+						
+						$fifoPrice = $fifoMoney / $gc;
+						
+						$fvOutCount += $gc;
+						$fvOutMoney += $fifoMoney;
+						$fvOutPrice = $fvOutMoney / $fvOutCount;
+						
+						$fvBalanceCount -= $gc;
+						$fvBalanceMoney -= $fifoMoney;
+						
+						$sql = "update t_inventory_fifo
+								set out_count = %d, out_price = %f, out_money = %f,
+									balance_count = %d, balance_money = %f
+								where id = %d ";
+						$rc = $db->execute($sql, $fvOutCount, $fvOutPrice, $fvOutMoney, 
+								$fvBalanceCount, $fvBalanceMoney, $fvId);
+						if ($rc === false) {
+							return $this->sqlError(__METHOD__, __LINE__);
+						}
+						
+						// fifo 的明细记录
+						$sql = "insert into t_inventory_fifo_detail(out_count, out_price, out_money,
+								balance_count, balance_price, balance_money, warehouse_id, goods_id,
+								date_created, wsbilldetail_id)
+								values (%d, %f, %f, %d, %f, %f, '%s', '%s', '%s', '%s')";
+						$rc = $db->execute($sql, $gc, $fifoPrice, $fifoMoney, $fvBalanceCount, 
+								$fvBalancePrice, $fvBalanceMoney, $warehouseId, $goodsId, 
+								$fvDateCreated, $itemId);
+						if ($rc === false) {
+							return $this->sqlError(__METHOD__, __LINE__);
+						}
+						
+						$gc = 0;
+					} else {
+						$fifoMoneyTotal += $fvBalanceMoney;
+						
+						$sql = "update t_inventory_fifo
+								set out_count = in_count, out_price = in_price, out_money = in_money,
+									balance_count = 0, balance_money = 0
+								where id = %d ";
+						$rc = $db->execute($sql, $fvId);
+						if ($rc === false) {
+							return $this->sqlError(__METHOD__, __LINE__);
+						}
+						
+						// fifo 的明细记录
+						$sql = "insert into t_inventory_fifo_detail(out_count, out_price, out_money,
+								balance_count, balance_price, balance_money, warehouse_id, goods_id,
+								date_created, wsbilldetail_id)
+								values (%d, %f, %f, %d, %f, %f, '%s', '%s', '%s', '%s')";
+						$rc = $db->execute($sql, $fvBalanceCount, $fvBalancePrice, $fvBalanceMoney, 
+								0, 0, 0, $warehouseId, $goodsId, $fvDateCreated, $itemId);
+						if ($rc === false) {
+							return $this->sqlError(__METHOD__, __LINE__);
+						}
+						
+						$gc -= $fvBalanceCount;
+					}
+				}
+				
+				$fifoPrice = $fifoMoneyTotal / $goodsCount;
+				
+				// 更新总账
+				$outCount += $goodsCount;
+				$outMoney += $fifoMoneyTotal;
+				$outPrice = $outMoney / $outCount;
+				$balanceCount -= $goodsCount;
+				if ($balanceCount == 0) {
+					$balanceMoney = 0;
+					$balancePrice = 0;
+				} else {
+					$balanceMoney -= $fifoMoneyTotal;
+					$balancePrice = $balanceMoney / $balanceCount;
+				}
+				
+				$sql = "update t_inventory
+						set out_count = %d, out_price = %f, out_money = %f,
+						    balance_count = %d, balance_price = %f, balance_money = %f
+						where warehouse_id = '%s' and goods_id = '%s' ";
+				$rc = $db->execute($sql, $outCount, $outPrice, $outMoney, $balanceCount, 
+						$balancePrice, $balanceMoney, $warehouseId, $goodsId);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+				
+				// 更新明细账
+				$sql = "insert into t_inventory_detail(out_count, out_price, out_money,
+						balance_count, balance_price, balance_money, warehouse_id,
+						goods_id, biz_date, biz_user_id, date_created, ref_number, ref_type)
+						values(%d, %f, %f, %d, %f, %f, '%s', '%s', '%s', '%s', now(), '%s', '销售出库')";
+				$rc = $db->execute($sql, $goodsCount, $fifoPrice, $fifoMoneyTotal, $balanceCount, 
+						$balancePrice, $balanceMoney, $warehouseId, $goodsId, $bizDT, $bizUserId, 
+						$ref);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+				
+				// 更新单据本身的记录
+				$sql = "update t_ws_bill_detail
+						set inventory_price = %f, inventory_money = %f
+						where id = '%s' ";
+				$rc = $db->execute($sql, $fifoPrice, $fifoMoneyTotal, $id);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+			} else {
+				// 移动平均法
+				
+				// 库存总账
+				$sql = "select out_count, out_money, balance_count, balance_price,
+						balance_money from t_inventory
+						where warehouse_id = '%s' and goods_id = '%s' ";
+				$data = $db->query($sql, $warehouseId, $goodsId);
+				if (! $data) {
+					return $this->bad(
+							"商品 [{$goodsCode} {$goodsName}] 在仓库 [{$warehouseName}] 中没有存货，无法出库");
+				}
+				$balanceCount = $data[0]["balance_count"];
+				if ($balanceCount < $goodsCount) {
+					return $this->bad(
+							"商品 [{$goodsCode} {$goodsName}] 在仓库 [{$warehouseName}] 中存货数量不足，无法出库");
+				}
+				$balancePrice = $data[0]["balance_price"];
+				$balanceMoney = $data[0]["balance_money"];
+				$outCount = $data[0]["out_count"];
+				$outMoney = $data[0]["out_money"];
+				$balanceCount -= $goodsCount;
+				if ($balanceCount == 0) {
+					// 当全部出库的时候，金额也需要全部转出去
+					$outMoney += $balanceMoney;
+					$outPriceDetail = $balanceMoney / $goodsCount;
+					$outMoneyDetail = $balanceMoney;
+					$balanceMoney = 0;
+				} else {
+					$outMoney += $goodsCount * $balancePrice;
+					$outPriceDetail = $balancePrice;
+					$outMoneyDetail = $goodsCount * $balancePrice;
+					$balanceMoney -= $goodsCount * $balancePrice;
+				}
+				$outCount += $goodsCount;
+				$outPrice = $outMoney / $outCount;
+				
+				$sql = "update t_inventory
+						set out_count = %d, out_price = %f, out_money = %f,
+						    balance_count = %d, balance_money = %f
+						where warehouse_id = '%s' and goods_id = '%s' ";
+				$rc = $db->execute($sql, $outCount, $outPrice, $outMoney, $balanceCount, 
+						$balanceMoney, $warehouseId, $goodsId);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+				
+				// 库存明细账
+				$sql = "insert into t_inventory_detail(out_count, out_price, out_money,
+						balance_count, balance_price, balance_money, warehouse_id,
+						goods_id, biz_date, biz_user_id, date_created, ref_number, ref_type)
+						values(%d, %f, %f, %d, %f, %f, '%s', '%s', '%s', '%s', now(), '%s', '销售出库')";
+				$rc = $db->execute($sql, $goodsCount, $outPriceDetail, $outMoneyDetail, 
+						$balanceCount, $balancePrice, $balanceMoney, $warehouseId, $goodsId, $bizDT, 
+						$bizUserId, $ref);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+				
+				// 单据本身的记录
+				$sql = "update t_ws_bill_detail
+						set inventory_price = %f, inventory_money = %f
+						where id = '%s' ";
+				$rc = $db->execute($sql, $outPriceDetail, $outMoneyDetail, $itemId);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+			}
+		}
+		
+		if ($receivingType == 0) {
+			
+			// 记应收账款
+			// 应收总账
+			$sql = "select rv_money, balance_money
+					from t_receivables
+					where ca_id = '%s' and ca_type = 'customer' and company_id = '%s' ";
+			$data = $db->query($sql, $customerId, $companyId);
+			if ($data) {
+				$rvMoney = $data[0]["rv_money"];
+				$balanceMoney = $data[0]["balance_money"];
+				
+				$rvMoney += $saleMoney;
+				$balanceMoney += $saleMoney;
+				
+				$sql = "update t_receivables
+						set rv_money = %f,  balance_money = %f
+						where ca_id = '%s' and ca_type = 'customer'
+							and company_id = '%s' ";
+				$rc = $db->execute($sql, $rvMoney, $balanceMoney, $customerId, $companyId);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+			} else {
+				$sql = "insert into t_receivables (id, rv_money, act_money, balance_money,
+							ca_id, ca_type, company_id)
+						values ('%s', %f, 0, %f, '%s', 'customer', '%s')";
+				$rc = $db->execute($sql, $this->newId(), $saleMoney, $saleMoney, $customerId, 
+						$companyId);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+			}
+			
+			// 应收明细账
+			$sql = "insert into t_receivables_detail (id, rv_money, act_money, balance_money,
+					ca_id, ca_type, date_created, ref_number, ref_type, biz_date, company_id)
+					values('%s', %f, 0, %f, '%s', 'customer', now(), '%s', '销售出库', '%s', '%s')";
+			
+			$rc = $db->execute($sql, $this->newId(), $saleMoney, $saleMoney, $customerId, $ref, 
+					$bizDT, $companyId);
+			if ($rc === false) {
+				return $this->sqlError(__METHOD__, __LINE__);
+			}
+		} else if ($receivingType == 1) {
+			// 现金收款
+			$inCash = $saleMoney;
+			
+			$sql = "select in_money, out_money, balance_money
+					from t_cash
+					where biz_date = '%s' and company_id = '%s' ";
+			$data = $db->query($sql, $bizDT, $companyId);
+			if (! $data) {
+				// 当天首次发生现金业务
+				$sql = "select sum(in_money) as sum_in_money, sum(out_money) as sum_out_money
+							from t_cash
+							where biz_date <= '%s' and company_id = '%s' ";
+				$data = $db->query($sql, $bizDT, $companyId);
+				$sumInMoney = $data[0]["sum_in_money"];
+				$sumOutMoney = $data[0]["sum_out_money"];
+				if (! $sumInMoney) {
+					$sumInMoney = 0;
+				}
+				if (! $sumOutMoney) {
+					$sumOutMoney = 0;
+				}
+				
+				$balanceCash = $sumInMoney - $sumOutMoney + $inCash;
+				$sql = "insert into t_cash(in_money, balance_money, biz_date, company_id)
+							values (%f, %f, '%s', '%s')";
+				$rc = $db->execute($sql, $inCash, $balanceCash, $bizDT, $companyId);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+				
+				// 记现金明细账
+				$sql = "insert into t_cash_detail(in_money, balance_money, biz_date, ref_type,
+								ref_number, date_created, company_id)
+							values (%f, %f, '%s', '销售出库', '%s', now(), '%s')";
+				$rc = $db->execute($sql, $inCash, $balanceCash, $bizDT, $ref, $companyId);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+			} else {
+				$balanceCash = $data[0]["balance_money"] + $inCash;
+				$sumInMoney = $data[0]["in_money"] + $inCash;
+				$sql = "update t_cash
+						set in_money = %f, balance_money = %f
+						where biz_date = '%s' and company_id = '%s' ";
+				$rc = $db->execute($sql, $sumInMoney, $balanceCash, $bizDT, $companyId);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+				
+				// 记现金明细账
+				$sql = "insert into t_cash_detail(in_money, balance_money, biz_date, ref_type,
+							ref_number, date_created, company_id)
+						values (%f, %f, '%s', '销售出库', '%s', now(), '%s')";
+				$rc = $db->execute($sql, $inCash, $balanceCash, $bizDT, $ref, $companyId);
+				if ($rc === false) {
+					return $this->sqlError(__METHOD__, __LINE__);
+				}
+			}
+			
+			// 调整业务日期之后的现金总账和明细账的余额
+			$sql = "update t_cash
+					set balance_money = balance_money + %f
+					where biz_date > '%s' and company_id = '%s' ";
+			$rc = $db->execute($sql, $inCash, $bizDT, $companyId);
+			if ($rc === false) {
+				return $this->sqlError(__METHOD__, __LINE__);
+			}
+			
+			$sql = "update t_cash_detail
+					set balance_money = balance_money + %f
+					where biz_date > '%s' and company_id = '%s' ";
+			$rc = $db->execute($sql, $inCash, $bizDT, $companyId);
+			if ($rc === false) {
+				return $this->sqlError(__METHOD__, __LINE__);
+			}
+		} else if ($receivingType == 2) {
+			// 2: 用预收款支付
+			
+			$outMoney = $saleMoney;
+			
+			// 预收款总账
+			$sql = "select out_money, balance_money from t_pre_receiving
+						where customer_id = '%s' and company_id = '%s' ";
+			$data = $db->query($sql, $customerId, $companyId);
+			$totalOutMoney = $data[0]["out_money"];
+			if (! $totalOutMoney) {
+				$totalOutMoney = 0;
+			}
+			$totalBalanceMoney = $data[0]["balance_money"];
+			if (! $totalBalanceMoney) {
+				$totalBalanceMoney = 0;
+			}
+			if ($totalBalanceMoney < $outMoney) {
+				return $this->bad("付余款余额是{$totalBalanceMoney}元，小于销售金额，无法付款");
+			}
+			
+			$totalOutMoney += $outMoney;
+			$totalBalanceMoney -= $outMoney;
+			$sql = "update t_pre_receiving
+					set out_money = %f, balance_money = %f
+					where customer_id = '%s' and company_id = '%s' ";
+			$rc = $db->execute($sql, $totalOutMoney, $totalBalanceMoney, $customerId, $companyId);
+			if ($rc === false) {
+				return $this->sqlError(__METHOD__, __LINE__);
+			}
+			
+			// 预收款明细账
+			$sql = "insert into t_pre_receiving_detail (id, customer_id, out_money, balance_money,
+						biz_date, date_created, ref_number, ref_type, biz_user_id, input_user_id, company_id)
+					values ('%s', '%s', %f, %f, '%s', now(), '%s', '销售出库', '%s', '%s', '%s')";
+			$idGen = new IdGenService();
+			$us = new UserService();
+			$rc = $db->execute($sql, $this->newId(), $customerId, $outMoney, $totalBalanceMoney, 
+					$bizDT, $ref, $bizUserId, $loginUserId, $companyId);
+			if ($rc === false) {
+				return $this->sqlError(__METHOD__, __LINE__);
+			}
+		}
+		
+		// 把单据本身设置为已经提交出库
+		$sql = "select sum(inventory_money) as sum_inventory_money
+				from t_ws_bill_detail
+				where wsbill_id = '%s' ";
+		$data = $db->query($sql, $id);
+		$sumInventoryMoney = $data[0]["sum_inventory_money"];
+		if (! $sumInventoryMoney) {
+			$sumInventoryMoney = 0;
+		}
+		
+		$profit = $saleMoney - $sumInventoryMoney;
+		
+		$sql = "update t_ws_bill
+				set bill_status = 1000, inventory_money = %f, profit = %f
+				where id = '%s' ";
+		$rc = $db->execute($sql, $sumInventoryMoney, $profit, $id);
+		if ($rc === false) {
+			return $this->sqlError(__METHOD__, __LINE__);
+		}
+		
+		$params["ref"] = $ref;
+		
+		// 操作成功
+		return null;
+	}
 }
