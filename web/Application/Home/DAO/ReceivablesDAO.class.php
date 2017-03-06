4b9396c1c969cdc7c2dc90d49a946527c1448799
CRM8000 <crm8000@qq.com>
2017-03-06 09:36:32 +0800

    代码重构 - 应收账款管理

--- web/Application/Home/DAO/ReceivablesDAO.class.php
+++ web/Application/Home/DAO/ReceivablesDAO.class.php
@@ -32,7 +32,7 @@ class ReceivablesDAO extends PSIBaseExDAO {
 			$queryParams = array();
 			
 			$ds = new DataOrgDAO($db);
-			$rs = $ds->buildSQL(FIdConst::RECEIVING, "t_customer_category");
+			$rs = $ds->buildSQL(FIdConst::RECEIVING, "t_customer_category", $loginUserId);
 			if ($rs) {
 				$sql .= " where " . $rs[0];
 				$queryParams = $rs[1];
@@ -49,7 +49,7 @@ class ReceivablesDAO extends PSIBaseExDAO {
 			$queryParams = array();
 			
 			$ds = new DataOrgDAO($db);
-			$rs = $ds->buildSQL(FIdConst::RECEIVING, "t_supplier_category");
+			$rs = $ds->buildSQL(FIdConst::RECEIVING, "t_supplier_category", $loginUserId);
 			if ($rs) {
 				$sql .= " where " . $rs[0];
 				$queryParams = $rs[1];
@@ -64,5 +64,139 @@ class ReceivablesDAO extends PSIBaseExDAO {
 		}
 		
 		return $result;
+	}
+
+	/**
+	 * 应收账款列表
+	 */
+	public function rvList($params) {
+		$db = $this->db;
+		
+		$loginUserId = $params["loginUserId"];
+		if ($this->loginUserIdNotExists($loginUserId)){
+			return $this->emptyResult();
+		}
+		
+		$caType = $params["caType"];
+		$categoryId = $params["categoryId"];
+		$page = $params["page"];
+		$start = $params["start"];
+		$limit = $params["limit"];
+		
+		if ($caType == "customer") {
+			$queryParams = array();
+			$sql = "select r.id, r.ca_id, c.code, c.name, r.act_money, r.balance_money, r.rv_money
+					from t_receivables r, t_customer c
+					where (r.ca_type = '%s' and r.ca_id = c.id)";
+			$queryParams[] = $caType;
+			
+			$ds = new DataOrgDAO($db);
+			$rs = $ds->buildSQL(FIdConst::RECEIVING, "c", $loginUserId);
+			if ($rs) {
+				$sql .= " and " . $rs[0];
+				$queryParams = array_merge($queryParams, $rs[1]);
+			}
+			
+			if ($categoryId) {
+				$sql .= " and c.category_id = '%s' ";
+				$queryParams[] = $categoryId;
+			}
+			$sql .= " order by c.code
+					limit %d , %d ";
+			$queryParams[] = $start;
+			$queryParams[] = $limit;
+			$data = $db->query($sql, $queryParams);
+			$result = array();
+			
+			foreach ( $data as $i => $v ) {
+				$result[$i]["id"] = $v["id"];
+				$result[$i]["caId"] = $v["ca_id"];
+				$result[$i]["code"] = $v["code"];
+				$result[$i]["name"] = $v["name"];
+				$result[$i]["actMoney"] = $v["act_money"];
+				$result[$i]["balanceMoney"] = $v["balance_money"];
+				$result[$i]["rvMoney"] = $v["rv_money"];
+			}
+			
+			$queryParams = array();
+			$sql = "select count(*) as cnt
+					from t_receivables r, t_customer c
+					where r.ca_type = '%s'  and r.ca_id = c.id";
+			$queryParams[] = $caType;
+			
+			$ds = new DataOrgDAO($db);
+			$rs = $ds->buildSQL(FIdConst::RECEIVING, "c", $loginUserId);
+			if ($rs) {
+				$sql .= " and " . $rs[0];
+				$queryParams = array_merge($queryParams, $rs[1]);
+			}
+			
+			if ($categoryId) {
+				$sql .= " and c.category_id = '%s' ";
+				$queryParams[] = $categoryId;
+			}
+			$data = $db->query($sql, $queryParams);
+			$cnt = $data[0]["cnt"];
+			
+			return array(
+					"dataList" => $result,
+					"totalCount" => $cnt
+			);
+		} else {
+			$queryParams = array();
+			$sql = "select r.id, r.ca_id, c.code, c.name, r.act_money, r.balance_money, r.rv_money
+					from t_receivables r, t_supplier c
+					where r.ca_type = '%s' and r.ca_id = c.id ";
+			$queryParams[] = $caType;
+			$ds = new DataOrgDAO($db);
+			$rs = $ds->buildSQL(FIdConst::RECEIVING, "c", $loginUserId);
+			if ($rs) {
+				$sql .= " and " . $rs[0];
+				$queryParams = array_merge($queryParams, $rs[1]);
+			}
+			if ($categoryId) {
+				$sql .= " and c.category_id = '%s' ";
+				$queryParams[] = $categoryId;
+			}
+			$sql .= " order by c.code
+					limit %d , %d ";
+			$queryParams[] = $start;
+			$queryParams[] = $limit;
+			$data = $db->query($sql, $queryParams);
+			$result = array();
+			
+			foreach ( $data as $i => $v ) {
+				$result[$i]["id"] = $v["id"];
+				$result[$i]["caId"] = $v["ca_id"];
+				$result[$i]["code"] = $v["code"];
+				$result[$i]["name"] = $v["name"];
+				$result[$i]["actMoney"] = $v["act_money"];
+				$result[$i]["balanceMoney"] = $v["balance_money"];
+				$result[$i]["rvMoney"] = $v["rv_money"];
+			}
+			
+			$queryParams = array();
+			$sql = "select count(*) as cnt
+					from t_receivables r, t_supplier c
+					where r.ca_type = '%s'  and r.ca_id = c.id";
+			$queryParams[] = $caType;
+			$ds = new DataOrgDAO($db);
+			$rs = $ds->buildSQL(FIdConst::RECEIVING, "c", $loginUserId);
+			if ($rs) {
+				$sql .= " and " . $rs[0];
+				$queryParams = array_merge($queryParams, $rs[1]);
+			}
+			if ($categoryId) {
+				$sql .= " and c.category_id = '%s' ";
+				$queryParams[] = $categoryId;
+			}
+			$data = $db->query($sql, $queryParams);
+			$cnt = $data[0]["cnt"];
+			
+			return array(
+					"dataList" => $result,
+					"totalCount" => $cnt
+			);
+		}
 	}
 }
