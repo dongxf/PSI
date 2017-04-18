d4bd8cea819f4c3bef903fc3c9ad2b099ef32db7
CRM8000 <crm8000@qq.com>
2017-04-18 17:21:07 +0800

    代码注释

--- web/Application/Home/DAO/PSIBaseDAO.class.php
+++ web/Application/Home/DAO/PSIBaseDAO.class.php
@@ -14,6 +14,7 @@ class PSIBaseDAO {
 	 *
 	 * @param string $msg
 	 *        	错误信息
+	 * @return array
 	 */
 	protected function bad($msg) {
 		return array(
@@ -24,14 +25,23 @@ class PSIBaseDAO {
 
 	/**
 	 * 数据库错误
+	 *
+	 * @param string $methodName
+	 *        	方法名称
+	 * @param int $codeLine
+	 *        	代码行号
+	 * @return array
 	 */
-	protected function sqlError($fileName, $codeLine) {
-		$info = "数据库错误，请联系管理员<br />错误定位：{$fileName} - {$codeLine}行";
+	protected function sqlError($methodName, $codeLine) {
+		$info = "数据库错误，请联系管理员<br />错误定位：{$methodName} - {$codeLine}行";
 		return $this->bad($info);
 	}
 
 	/**
 	 * 把时间类型格式化成类似2015-08-13的格式
+	 *
+	 * @param string $d        	
+	 * @return string
 	 */
 	protected function toYMD($d) {
 		return date("Y-m-d", strtotime($d));
@@ -42,6 +52,7 @@ class PSIBaseDAO {
 	 *
 	 * @param string $info
 	 *        	附加信息
+	 * @return array
 	 */
 	protected function todo($info = null) {
 		if ($info) {
