e97ee281b77fdbae7a76c0c6735e10f59d19ae45
CRM8000 <crm8000@qq.com>
2017-05-24 10:19:00 +0800

    ChangeUserPasswordForm.js代码微调

--- web/Public/Scripts/PSI/User/ChangeUserPasswordForm.js
+++ web/Public/Scripts/PSI/User/ChangeUserPasswordForm.js
@@ -2,11 +2,7 @@
  * 修改用户密码
  */
 Ext.define("PSI.User.ChangeUserPasswordForm", {
-	extend : "Ext.window.Window",
-
-	config : {
-		entity : null
-	},
+	extend : "PSI.AFX.BaseDialogForm",
 
 	/**
 	 * 初始化组件
@@ -17,91 +13,100 @@ Ext.define("PSI.User.ChangeUserPasswordForm", {
 
 		Ext.apply(me, {
 			title : "修改密码",
-			modal : true,
-			onEsc : Ext.emptyFn,
 			width : 400,
 			height : 200,
 			layout : "fit",
-			defaultFocus : "editPassword",
 			items : [{
-						id : "editForm",
-						xtype : "form",
-						layout : {
-							type : "table",
-							columns : 1
-						},
-						height : "100%",
-						bodyPadding : 5,
-						defaultType : 'textfield',
-						fieldDefaults : {
-							labelWidth : 60,
-							labelAlign : "right",
-							labelSeparator : "",
-							msgTarget : 'side',
-							width : 370
-						},
-						items : [{
-									xtype : "hidden",
-									name : "id",
-									value : entity.id
-								}, {
-									id : "editLoginName",
-									fieldLabel : "登录名",
-									value : entity.loginName,
-									xtype : "displayfield"
-								}, {
-									id : "editName",
-									fieldLabel : "姓名",
-									value : entity.name,
-									xtype : "displayfield"
-								}, {
-									id : "editPassword",
-									fieldLabel : "密码",
-									allowBlank : false,
-									blankText : "没有输入密码",
-									beforeLabelTextTpl : PSI.Const.REQUIRED,
-									inputType : "password",
-									name : "password",
-									listeners : {
-										specialkey : {
-											fn : me.onEditPasswordSpecialKey,
-											scope : me
-										}
-									}
-								}, {
-									id : "editConfirmPassword",
-									fieldLabel : "确认密码",
-									allowBlank : false,
-									blankText : "没有输入确认密码",
-									beforeLabelTextTpl : PSI.Const.REQUIRED,
-									inputType : "password",
-									listeners : {
-										specialkey : {
-											fn : me.onEditConfirmPasswordSpecialKey,
-											scope : me
-										}
-									}
-								}],
-						buttons : [{
-									text : "确定",
-									formBind : true,
-									iconCls : "PSI-button-ok",
-									handler : me.onOK,
+				id : "PSI_User_ChangeUserPasswordForm_editForm",
+				xtype : "form",
+				layout : {
+					type : "table",
+					columns : 1
+				},
+				height : "100%",
+				bodyPadding : 5,
+				defaultType : 'textfield',
+				fieldDefaults : {
+					labelWidth : 60,
+					labelAlign : "right",
+					labelSeparator : "",
+					msgTarget : 'side',
+					width : 370
+				},
+				items : [{
+							xtype : "hidden",
+							name : "id",
+							value : entity.id
+						}, {
+							fieldLabel : "登录名",
+							value : entity.loginName,
+							xtype : "displayfield"
+						}, {
+							fieldLabel : "姓名",
+							value : entity.name,
+							xtype : "displayfield"
+						}, {
+							id : "PSI_User_ChangeUserPasswordForm_editPassword",
+							fieldLabel : "密码",
+							allowBlank : false,
+							blankText : "没有输入密码",
+							beforeLabelTextTpl : PSI.Const.REQUIRED,
+							inputType : "password",
+							name : "password",
+							listeners : {
+								specialkey : {
+									fn : me.onEditPasswordSpecialKey,
 									scope : me
-								}, {
-									text : "取消",
-									handler : function() {
-										me.close();
-									},
+								}
+							}
+						}, {
+							id : "PSI_User_ChangeUserPasswordForm_editConfirmPassword",
+							fieldLabel : "确认密码",
+							allowBlank : false,
+							blankText : "没有输入确认密码",
+							beforeLabelTextTpl : PSI.Const.REQUIRED,
+							inputType : "password",
+							listeners : {
+								specialkey : {
+									fn : me.onEditConfirmPasswordSpecialKey,
 									scope : me
-								}]
-					}]
+								}
+							}
+						}],
+				buttons : [{
+							text : "确定",
+							formBind : true,
+							iconCls : "PSI-button-ok",
+							handler : me.onOK,
+							scope : me
+						}, {
+							text : "取消",
+							handler : function() {
+								me.close();
+							},
+							scope : me
+						}]
+			}],
+			listeners : {
+				show : {
+					fn : me.onEditFormShow,
+					scope : me
+				}
+			}
 		});
 
 		me.callParent(arguments);
 
-		me.editPassword = Ext.getCmp("editPassword");
-		me.editConfirmPassword = Ext.getCmp("editConfirmPassword");
+		me.editPassword = Ext
+				.getCmp("PSI_User_ChangeUserPasswordForm_editPassword");
+		me.editConfirmPassword = Ext
+				.getCmp("PSI_User_ChangeUserPasswordForm_editConfirmPassword");
+		me.editForm = Ext.getCmp("PSI_User_ChangeUserPasswordForm_editForm");
+	},
+
+	onEditFormShow : function() {
+		var me = this;
+		me.editPassword.focus();
 	},
 
 	/**
@@ -112,43 +117,46 @@ Ext.define("PSI.User.ChangeUserPasswordForm", {
 		var pass = me.editPassword.getValue();
 		var pass2 = me.editConfirmPassword.getValue();
 		if (pass != pass2) {
-			PSI.MsgBox.showInfo("输入的密码和确认密码不一致，请重新输入", function() {
+			me.showInfo("输入的密码和确认密码不一致，请重新输入", function() {
 						me.editPassword.focus();
 					});
 
 			return;
 		}
 
-		var f = Ext.getCmp("editForm");
+		var f = me.editForm;
 		var el = f.getEl();
 		el.mask("数据保存中...");
 		f.submit({
-					url : PSI.Const.BASE_URL + "Home/User/changePassword",
+					url : me.URL("Home/User/changePassword"),
 					method : "POST",
 					success : function(form, action) {
 						el.unmask();
-						PSI.MsgBox.showInfo("成功修改密码", function() {
+						me.showInfo("成功修改密码", function() {
 									me.close();
 								});
 					},
 					failure : function(form, action) {
 						el.unmask();
-						PSI.MsgBox.showInfo(action.result.msg);
+						me.showInfo(action.result.msg);
 					}
 				});
 	},
 
 	onEditPasswordSpecialKey : function(field, e) {
+		var me = this;
+
 		if (e.getKey() == e.ENTER) {
-			Ext.getCmp("editConfirmPassword").focus();
+			me.editConfirmPassword.focus();
 		}
 	},
 
 	onEditConfirmPasswordSpecialKey : function(field, e) {
+		var me = this;
+
 		if (e.getKey() == e.ENTER) {
-			var f = Ext.getCmp("editForm");
-			if (f.getForm().isValid()) {
-				this.onOK();
+			if (me.editForm.getForm().isValid()) {
+				me.onOK();
 			}
 		}
 	}
