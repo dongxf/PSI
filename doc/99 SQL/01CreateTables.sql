8171464a36d8e5a23b75f1279bf428b9b01f7078
CRM8000 <crm8000@qq.com>
2017-05-30 20:51:01 +0800

    更新数据库文档：t_ws_bill新增字段deal_address

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -427,6 +427,7 @@ CREATE TABLE IF NOT EXISTS `t_ws_bill` (
   `data_org` varchar(255) DEFAULT NULL,
   `company_id` varchar(255) DEFAULT NULL,
   `memo` varchar(1000) DEFAULT NULL,
+  `deal_address` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
