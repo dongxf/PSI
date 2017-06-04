00cbed89da87fbfdc3977306fb18c09350c82e4c
CRM8000 <crm8000@qq.com>
2017-06-04 20:36:07 +0800

    把session持久化到数据库里

--- web/Application/Home/Service/UserService.class.php
+++ web/Application/Home/Service/UserService.class.php
@@ -29,32 +29,6 @@ class UserService extends PSIBaseExService {
 	}
 
 	/**
-	 * 使用Memcache作为Session持久化容器
-	 */
-	private function initMemcacheAsSessionSaveHandler() {
-		$useMemcache = getenv("PSI_USE_MEMCACHE") == 1;
-		
-		if (! $useMemcache) {
-			return;
-		}
-		
-		$saveHandler = ini_get("session.save_handler");
-		if ($saveHandler == "memcached") {
-			// 已经初始化过了
-			return;
-		}
-		
-		$savePath = getenv("PSI_SESSION_SAVE_PATH");
-		if (! $savePath) {
-			// 没有设置保存路径
-			return;
-		}
-		
-		ini_set("session.save_handler", "memcached");
-		ini_set("session.save_path", $savePath);
-	}
-
-	/**
 	 * 判断当前用户是否有$fid对应的权限
 	 *
 	 * @param string $fid
@@ -62,9 +36,6 @@ class UserService extends PSIBaseExService {
 	 * @return boolean true：有对应的权限
 	 */
 	public function hasPermission($fid = null) {
-		// TODO 需要和MoPaaS技术人员进一步咨询
-		// $this->initMemcacheAsSessionSaveHandler();
-		//
 		$result = session("loginUserId") != null;
 		if (! $result) {
 			return false;
