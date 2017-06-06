a73b5eef111c39d6303e67ccd1cc1aa17f8b1f49
CRM8000 <crm8000@qq.com>
2017-06-06 10:00:43 +0800

    更新数据库文档：t_customer_category新增字段ps_id

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -242,6 +242,7 @@ CREATE TABLE IF NOT EXISTS `t_customer_category` (
   `parent_id` varchar(255) DEFAULT NULL,
   `data_org` varchar(255) DEFAULT NULL,
   `company_id` varchar(255) DEFAULT NULL,
+  `ps_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
