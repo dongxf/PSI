36b7f0b2b999a81cc238e8ebac38e0512251ff78
CRM8000 <crm8000@qq.com>
2017-06-10 13:00:05 +0800

    重写登录页面

--- web/Public/Scripts/PSI/Const.js
+++ web/Public/Scripts/PSI/Const.js
@@ -4,6 +4,6 @@ Ext.define("PSI.Const", {
 		LOADING : "数据加载中...",
 		SAVING : "数据保存中...",
 		BASE_URL : "",
-		VERSION : "PSI 2017 - build201706101241"
+		VERSION : "PSI 2017 - build201706101259"
 	}
 });
