aeed4331e26aee6cc54c7b3dd5f7b406e1279ca5
CRM8000 <crm8000@qq.com>
2017-06-13 09:03:40 +0800

    界面风格调整

--- web/Public/Scripts/PSI/SaleOrder/SOEditForm.js
+++ web/Public/Scripts/PSI/SaleOrder/SOEditForm.js
@@ -584,6 +584,7 @@ Ext.define("PSI.SaleOrder.SOEditForm", {
 								items : [{
 									icon : PSI.Const.BASE_URL
 											+ "Public/Images/icons/delete.png",
+									tooltip : "删除当前记录",
 									handler : function(grid, row) {
 										var store = grid.getStore();
 										store.remove(store.getAt(row));
@@ -603,7 +604,8 @@ Ext.define("PSI.SaleOrder.SOEditForm", {
 								xtype : "actioncolumn",
 								items : [{
 									icon : PSI.Const.BASE_URL
-											+ "Public/Images/icons/add.png",
+											+ "Public/Images/icons/insert.png",
+									tooltip : "在当前记录之前插入新记录",
 									handler : function(grid, row) {
 										var store = grid.getStore();
 										store.insert(row, [{
@@ -622,7 +624,8 @@ Ext.define("PSI.SaleOrder.SOEditForm", {
 								xtype : "actioncolumn",
 								items : [{
 									icon : PSI.Const.BASE_URL
-											+ "Public/Images/icons/add_detail.png",
+											+ "Public/Images/icons/add.png",
+									tooltip : "在当前记录之后新增记录",
 									handler : function(grid, row) {
 										var store = grid.getStore();
 										store.insert(row + 1, [{
