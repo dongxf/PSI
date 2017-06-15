d9a16e4182ef99e0713a41daa4994005452665b4
CRM8000 <crm8000@qq.com>
2017-06-15 09:52:16 +0800

    界面风格调整

--- web/Public/Scripts/PSI/Sale/SRMainForm.js
+++ web/Public/Scripts/PSI/Sale/SRMainForm.js
@@ -10,12 +10,14 @@ Ext.define("PSI.Sale.SRMainForm", {
 		Ext.apply(me, {
 					tbar : me.getToolbarCmp(),
 					items : [{
+								id : "panelQueryCmp",
 								region : "north",
 								height : 90,
 								layout : "fit",
 								border : 0,
 								title : "查询条件",
 								collapsible : true,
+								collapseMode : "mini",
 								layout : {
 									type : "table",
 									columns : 5
@@ -183,6 +185,18 @@ Ext.define("PSI.Sale.SRMainForm", {
 								width : 100,
 								margin : "5, 0, 0, 10",
 								handler : me.onClearQuery,
+								scope : me
+							}]
+				}, {
+					xtype : "container",
+					items : [{
+								xtype : "button",
+								text : "隐藏查询条件栏",
+								width : 100,
+								margin : "5 0 0 10",
+								handler : function() {
+									Ext.getCmp("panelQueryCmp").collapse();
+								},
 								scope : me
 							}]
 				}];
@@ -755,7 +769,8 @@ Ext.define("PSI.Sale.SRMainForm", {
 		}
 		var bill = item[0];
 
-		var url = PSI.Const.BASE_URL + "Home/Sale/srBillPdf?ref=" + bill.get("ref");
+		var url = PSI.Const.BASE_URL + "Home/Sale/srBillPdf?ref="
+				+ bill.get("ref");
 		window.open(url);
 	}
 });
