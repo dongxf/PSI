7bbfea72a2bb52d2e7732cfc337a9eb6ac27117a
CRM8000 <crm8000@qq.com>
2017-06-15 09:28:01 +0800

    界面风格调整

--- web/Public/Scripts/PSI/PurchaseOrder/POMainForm.js
+++ web/Public/Scripts/PSI/PurchaseOrder/POMainForm.js
@@ -19,6 +19,7 @@ Ext.define("PSI.PurchaseOrder.POMainForm", {
 		Ext.apply(me, {
 			tbar : me.getToolbarCmp(),
 			items : [{
+						id : "panelQueryCmp",
 						region : "north",
 						height : 90,
 						layout : "fit",
@@ -232,6 +233,18 @@ Ext.define("PSI.PurchaseOrder.POMainForm", {
 						width : 100,
 						margin : "5, 0, 0, 10",
 						handler : me.onClearQuery,
+						scope : me
+					}]
+		}, {
+			xtype : "container",
+			items : [{
+						xtype : "button",
+						text : "隐藏查询条件栏",
+						width : 100,
+						margin : "5 0 0 10",
+						handler : function() {
+							Ext.getCmp("panelQueryCmp").collapse();
+						},
 						scope : me
 					}]
 		}];
