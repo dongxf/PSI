151dc425785eeca8e35e8707918c2b461a71b592
CRM8000 <crm8000@qq.com>
2017-06-17 16:44:21 +0800

    商品 - 编辑的时候刷新界面之前给出提示

--- web/Public/Scripts/PSI/Goods/SafetyInventoryEditForm.js
+++ web/Public/Scripts/PSI/Goods/SafetyInventoryEditForm.js
@@ -75,6 +75,10 @@ Ext.define("PSI.Goods.SafetyInventoryEditForm", {
 						show : {
 							fn : me.onWndShow,
 							scope : me
+						},
+						close : {
+							fn : me.onWndClose,
+							scope : me
 						}
 					}
 				});
@@ -82,8 +86,20 @@ Ext.define("PSI.Goods.SafetyInventoryEditForm", {
 		me.callParent(arguments);
 	},
 
+	onWindowBeforeUnload : function(e) {
+		return (window.event.returnValue = e.returnValue = '确认离开当前页面？');
+	},
+
+	onWndClose : function() {
+		var me = this;
+
+		Ext.get(window).un('beforeunload', me.onWindowBeforeUnload);
+	},
+
 	onWndShow : function() {
 		var me = this;
+
+		Ext.get(window).on('beforeunload', me.onWindowBeforeUnload);
 
 		var el = me.getEl() || Ext.getBody();
 		el.mask(PSI.Const.LOADING);
