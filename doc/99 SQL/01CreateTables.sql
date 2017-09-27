49e8724a528c5a7f2ef877c2f407b933d9c7c732
CRM8000 <crm8000@qq.com>
2017-09-27 15:07:41 +0800

    更新数据库文档，供应商新增税率字段

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -147,6 +147,7 @@ CREATE TABLE IF NOT EXISTS `t_supplier` (
   `note` varchar(255) DEFAULT NULL,
   `data_org` varchar(255) DEFAULT NULL,
   `company_id` varchar(255) DEFAULT NULL,
+  `tax_rate` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
