82c093375f13443d6915601b455cd1a8425e4dab
CRM8000 <crm8000@qq.com>
2017-10-07 17:34:28 +0800

    修正bug：Some data has already been output, can't send PDFfile

--- web/Application/Home/Service/SRBillService.class.php
+++ web/Application/Home/Service/SRBillService.class.php
@@ -226,6 +226,8 @@ class SRBillService extends PSIBaseExService {
 			return;
 		}
 		
+		ob_start();
+		
 		$ps = new PDFService();
 		$pdf = $ps->getInstance();
 		$pdf->SetTitle("销售退货入库，单号：{$ref}");
@@ -284,6 +286,8 @@ class SRBillService extends PSIBaseExService {
 		
 		$html .= '</table>';
 		$pdf->writeHTML($html, true, false, true, false, '');
+		
+		ob_end_clean();
 		
 		$pdf->Output("$ref.pdf", "I");
 	}
