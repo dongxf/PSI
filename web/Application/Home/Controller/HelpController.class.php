56991a0e359c4903c965ebb925ead25224298ab8
CRM8000 <crm8000@qq.com>
2017-10-18 09:41:29 +0800

    更新帮助 - 采购订单

--- web/Application/Home/Controller/HelpController.class.php
+++ web/Application/Home/Controller/HelpController.class.php
@@ -69,6 +69,10 @@ class HelpController extends PSIBaseController {
 				$bs->insertBizlog("访问帮助页面：业务设置", "帮助");
 				redirect("/help/02-03.html");
 				break;
+			case "sobill" :
+				$bs->insertBizlog("访问帮助页面：采购订单", "帮助");
+				redirect("/help/20-01.html");
+				break;
 			default :
 				$bs->insertBizlog("通过主菜单进入帮助页面", "帮助");
 				redirect("/help");
--- web/Public/Scripts/PSI/PurchaseOrder/POMainForm.js
+++ web/Public/Scripts/PSI/PurchaseOrder/POMainForm.js
@@ -138,6 +138,12 @@ Ext.define("PSI.PurchaseOrder.POMainForm", {
 					scope : me,
 					handler : me.onPDF
 				}, "-", {
+					text : "帮助",
+					iconCls : "PSI-help",
+					handler : function() {
+						window.open(me.URL("/Home/Help/index?t=sobill"));
+					}
+				}, "-", {
 					text : "关闭",
 					iconCls : "PSI-button-exit",
 					handler : function() {
