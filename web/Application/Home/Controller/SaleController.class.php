9b43ee24758743f64ae9dd3f01ea29e1bba86a70
CRM8000 <crm8000@qq.com>
2018-02-02 10:12:40 +0800

    销售出库 - 权限细化到按钮

--- web/Application/Home/Controller/SaleController.class.php
+++ web/Application/Home/Controller/SaleController.class.php
@@ -53,6 +53,14 @@ class SaleController extends PSIBaseController {
 			
 			$this->assign("title", "销售出库");
 			
+			$this->assign("pAdd", $us->hasPermission(FIdConst::WAREHOUSING_SALE_ADD) ? "1" : "0");
+			$this->assign("pEdit", $us->hasPermission(FIdConst::WAREHOUSING_SALE_EDIT) ? "1" : "0");
+			$this->assign("pDelete", 
+					$us->hasPermission(FIdConst::WAREHOUSING_SALE_DELETE) ? "1" : "0");
+			$this->assign("pCommit", 
+					$us->hasPermission(FIdConst::WAREHOUSING_SALE_COMMIT) ? "1" : "0");
+			$this->assign("pGenPDF", $us->hasPermission(FIdConst::WAREHOUSING_SALE_PDF) ? "1" : "0");
+			
 			$this->display();
 		} else {
 			$this->gotoLoginPage("/Home/Sale/wsIndex");
--- web/Public/Scripts/PSI/Sale/WSMainForm.js
+++ web/Public/Scripts/PSI/Sale/WSMainForm.js
@@ -6,6 +6,10 @@
 Ext.define("PSI.Sale.WSMainForm", {
 	extend : "PSI.AFX.BaseMainExForm",
 
+	config : {
+		permission : null
+	},
+
 	initComponent : function() {
 		var me = this;
 
@@ -47,6 +51,21 @@ Ext.define("PSI.Sale.WSMainForm", {
 
 		me.callParent(arguments);
 
+		var bAdd = me.getPermission().add == "1";
+		Ext.getCmp("buttonAdd").setVisible(bAdd);
+
+		var bEdit = me.getPermission().edit == "1";
+		Ext.getCmp("buttonEdit").setVisible(bEdit);
+
+		var bDel = me.getPermission().del == "1";
+		Ext.getCmp("buttonDelete").setVisible(bDel);
+
+		var bCommit = me.getPermission().commit == "1";
+		Ext.getCmp("buttonCommit").setVisible(bCommit);
+
+		var bPDF = me.getPermission().genPDF == "1";
+		Ext.getCmp("buttonPDF").setVisible(bPDF);
+
 		me.refreshMainGrid();
 	},
 
@@ -54,6 +73,7 @@ Ext.define("PSI.Sale.WSMainForm", {
 		var me = this;
 		return [{
 					text : "新建销售出库单",
+					id : "buttonAdd",
 					iconCls : "PSI-button-add",
 					scope : me,
 					handler : me.onAddBill
