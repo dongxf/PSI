010e39cb421aa0e66213f5db94f1f3612dd39959
CRM8000 <crm8000@qq.com>
2018-03-16 10:03:28 +0800

    一键升级数据库：t_goods_bom商品数量改为decimal(19,8)

--- web/Application/Home/Service/UpdateDBService.class.php
+++ web/Application/Home/Service/UpdateDBService.class.php
@@ -157,6 +157,7 @@ class UpdateDBService extends PSIBaseService {
 		$this->update_20180313_02();
 		$this->update_20180314_01();
 		$this->update_20180314_02();
+		$this->update_20180316_01();
 		
 		$sql = "delete from t_psi_db_version";
 		$db->execute($sql);
@@ -177,6 +178,14 @@ class UpdateDBService extends PSIBaseService {
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	// ============================================
 	private function notForgot() {
+	}
+
+	private function update_20180316_01() {
+		// 本次更新： t_goods_bom商品数量改为decimal(19,8)
+		$tableName = "t_goods_bom";
+		
+		$fieldName = "sub_goods_count";
+		$this->changeFieldTypeToDeciaml($tableName, $fieldName);
 	}
 
 	private function update_20180314_02() {
