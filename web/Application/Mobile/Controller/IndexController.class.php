299c7cf20a9de6c66fa642c3d689d85e4c9e9583
CRM8000 <crm8000@qq.com>
2018-04-28 21:20:04 +0800

    H5 - 登录页面

--- web/Application/Mobile/Controller/IndexController.class.php
+++ web/Application/Mobile/Controller/IndexController.class.php
@@ -3,16 +3,25 @@
 namespace Mobile\Controller;
 
 use Think\Controller;
+use Home\Service\UserService;
 
 class IndexController extends Controller {
 
 	public function index() {
-		$this->assign("title", "首页");
-		$this->assign("uri", __ROOT__ . "/");
+		$us = new UserService();
 		
-		$dtFlag = getdate();
-		$this->assign("dtFlag", $dtFlag[0]);
-		
-		$this->display();
+		if ($us->hasPermission()) {
+			$this->assign("title", "首页");
+			$this->assign("uri", __ROOT__ . "/");
+			
+			$dtFlag = getdate();
+			$this->assign("dtFlag", $dtFlag[0]);
+			
+			$this->display();
+		} else {
+			$url = __ROOT__ . "/Mobile/User/login";
+			
+			redirect($url);
+		}
 	}
 }
