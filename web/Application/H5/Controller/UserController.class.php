cdbef20a341f9a32539cbabc349ada2f0b532939
CRM8000 <crm8000@qq.com>
2018-05-05 22:22:14 +0800

    H5 - 登录页面

--- web/Application/H5/Controller/UserController.class.php
+++ web/Application/H5/Controller/UserController.class.php
@@ -3,15 +3,19 @@
 namespace H5\Controller;
 
 use Think\Controller;
+use H5\Service\UserServiceH5;
 
 class UserController extends Controller {
 
 	public function doLogin() {
 		if (IS_POST) {
-			$this->ajaxReturn([
-					"success" => false,
-					"msg" => "TODO"
-			]);
+			$params = array(
+					"loginName" => I("post.loginName"),
+					"password" => I("post.password")
+			);
+			
+			$us = new UserServiceH5();
+			$this->ajaxReturn($us->doLogin($params));
 		}
 	}
 }
