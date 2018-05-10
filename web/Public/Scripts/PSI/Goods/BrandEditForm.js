ab90d1f499b335e48f330c7896851a9ac19bb401
CRM8000 <crm8000@qq.com>
2018-05-10 12:09:23 +0800

    商品品牌 - 编辑界面风格调整

--- web/Public/Scripts/PSI/Goods/BrandEditForm.js
+++ web/Public/Scripts/PSI/Goods/BrandEditForm.js
@@ -11,91 +11,110 @@ Ext.define("PSI.Goods.BrandEditForm", {
 		var me = this;
 		var entity = me.getEntity();
 
-		var title = entity == null ? "新增商品品牌" : "编辑商品品牌";
-		title = me.formatTitle(title);
-		var iconCls = entity === null ? "PSI-button-add" : "PSI-button-edit";
+		var t = entity == null ? "新增商品品牌" : "编辑商品品牌";
+		var f = entity == null
+				? "edit-form-create.png"
+				: "edit-form-update.png";
+		var logoHtml = "<img style='float:left;margin:10px 20px 0px 10px;width:48px;height:48px;' src='"
+				+ PSI.Const.BASE_URL
+				+ "Public/Images/"
+				+ f
+				+ "'></img>"
+				+ "<h2 style='color:#196d83'>"
+				+ t
+				+ "</h2>"
+				+ "<p style='color:#196d83'>标记 <span style='color:red;font-weight:bold'>*</span>的是必须录入数据的字段</p>";
 
 		Ext.apply(me, {
 			header : {
-				title : title,
-				height : 40,
-				iconCls : iconCls
+				title : me.formatTitle(PSI.Const.PROD_NAME),
+				height : 40
 			},
 			width : 400,
-			height : 150,
-			layout : "fit",
+			height : 240,
+			layout : "border",
 			items : [{
-				id : "PSI_Goods_BrandEditForm_editForm",
-				xtype : "form",
-				layout : {
-					type : "table",
-					columns : 1
-				},
-				height : "100%",
-				bodyPadding : 5,
-				defaultType : 'textfield',
-				fieldDefaults : {
-					labelWidth : 50,
-					labelAlign : "right",
-					labelSeparator : "",
-					msgTarget : 'side'
-				},
-				items : [{
-							xtype : "hidden",
-							name : "id",
-							value : entity === null ? null : entity.get("id")
-						}, {
-							id : "PSI_Goods_BrandEditForm_editName",
-							fieldLabel : "品牌",
-							labelWidth : 60,
-							allowBlank : false,
-							blankText : "没有输入品牌",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							name : "name",
-							value : entity === null ? null : entity.get("text"),
-							listeners : {
-								specialkey : {
-									fn : me.onEditNameSpecialKey,
+						region : "north",
+						border : 0,
+						height : 90,
+						html : logoHtml
+					}, {
+						region : "center",
+						border : 0,
+						id : "PSI_Goods_BrandEditForm_editForm",
+						xtype : "form",
+						layout : {
+							type : "table",
+							columns : 1
+						},
+						height : "100%",
+						bodyPadding : 5,
+						defaultType : 'textfield',
+						fieldDefaults : {
+							labelWidth : 50,
+							labelAlign : "right",
+							labelSeparator : "",
+							msgTarget : 'side'
+						},
+						items : [{
+									xtype : "hidden",
+									name : "id",
+									value : entity === null ? null : entity
+											.get("id")
+								}, {
+									id : "PSI_Goods_BrandEditForm_editName",
+									fieldLabel : "品牌",
+									labelWidth : 60,
+									allowBlank : false,
+									blankText : "没有输入品牌",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									name : "name",
+									value : entity === null ? null : entity
+											.get("text"),
+									listeners : {
+										specialkey : {
+											fn : me.onEditNameSpecialKey,
+											scope : me
+										}
+									},
+									width : 370
+								}, {
+									id : "PSI_Goods_BrandEditForm_editParentBrand",
+									xtype : "PSI_parent_brand_editor",
+									parentItem : me,
+									fieldLabel : "上级品牌",
+									labelWidth : 60,
+									listeners : {
+										specialkey : {
+											fn : me.onEditParentBrandSpecialKey,
+											scope : me
+										}
+									},
+									width : 370
+								}, {
+									id : "PSI_Goods_BrandEditForm_editParentBrandId",
+									xtype : "hidden",
+									name : "parentId",
+									value : entity === null ? null : entity
+											.get("parentId")
+								}],
+						buttons : [{
+									text : "确定",
+									formBind : true,
+									iconCls : "PSI-button-ok",
+									handler : me.onOK,
 									scope : me
-								}
-							},
-							width : 370
-						}, {
-							id : "PSI_Goods_BrandEditForm_editParentBrand",
-							xtype : "PSI_parent_brand_editor",
-							parentItem : me,
-							fieldLabel : "上级品牌",
-							labelWidth : 60,
-							listeners : {
-								specialkey : {
-									fn : me.onEditParentBrandSpecialKey,
+								}, {
+									text : "取消",
+									handler : function() {
+										PSI.MsgBox.confirm("请确认是否取消操作?",
+												function() {
+													me.close();
+												});
+									},
 									scope : me
-								}
-							},
-							width : 370
-						}, {
-							id : "PSI_Goods_BrandEditForm_editParentBrandId",
-							xtype : "hidden",
-							name : "parentId",
-							value : entity === null ? null : entity
-									.get("parentId")
-						}],
-				buttons : [{
-							text : "确定",
-							formBind : true,
-							iconCls : "PSI-button-ok",
-							handler : me.onOK,
-							scope : me
-						}, {
-							text : "取消",
-							handler : function() {
-								PSI.MsgBox.confirm("请确认是否取消操作?", function() {
-											me.close();
-										});
-							},
-							scope : me
-						}]
-			}],
+								}]
+					}],
 			listeners : {
 				show : {
 					fn : me.onEditFormShow,
