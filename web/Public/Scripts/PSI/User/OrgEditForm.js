37e4e9f7513bee7ec2632f73ae71f269fa34c686
CRM8000 <crm8000@qq.com>
2018-05-10 13:22:18 +0800

    用户管理 - 编辑界面风格调整

--- web/Public/Scripts/PSI/User/OrgEditForm.js
+++ web/Public/Scripts/PSI/User/OrgEditForm.js
@@ -11,109 +11,127 @@ Ext.define("PSI.User.OrgEditForm", {
 		var me = this;
 		var entity = me.getEntity();
 
-		var title = entity == null ? "新增组织机构" : "编辑组织机构";
-		title = me.formatTitle(title);
-
-		var iconCls = entity == null ? "PSI-button-add" : "PSI-button-edit";
+		var t = entity == null ? "新增组织机构" : "编辑组织机构";
+		var f = entity == null
+				? "edit-form-create.png"
+				: "edit-form-update.png";
+		var logoHtml = "<img style='float:left;margin:10px 20px 0px 10px;width:48px;height:48px;' src='"
+				+ PSI.Const.BASE_URL
+				+ "Public/Images/"
+				+ f
+				+ "'></img>"
+				+ "<h2 style='color:#196d83'>"
+				+ t
+				+ "</h2>"
+				+ "<p style='color:#196d83'>标记 <span style='color:red;font-weight:bold'>*</span>的是必须录入数据的字段</p>";
 
 		Ext.apply(me, {
 			header : {
-				title : title,
-				height : 40,
-				iconCls : iconCls
+				title : me.formatTitle(PSI.Const.PROD_NAME),
+				height : 40
 			},
 			width : 400,
-			height : 200,
-			layout : "fit",
+			height : 290,
+			layout : "border",
 			items : [{
-				id : "PSI_User_OrgEditForm_editForm",
-				xtype : "form",
-				layout : {
-					type : "table",
-					columns : 1
-				},
-				height : "100%",
-				bodyPadding : 5,
-				defaultType : 'textfield',
-				fieldDefaults : {
-					labelWidth : 60,
-					labelAlign : "right",
-					labelSeparator : "",
-					msgTarget : 'side'
-				},
-				items : [{
-							xtype : "hidden",
-							name : "id",
-							value : entity === null ? null : entity.get("id")
-						}, {
-							id : "PSI_User_OrgEditForm_editName",
-							fieldLabel : "名称",
-							allowBlank : false,
-							blankText : "没有输入名称",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							name : "name",
-							value : entity === null ? null : entity.get("text"),
-							listeners : {
-								specialkey : {
-									fn : me.onEditNameSpecialKey,
+						region : "north",
+						border : 0,
+						height : 90,
+						html : logoHtml
+					}, {
+						region : "center",
+						border : 0,
+						id : "PSI_User_OrgEditForm_editForm",
+						xtype : "form",
+						layout : {
+							type : "table",
+							columns : 1
+						},
+						height : "100%",
+						bodyPadding : 5,
+						defaultType : 'textfield',
+						fieldDefaults : {
+							labelWidth : 60,
+							labelAlign : "right",
+							labelSeparator : "",
+							msgTarget : 'side'
+						},
+						items : [{
+									xtype : "hidden",
+									name : "id",
+									value : entity === null ? null : entity
+											.get("id")
+								}, {
+									id : "PSI_User_OrgEditForm_editName",
+									fieldLabel : "名称",
+									allowBlank : false,
+									blankText : "没有输入名称",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									name : "name",
+									value : entity === null ? null : entity
+											.get("text"),
+									listeners : {
+										specialkey : {
+											fn : me.onEditNameSpecialKey,
+											scope : me
+										}
+									},
+									width : 370
+								}, {
+									id : "PSI_User_OrgEditForm_editParentOrg",
+									xtype : "PSI_parent_org_editor",
+									parentItem : me,
+									fieldLabel : "上级组织",
+									listeners : {
+										specialkey : {
+											fn : me.onEditParentOrgSpecialKey,
+											scope : me
+										}
+									},
+									width : 370
+								}, {
+									id : "PSI_User_OrgEditForm_editParentOrgId",
+									xtype : "hidden",
+									name : "parentId",
+									value : entity === null ? null : entity
+											.get("parentId")
+								}, {
+									id : "PSI_User_OrgEditForm_editOrgCode",
+									fieldLabel : "编码",
+									allowBlank : false,
+									blankText : "没有输入编码",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									name : "orgCode",
+									value : entity === null ? null : entity
+											.get("orgCode"),
+									listeners : {
+										specialkey : {
+											fn : me.onEditOrgCodeSpecialKey,
+											scope : me
+										}
+									},
+									width : 370
+								}, {
+									xtype : "displayfield",
+									fieldLabel : "说明",
+									value : "上级组织机构为空的时候，该组织机构是公司"
+								}],
+						buttons : [{
+									text : "确定",
+									formBind : true,
+									iconCls : "PSI-button-ok",
+									handler : me.onOK,
 									scope : me
-								}
-							},
-							width : 370
-						}, {
-							id : "PSI_User_OrgEditForm_editParentOrg",
-							xtype : "PSI_parent_org_editor",
-							parentItem : me,
-							fieldLabel : "上级组织",
-							listeners : {
-								specialkey : {
-									fn : me.onEditParentOrgSpecialKey,
+								}, {
+									text : "取消",
+									handler : function() {
+										me.confirm("请确认是否取消操作?", function() {
+													me.close();
+												});
+									},
 									scope : me
-								}
-							},
-							width : 370
-						}, {
-							id : "PSI_User_OrgEditForm_editParentOrgId",
-							xtype : "hidden",
-							name : "parentId",
-							value : entity === null ? null : entity
-									.get("parentId")
-						}, {
-							id : "PSI_User_OrgEditForm_editOrgCode",
-							fieldLabel : "编码",
-							allowBlank : false,
-							blankText : "没有输入编码",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							name : "orgCode",
-							value : entity === null ? null : entity
-									.get("orgCode"),
-							listeners : {
-								specialkey : {
-									fn : me.onEditOrgCodeSpecialKey,
-									scope : me
-								}
-							},
-							width : 370
-						}, {
-							xtype : "displayfield",
-							value : "上级组织机构为空的时候，该组织机构是公司"
-						}],
-				buttons : [{
-							text : "确定",
-							formBind : true,
-							iconCls : "PSI-button-ok",
-							handler : me.onOK,
-							scope : me
-						}, {
-							text : "取消",
-							handler : function() {
-								me.confirm("请确认是否取消操作?", function() {
-											me.close();
-										});
-							},
-							scope : me
-						}]
-			}],
+								}]
+					}],
 			listeners : {
 				show : {
 					fn : me.onEditFormShow,
--- web/Public/Scripts/PSI/User/UserEditForm.js
+++ web/Public/Scripts/PSI/User/UserEditForm.js
@@ -17,233 +17,265 @@ Ext.define("PSI.User.UserEditForm", {
 		var entity = me.getEntity();
 		me.adding = entity == null;
 
-		var title = entity == null ? "新增用户" : "编辑用户";
-		title = me.formatTitle(title);
+		var t = entity == null ? "新增用户" : "编辑用户";
+		var f = entity == null
+				? "edit-form-create.png"
+				: "edit-form-update.png";
+		var logoHtml = "<img style='float:left;margin:10px 20px 0px 10px;width:48px;height:48px;' src='"
+				+ PSI.Const.BASE_URL
+				+ "Public/Images/"
+				+ f
+				+ "'></img>"
+				+ "<h2 style='color:#196d83'>"
+				+ t
+				+ "</h2>"
+				+ "<p style='color:#196d83'>标记 <span style='color:red;font-weight:bold'>*</span>的是必须录入数据的字段</p>";
 
-		var iconCls = entity == null
-				? "PSI-button-add-user"
-				: "PSI-button-edit-user";
 		Ext.apply(me, {
 			header : {
-				title : title,
-				height : 40,
-				iconCls : iconCls
+				title : me.formatTitle(PSI.Const.PROD_NAME),
+				height : 40
 			},
 			modal : true,
 			onEsc : Ext.emptyFn,
 			width : 470,
-			height : entity == null ? 310 : 280,
-			layout : "fit",
+			height : me.adding ? 400 : 370,
+			layout : "border",
 			items : [{
-				id : "editForm",
-				xtype : "form",
-				layout : {
-					type : "table",
-					columns : 2
-				},
-				height : "100%",
-				bodyPadding : 5,
-				defaultType : 'textfield',
-				fieldDefaults : {
-					labelWidth : 60,
-					labelAlign : "right",
-					labelSeparator : "",
-					msgTarget : 'side'
-				},
-				items : [{
-							xtype : "hidden",
-							name : "id",
-							value : entity === null ? null : entity.id
-						}, {
-							id : "editLoginName",
-							fieldLabel : "登录名",
-							allowBlank : false,
-							blankText : "没有输入登录名",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							name : "loginName",
-							value : entity === null ? null : entity.loginName,
-							listeners : {
-								specialkey : {
-									fn : me.onEditSpecialKey,
+						region : "north",
+						border : 0,
+						height : 90,
+						html : logoHtml
+					}, {
+						region : "center",
+						border : 0,
+						id : "editForm",
+						xtype : "form",
+						layout : {
+							type : "table",
+							columns : 2
+						},
+						height : "100%",
+						bodyPadding : 5,
+						defaultType : 'textfield',
+						fieldDefaults : {
+							labelWidth : 60,
+							labelAlign : "right",
+							labelSeparator : "",
+							msgTarget : 'side'
+						},
+						items : [{
+									xtype : "hidden",
+									name : "id",
+									value : entity === null ? null : entity.id
+								}, {
+									id : "editLoginName",
+									fieldLabel : "登录名",
+									allowBlank : false,
+									blankText : "没有输入登录名",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									name : "loginName",
+									value : entity === null
+											? null
+											: entity.loginName,
+									listeners : {
+										specialkey : {
+											fn : me.onEditSpecialKey,
+											scope : me
+										}
+									}
+								}, {
+									id : "editName",
+									fieldLabel : "姓名",
+									allowBlank : false,
+									blankText : "没有输入姓名",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									name : "name",
+									value : entity === null
+											? null
+											: entity.name,
+									listeners : {
+										specialkey : {
+											fn : me.onEditSpecialKey,
+											scope : me
+										}
+									}
+								}, {
+									id : "editOrgCode",
+									fieldLabel : "编码",
+									allowBlank : false,
+									blankText : "没有输入编码",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									name : "orgCode",
+									value : entity === null
+											? null
+											: entity.orgCode,
+									listeners : {
+										specialkey : {
+											fn : me.onEditSpecialKey,
+											scope : me
+										}
+									},
+									colspan : 2
+								}, {
+									id : "editOrgName",
+									xtype : "PSI_org_editor",
+									fieldLabel : "所属组织",
+									allowBlank : false,
+									blankText : "没有选择组织机构",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									parentItem : this,
+									value : entity === null
+											? null
+											: entity.orgName,
+									listeners : {
+										specialkey : {
+											fn : me.onEditSpecialKey,
+											scope : me
+										}
+									},
+									colspan : 2,
+									width : 430
+								}, {
+									id : "editOrgId",
+									xtype : "hidden",
+									name : "orgId",
+									value : entity === null
+											? null
+											: entity.orgId
+								}, {
+									id : "editBirthday",
+									fieldLabel : "生日",
+									xtype : "datefield",
+									format : "Y-m-d",
+									name : "birthday",
+									value : entity === null
+											? null
+											: entity.birthday,
+									listeners : {
+										specialkey : {
+											fn : me.onEditSpecialKey,
+											scope : me
+										}
+									}
+								}, {
+									id : "editIdCardNumber",
+									fieldLabel : "身份证号",
+									name : "idCardNumber",
+									value : entity === null
+											? null
+											: entity.idCardNumber,
+									listeners : {
+										specialkey : {
+											fn : me.onEditSpecialKey,
+											scope : me
+										}
+									}
+								}, {
+									id : "editTel",
+									fieldLabel : "联系电话",
+									name : "tel",
+									value : entity === null ? null : entity.tel,
+									listeners : {
+										specialkey : {
+											fn : me.onEditSpecialKey,
+											scope : me
+										}
+									}
+								}, {
+									id : "editTel02",
+									fieldLabel : "备用电话",
+									name : "tel02",
+									value : entity === null
+											? null
+											: entity.tel02,
+									listeners : {
+										specialkey : {
+											fn : me.onEditSpecialKey,
+											scope : me
+										}
+									}
+								}, {
+									id : "editAddress",
+									fieldLabel : "家庭住址",
+									name : "address",
+									value : entity === null
+											? null
+											: entity.address,
+									listeners : {
+										specialkey : {
+											fn : me.onLastEditSpecialKey,
+											scope : me
+										}
+									},
+									colspan : 2,
+									width : 430
+								}, {
+									xtype : "radiogroup",
+									fieldLabel : "性别",
+									id : "editGender",
+									columns : 2,
+									items : [{
+										boxLabel : "男 ",
+										name : "gender",
+										inputValue : "男",
+										checked : entity === null
+												? true
+												: entity.gender == "男"
+									}, {
+										boxLabel : "女 ",
+										name : "gender",
+										inputValue : "女",
+										checked : entity === null
+												? false
+												: entity.gender == "女"
+									}],
+									width : 200
+								}, {
+									xtype : "radiogroup",
+									fieldLabel : "能否登录",
+									id : "editEnabled",
+									columns : 2,
+									items : [{
+										boxLabel : "允许登录",
+										name : "enabled",
+										inputValue : true,
+										checked : entity === null
+												? true
+												: entity.enabled == 1
+									}, {
+										boxLabel : "<span style='color:red'>禁止登录</span>",
+										name : "enabled",
+										inputValue : false,
+										checked : entity === null
+												? false
+												: entity.enabled != 1
+									}],
+									width : 240
+								}, {
+									xtype : "displayfield",
+									fieldLabel : "说明",
+									colspan : 2,
+									value : "新用户的默认登录密码是 123456",
+									hidden : !me.adding
+								}],
+						buttons : [{
+									text : "确定",
+									formBind : true,
+									iconCls : "PSI-button-ok",
+									handler : me.onOK,
 									scope : me
-								}
-							}
-						}, {
-							id : "editName",
-							fieldLabel : "姓名",
-							allowBlank : false,
-							blankText : "没有输入姓名",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							name : "name",
-							value : entity === null ? null : entity.name,
-							listeners : {
-								specialkey : {
-									fn : me.onEditSpecialKey,
+								}, {
+									text : "取消",
+									handler : function() {
+										PSI.MsgBox.confirm("请确认是否取消操作?",
+												function() {
+													me.close();
+												});
+									},
 									scope : me
-								}
-							}
-						}, {
-							id : "editOrgCode",
-							fieldLabel : "编码",
-							allowBlank : false,
-							blankText : "没有输入编码",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							name : "orgCode",
-							value : entity === null ? null : entity.orgCode,
-							listeners : {
-								specialkey : {
-									fn : me.onEditSpecialKey,
-									scope : me
-								}
-							},
-							colspan : 2
-						}, {
-							id : "editOrgName",
-							xtype : "PSI_org_editor",
-							fieldLabel : "所属组织",
-							allowBlank : false,
-							blankText : "没有选择组织机构",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							parentItem : this,
-							value : entity === null ? null : entity.orgName,
-							listeners : {
-								specialkey : {
-									fn : me.onEditSpecialKey,
-									scope : me
-								}
-							},
-							colspan : 2,
-							width : 430
-						}, {
-							id : "editOrgId",
-							xtype : "hidden",
-							name : "orgId",
-							value : entity === null ? null : entity.orgId
-						}, {
-							id : "editBirthday",
-							fieldLabel : "生日",
-							xtype : "datefield",
-							format : "Y-m-d",
-							name : "birthday",
-							value : entity === null ? null : entity.birthday,
-							listeners : {
-								specialkey : {
-									fn : me.onEditSpecialKey,
-									scope : me
-								}
-							}
-						}, {
-							id : "editIdCardNumber",
-							fieldLabel : "身份证号",
-							name : "idCardNumber",
-							value : entity === null
-									? null
-									: entity.idCardNumber,
-							listeners : {
-								specialkey : {
-									fn : me.onEditSpecialKey,
-									scope : me
-								}
-							}
-						}, {
-							id : "editTel",
-							fieldLabel : "联系电话",
-							name : "tel",
-							value : entity === null ? null : entity.tel,
-							listeners : {
-								specialkey : {
-									fn : me.onEditSpecialKey,
-									scope : me
-								}
-							}
-						}, {
-							id : "editTel02",
-							fieldLabel : "备用电话",
-							name : "tel02",
-							value : entity === null ? null : entity.tel02,
-							listeners : {
-								specialkey : {
-									fn : me.onEditSpecialKey,
-									scope : me
-								}
-							}
-						}, {
-							id : "editAddress",
-							fieldLabel : "家庭住址",
-							name : "address",
-							value : entity === null ? null : entity.address,
-							listeners : {
-								specialkey : {
-									fn : me.onLastEditSpecialKey,
-									scope : me
-								}
-							},
-							colspan : 2,
-							width : 430
-						}, {
-							xtype : "radiogroup",
-							fieldLabel : "性别",
-							id : "editGender",
-							columns : 2,
-							items : [{
-								boxLabel : "男 ",
-								name : "gender",
-								inputValue : "男",
-								checked : entity === null
-										? true
-										: entity.gender == "男"
-							}, {
-								boxLabel : "女 ",
-								name : "gender",
-								inputValue : "女",
-								checked : entity === null
-										? false
-										: entity.gender == "女"
-							}],
-							width : 200
-						}, {
-							xtype : "radiogroup",
-							fieldLabel : "能否登录",
-							id : "editEnabled",
-							columns : 2,
-							items : [{
-								boxLabel : "允许登录",
-								name : "enabled",
-								inputValue : true,
-								checked : entity === null
-										? true
-										: entity.enabled == 1
-							}, {
-								boxLabel : "<span style='color:red'>禁止登录</span>",
-								name : "enabled",
-								inputValue : false,
-								checked : entity === null
-										? false
-										: entity.enabled != 1
-							}],
-							width : 240
-						}, {
-							xtype : "displayfield",
-							fieldLabel : "说明",
-							colspan : 2,
-							value : "新用户的默认登录密码是 123456"
-						}],
-				buttons : [{
-							text : "确定",
-							formBind : true,
-							iconCls : "PSI-button-ok",
-							handler : me.onOK,
-							scope : me
-						}, {
-							text : "取消",
-							handler : function() {
-								PSI.MsgBox.confirm("请确认是否取消操作?", function() {
-											me.close();
-										});
-							},
-							scope : me
-						}]
-			}],
+								}]
+					}],
 			listeners : {
 				show : {
 					fn : me.onWndShow,
