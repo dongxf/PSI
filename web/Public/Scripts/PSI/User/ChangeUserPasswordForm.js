19a4f1c59e5e9334fa061f2a7740721bad744be1
CRM8000 <crm8000@qq.com>
2018-05-11 10:15:51 +0800

    用户管理 - 编辑界面风格调整

--- web/Public/Scripts/PSI/User/ChangeUserPasswordForm.js
+++ web/Public/Scripts/PSI/User/ChangeUserPasswordForm.js
@@ -11,86 +11,104 @@ Ext.define("PSI.User.ChangeUserPasswordForm", {
 		var me = this;
 		var entity = me.getEntity();
 
+		var t = "修改用户登录密码";
+		var f = "edit-form-password.png";
+		var logoHtml = "<img style='float:left;margin:20px 20px 0px 10px;width:48px;height:48px;' src='"
+				+ PSI.Const.BASE_URL
+				+ "Public/Images/"
+				+ f
+				+ "'></img>"
+				+ "<h2 style='color:#196d83'>"
+				+ t
+				+ "</h2>"
+				+ "<p style='color:#196d83'>标记 <span style='color:red;font-weight:bold'>*</span>的是必须录入数据的字段</p>";
+
 		Ext.apply(me, {
 			header : {
-				title : me.formatTitle("修改用户登录密码"),
-				height : 40,
-				iconCls : "PSI-button-change-password"
+				title : me.formatTitle(PSI.Const.PROD_NAME),
+				height : 40
 			},
 			width : 400,
-			height : 200,
-			layout : "fit",
+			height : 290,
+			layout : "border",
 			items : [{
-				id : "PSI_User_ChangeUserPasswordForm_editForm",
-				xtype : "form",
-				layout : {
-					type : "table",
-					columns : 1
-				},
-				height : "100%",
-				bodyPadding : 5,
-				defaultType : 'textfield',
-				fieldDefaults : {
-					labelWidth : 60,
-					labelAlign : "right",
-					labelSeparator : "",
-					msgTarget : 'side',
-					width : 370
-				},
-				items : [{
-							xtype : "hidden",
-							name : "id",
-							value : entity.id
-						}, {
-							fieldLabel : "登录名",
-							value : entity.loginName,
-							xtype : "displayfield"
-						}, {
-							fieldLabel : "姓名",
-							value : entity.name,
-							xtype : "displayfield"
-						}, {
-							id : "PSI_User_ChangeUserPasswordForm_editPassword",
-							fieldLabel : "密码",
-							allowBlank : false,
-							blankText : "没有输入密码",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							inputType : "password",
-							name : "password",
-							listeners : {
-								specialkey : {
-									fn : me.onEditPasswordSpecialKey,
+						region : "north",
+						border : 0,
+						height : 90,
+						html : logoHtml
+					}, {
+						region : "center",
+						border : 0,
+						id : "PSI_User_ChangeUserPasswordForm_editForm",
+						xtype : "form",
+						layout : {
+							type : "table",
+							columns : 1
+						},
+						height : "100%",
+						bodyPadding : 5,
+						defaultType : 'textfield',
+						fieldDefaults : {
+							labelWidth : 60,
+							labelAlign : "right",
+							labelSeparator : "",
+							msgTarget : 'side',
+							width : 370
+						},
+						items : [{
+									xtype : "hidden",
+									name : "id",
+									value : entity.id
+								}, {
+									fieldLabel : "登录名",
+									value : entity.loginName,
+									xtype : "displayfield"
+								}, {
+									fieldLabel : "姓名",
+									value : entity.name,
+									xtype : "displayfield"
+								}, {
+									id : "PSI_User_ChangeUserPasswordForm_editPassword",
+									fieldLabel : "密码",
+									allowBlank : false,
+									blankText : "没有输入密码",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									inputType : "password",
+									name : "password",
+									listeners : {
+										specialkey : {
+											fn : me.onEditPasswordSpecialKey,
+											scope : me
+										}
+									}
+								}, {
+									id : "PSI_User_ChangeUserPasswordForm_editConfirmPassword",
+									fieldLabel : "确认密码",
+									allowBlank : false,
+									blankText : "没有输入确认密码",
+									beforeLabelTextTpl : PSI.Const.REQUIRED,
+									inputType : "password",
+									listeners : {
+										specialkey : {
+											fn : me.onEditConfirmPasswordSpecialKey,
+											scope : me
+										}
+									}
+								}],
+						buttons : [{
+									text : "确定",
+									formBind : true,
+									iconCls : "PSI-button-ok",
+									handler : me.onOK,
 									scope : me
-								}
-							}
-						}, {
-							id : "PSI_User_ChangeUserPasswordForm_editConfirmPassword",
-							fieldLabel : "确认密码",
-							allowBlank : false,
-							blankText : "没有输入确认密码",
-							beforeLabelTextTpl : PSI.Const.REQUIRED,
-							inputType : "password",
-							listeners : {
-								specialkey : {
-									fn : me.onEditConfirmPasswordSpecialKey,
+								}, {
+									text : "取消",
+									handler : function() {
+										me.close();
+									},
 									scope : me
-								}
-							}
-						}],
-				buttons : [{
-							text : "确定",
-							formBind : true,
-							iconCls : "PSI-button-ok",
-							handler : me.onOK,
-							scope : me
-						}, {
-							text : "取消",
-							handler : function() {
-								me.close();
-							},
-							scope : me
-						}]
-			}],
+								}]
+					}],
 			listeners : {
 				show : {
 					fn : me.onEditFormShow,
