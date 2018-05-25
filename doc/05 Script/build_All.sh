010e465d35cf43ab94e8f92e0862935b605a4173
CRM8000 <crm8000@qq.com>
2018-05-25 14:46:11 +0800

    工具脚本 - 一键生成部署PC和移动端的全部文件

---
+++ doc/05 Script/build_All.sh
@@ -0,0 +1,27 @@
+# 删除旧文件
+cd /d/temp
+rm -rf /d/temp/PSI
+rm -rf /d/temp/PSI_Mobile
+
+# clone PSI
+git clone https://gitee.com/crm8000/PSI.git
+
+# clone PSI_Mobile
+git clone https://gitee.com/crm8000/PSI_Mobile.git
+
+# 删除不用发布的文件
+rm -rf /d/temp/PSI/doc
+rm -rf /d/temp/PSI/.git
+rm -rf /d/temp/PSI/static
+rm -rf /d/temp/PSI/m.html
+rm -rf /d/temp/PSI/service-worker.js
+
+cd PSI_Mobile
+npm install
+npm run build
+
+# 复制移动端文件到PSI主目录
+cp /d/temp/PSI_Mobile/dist/index.html /d/temp/PSI/m.html
+cp /d/temp/PSI_Mobile/dist/service-worker.js /d/temp/PSI
+mkdir /d/temp/PSI/static
+cp -r /d/temp/PSI_Mobile/dist/static/* /d/temp/PSI/static
