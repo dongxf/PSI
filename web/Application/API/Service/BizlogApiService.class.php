de05ea978b255c870aa3b330d7626924b3e3f9d7
CRM8000 <crm8000@qq.com>
2018-05-25 12:49:55 +0800

    API - 业务日志

---
+++ web/Application/API/Service/BizlogApiService.class.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace API\Service;
+
+use API\DAO\BizlogApiDAO;
+
+/**
+ * 业务日志API Service
+ *
+ * @author 李静波
+ */
+class BizlogApiService extends PSIApiBaseService {
+
+	private function getClientIP() {
+		return get_client_ip();
+	}
+
+	/**
+	 * 记录业务日志
+	 *
+	 * @param string $log
+	 *        	日志内容
+	 * @param string $category
+	 *        	日志分类
+	 */
+	public function insertBizlog($tokenId, $log, $category = "系统") {
+		if ($this->tokenIsInvalid($tokenId)) {
+			return;
+		}
+		
+		$params = array(
+				"loginUserId" => $this->getUserIdFromTokenId($tokenId),
+				"log" => $log,
+				"category" => $category,
+				"ip" => $this->getClientIP(),
+				"dataOrg" => $this->getDataOrgFromTokenId($tokenId),
+				"companyId" => $this->getCompanyIdFromTokenId($tokenId)
+		);
+		
+		$dao = new BizlogApiDAO($this->db());
+		return $dao->insertBizlog($params);
+	}
+}
