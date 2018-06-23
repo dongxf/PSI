608865bcb54ca6bd50a5c83bfcaa263ed6e4ba2e
CRM8000 <crm8000@qq.com>
2018-06-23 11:05:28 +0800

    t_supplier_category增加字段full_name

--- web/Application/Home/Service/InstallService.class.php
+++ web/Application/Home/Service/InstallService.class.php
@@ -228,6 +228,7 @@ class InstallService extends PSIBaseExService {
 				  `parent_id` varchar(255) DEFAULT NULL,
 				  `data_org` varchar(255) DEFAULT NULL,
 				  `company_id` varchar(255) DEFAULT NULL,
+				  `full_name` varchar(1000) DEFAULT NULL,
 				  PRIMARY KEY (`id`)
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 		";
