9ae39658e040670ceedb872a8e6d3f24e133818e
CRM8000 <crm8000@qq.com>
2018-08-25 16:37:54 +0800

    一键升级数据库 - 会计科目主菜单

--- doc/99 SQL/02InsertInitData.sql
+++ doc/99 SQL/02InsertInitData.sql
@@ -436,7 +436,7 @@ INSERT INTO `t_menu_item_h5` (`id`, `caption`, `fid`, `parent_id`, `show_order`)
 
 TRUNCATE TABLE `t_psi_db_version`;
 INSERT INTO `t_psi_db_version` (`db_version`, `update_dt`) VALUES
-('20180623-01', now());
+('20180825-01', now());
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- web/Application/Home/Service/PSIBaseService.class.php
+++ web/Application/Home/Service/PSIBaseService.class.php
@@ -176,5 +176,5 @@ class PSIBaseService {
 	/**
 	 * 当前数据库表结构版本
 	 */
-	protected $CURRENT_DB_VERSION = "20180623-01";
+	protected $CURRENT_DB_VERSION = "20180825-01";
 }
