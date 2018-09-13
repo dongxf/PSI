cb2c775f51851f08c3da3456098e11b237a58a9d
CRM8000 <crm8000@qq.com>
2018-09-13 10:40:42 +0800

    InventoryDAO.class.php - PSI业务架构

---
+++ web/Application/Home/DAO/Common/InventoryDAO.class.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Home\DAO\Common;
+
+/**
+ * 通用库存 DAO
+ *
+ * @author 李静波
+ */
+class InventoryDAO extends PSIBaseExDAO {
+
+	/**
+	 * 入库操作 - 修改库存账
+	 *
+	 * @param array $item        	
+	 */
+	public function inAction($item) {
+		$warehouseId = $item["warehouseId"];
+		if (! $warehouseId) {
+			return $this->badParam("warehouseId");
+		}
+		$goodsId = $item["goodsId"];
+		if (! $goodsId) {
+			return $this->badParam("goodsId");
+		}
+		
+		// TODO
+		return $this->todo();
+	}
+
+	/**
+	 * 出库操作 - 修改库存账
+	 *
+	 * @param array $item        	
+	 */
+	public function outAction($item) {
+		// TODO
+		return $this->todo();
+	}
+}
