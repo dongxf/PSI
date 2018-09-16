e459a9fa8cf72f11cd86c7559b6715de8fe8dce5
CRM8000 <crm8000@qq.com>
2018-09-16 11:38:30 +0800

    单元测试

--- web/Application/UnitTest/Service/PSIUnitTestService.class.php
+++ web/Application/UnitTest/Service/PSIUnitTestService.class.php
@@ -19,16 +19,28 @@ class PSIUnitTestService {
 		
 		// 下面的是示例数据
 		$result[] = [
-				"id" => 1,
-				"name" => "UnitTest\WarehouseUnitTest\testAddWarehouse",
+				"id" => "A000001",
+				"name" => "UnitTest\\WarehouseUnitTest\\testAddWarehouse",
 				"result" => 1,
 				"msg" => ""
 		];
 		$result[] = [
-				"id" => 2,
-				"name" => "UnitTest\WarehouseUnitTest\testDeleteWarehouse",
+				"id" => "A000002",
+				"name" => "UnitTest\\WarehouseUnitTest\\testDeleteWarehouse",
 				"result" => 0,
 				"msg" => "要删除的仓库不存在"
+		];
+		$result[] = [
+				"id" => "A000003",
+				"name" => "UnitTest\\WarehouseUnitTest\\testEditWarehouse",
+				"result" => 1,
+				"msg" => ""
+		];
+		$result[] = [
+				"id" => "A000004",
+				"name" => "UnitTest\\WarehouseUnitTest\\testEditWarehouseDataOrg",
+				"result" => 1,
+				"msg" => ""
 		];
 		
 		return $result;
