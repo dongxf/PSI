1e9560fafb17c415655bbf32e8125fb690596705
CRM8000 <crm8000@qq.com>
2018-10-06 08:53:23 +0800

    表单视图开发助手

--- doc/99 SQL/98test_form_view.sql
+++ doc/99 SQL/98test_form_view.sql
@@ -1,4 +1,6 @@
 TRUNCATE TABLE `t_fv_md`;
 INSERT INTO `t_fv_md` (id, parent_id, prop_name, prop_value, show_order) VALUES
-('1', NULL, 'view_name', '销售订单', 1),
-('2', NULL, 'view_name', '销售出库', 2);
+('01', NULL, 'view_name', '销售订单', 1),
+('0101', '01', 'fid', '2028', 0),
+('02', NULL, 'view_name', '销售出库', 2),
+('0201', '02', 'fid', '2002', 0);
