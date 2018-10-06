1b887f04b924fb2ce336e72eacefa0f29656d090
CRM8000 <crm8000@qq.com>
2018-10-06 08:23:31 +0800

    更新数据库设计文档 - t_fv_md新增字段show_order

---
+++ doc/99 SQL/98test_form_view.sql
@@ -0,0 +1,4 @@
+TRUNCATE TABLE `t_fv_md`;
+INSERT INTO `t_fv_md` (id, parent_id, prop_name, prop_value, show_order) VALUES
+('1', NULL, 'view_name', '销售订单', 1),
+('2', NULL, 'view_name', '销售出库', 2);
